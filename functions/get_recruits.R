2a2687d9ed318a2596e2909c86c951b33932811c
Samuel Truesdell <struesdell@neptune.org>
2018-05-01 12:11:57 -0400

    Some minor changes, mostly to use names rather than matrix positions.

--- functions/get_recruits.R
+++ functions/get_recruits.R
@@ -4,104 +4,112 @@
 
 # The function returns the recruits given
 # parameters as well as vector of environmental covariates and
-# environmental data. In the STRAWMAN version there is only a
-# super-simple temperature effect
+# environmental data.
 
 # type: the type of recruitment function
 # 
-#      *"constant" could be useful in testing.  par for constant is
+#      *"constantMean" could be useful in testing.  par for constantMean is
 #       R <- par * Rstoch
 #       par[1]: mean recruitment
 #       
 #      *"rickerlin" implementation of the Ricker model, in the form
 #       R = S*exp(a - b*S + c*tempY)
 #       par for the ricker linear model is
-#       par[1]: Ricker a
-#       par[2]: Ricker b
-#       par[3]: the temperature effect
+#       par['a',1]: Ricker a
+#       par['b',1]: Ricker b
+#       par['c',1]: the temperature effect
 #       
 #      *"rickerTS" time series implementation of the ricker model (i.e.,
 #       that includes an autocorrelative component
 #       R = a * S * exp(-b * S + c * tempY) * error + rho * resid0
 #       where resid0 is the residual from the previous year.
-#       par[1]: Ricker a
-#       par[2]: Ricker b
-#       par[3]: temperature effect c
-#       par[4]: autocorrelative component rho
-#       par[5]: previous year's recruitment residual
+#       par['a',1]: Ricker a
+#       par['b',1]: Ricker b
+#       par['c',1]: temperature effect c
+#       par['rho',1]: autocorrelative component rho
+#       par['resid0',1]: previous year's recruitment residual
 #       
 #      *"BHTS" time series implementation of the Beverton Holt model
 #       (i.e., that includes an autocorrelative component)
 #       R = a * S / (b + S) * exp(c * TempY) * error + rho * resid0;
 #       where resid0 is the residual from the previous year.
-#       par[1]: Beverton-Holt a
-#       par[2]: Beverton-Holt b
-#       par[3]: temperature effect c
-#       par[4]: autocorrelative component rho
-#       par[5]: previous year's recruitment residual
+#       par['a',1]: Ricker a
+#       par['b',1]: Ricker b
+#       par['c',1]: temperature effect c
+#       par['rho',1]: autocorrelative component rho
+#       par['resid0',1]: previous year's recruitment residual
 #      
 #      
 #      
-# par: the model parameters (see descriptions of type above)
+# par: the model parameters (see descriptions of type above). par must
+#      be a matrix of four columns with named rows that correspond
+#      to the parameter names listed under 'type' above. Column 1 must
+#      be the parameter estimate, column 2 must be the standard error,
+#      and columen 3 and 4 must be the lower and upper parameter bounds,
+#      respectively. For no parameter uncertainty, set the standard error
+#      column equal to zero. If estimates need not be specifically
+#      bounded (e.g., temperature coefficient) then use [-Inf,Inf]
 # 
 # S: the total spawning stock size -- mature individuals in weight
 # 
 # tempY: value for temperature in year y
 # 
-# stoch_sdlog: sd for lognormally distributed errors
+# resid0: Residual recruitment from the previous year (necessary if
+#         using models with autocorrelation)
 
 
 
-get_recruits <- function(type, par, S, tempY=NULL, stoch_sdlog){
+get_recruits <- function(type, par, S, tempY=NULL, resid0=NULL){
+  
+  # if no temperature is included then set to zero (will cancel out
+  # in the models)
+  if(is.null(tempY)){
+    tempY <- 0
+  }
   
   # stochastic multiplicitave effect
-  Rstoch <- rlnorm(n = 1, meanlog = log(1) - stoch_sdlog^2/2, 
-                   sdlog = stoch_sdlog)
-  
+  Rstoch <- rlnorm(n = 1, meanlog = log(1) - par['sigR']^2/2, 
+                   sdlog = par['sigR'])
   
   if(tolower(type) == 'rickerlin'){
     
-    if(length(par) != 3){
-      stop('get_recruits: length of par must be 3 with ricker
-           option')
+    if(!all(c('a', 'b', 'c') %in% names(par))){
+      stop('get_recruits: check parameters in Ricker linear option')
     }
     
     # (H & W p. 285)
-    Rhat <- S * exp(par[1] + par[2]*S + par[3]*tempY)
+    Rhat <- S * exp(par['a'] + par['b']*S + par['c']*tempY)
     R <- Rhat * Rstoch
 
   }else if(tolower(type) == 'bhts'){
     
-    if(length(par) != 5){
-      stop('get_recruits: length of par must be 5 with BHTS
-           option')
+    if(!all(c('a', 'b', 'c', 'rho', 'resid0') %in% names(par))){
+      stop('get_recruits: check parameters in BHTS option')
     }
     
     # BH parameterization from H&W p. 286
-    Rhat <- par[1] * S / (par[2] + S) * exp(par[3] * tempY)
-    R <- Rhat * Rstoch + par[4] * par[5]
+    Rhat <- par['a'] * S / (par['b'] + S) * exp(par['c'] * tempY)
+    R <- Rhat * Rstoch + par['rho'] * par['resid0']
     
   }else if(tolower(type) == 'RickerTS'){
     
-    if(length(par) != 5){
-      stop('get_recruits: length of par must be 5 with rickerts
-           option')
+    if(!all(c('a', 'b', 'c', 'rho', 'resid0') %in% names(par))){
+      stop('get_recruits: check parameters in RickerTS option')
     }
     
     # Ricker parameterization from Q&D p. 91
-    Rhat <- par[1] * S * exp(-par[2] * S + par[3] * tempY)
-    R <- Rhat * Rstoch + par[4] * par[5]
+    Rhat <- par['a'] * S * exp(-par['b'] * S + par['c'] * tempY)
+    R <- Rhat * Rstoch + par['rho'] * par['resid0']
     
-  }else if(tolower(type) == 'constant'){
+  }else if(tolower(type) == 'constantMean'){
  
-    if(length(par) != 2){
-      stop('get_recruits: length of par must be 1 with constant
-           option')
+    if(length(par) != 1){
+      stop('get_recruits: check parameters in constantMean option')
     }
     
     # include a small deviation because otherwise Rdevs will
     # all be zero which will result in NaN
-    R <- par * Rstoch
+    R <- par['a'] * par['sigR']
     
   }else{
     
