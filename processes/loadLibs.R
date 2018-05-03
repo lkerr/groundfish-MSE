2db55204a29052ae58858d68b83e717940fe2ad8
Samuel Truesdell <struesdell@neptune.org>
2018-05-03 10:03:26 -0400

    some changes in order of library loading for linux system

--- processes/loadLibs.R
+++ processes/loadLibs.R
@@ -2,21 +2,24 @@
 # load the necessary libraries. Same libraries are loaded
 # whether using Windows or Linux but a little less complicated
 # to not keep a local copy of corresponding Windows R
-# packages on the machine. Even though there is an Rlib
-# directory always it's only going to be used when running
-# on the ghpcc.
+# packages on the machine. An Rlib directory will be
+# necessary for running on the ghpcc.
 
 
 platform <- Sys.info()['sysname']
 if(platform == 'Linux'){
 
-  require(msm, lib.loc='Rlib/')
+  # seems you need to load gmm prior to loading tmvtnorm
+  require(gmm, lib.loc='Rlib/')
   require(tmvtnorm, lib.loc='Rlib/')
+  require(msm, lib.loc='Rlib/')
+  require(TMB, lib.loc='Rlib/')
   
 }else{
   
   require(msm)
   require(tmvtnorm)
+  require(TMB)
   
 }
 
