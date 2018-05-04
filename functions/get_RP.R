cdc806a82d7516aab84f8e00f27020c0b2684a93
Samuel Truesdell <struesdell@neptune.org>
2018-05-04 17:37:40 -0400

    function for reference point calcs

---
+++ functions/get_RP.R
@@ -0,0 +1,29 @@
+
+
+get_RP <- function(type, par){
+  
+  
+  if(type == 'YPR'){
+   
+    
+    return(F=F, B=NULL)
+     
+  }else if(type == 'SSBR'){
+    
+    
+    return(F=NULL, B=B)
+    
+  }else if(type == 'simR'){
+    
+    
+    return(F=F, B=B) 
+  }
+  
+  
+  
+}
+
+
+
+
+
