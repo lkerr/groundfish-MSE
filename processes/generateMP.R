b7de02eda7612fd042b8bc486dbf7b8602683af6
Samuel Truesdell <struesdell@neptune.org>
2018-05-10 16:52:50 -0400

    File to generate a list of management procedures

---
+++ processes/generateMP.R
@@ -0,0 +1,60 @@
+
+
+# Process to generate a set of management procedures to include when
+# running the assessment model
+
+
+# Potential proxies for Fmax. A list of the names of the proxy methods
+# and a list of the potential values to use for each method.
+Fprox <- list('YPR', 'SPR')
+Fval <- list(c(0.1, 0.15),
+             c(0.3, 0.4, 0.5))
+
+# Potential proxies for Bmax. A list of the names of the proxy methods
+# and a list of the potential values to use for each method.
+Bprox <- list('RSPR', 'dummy')
+Bval <- list(c(0.8, 1, 0.2),
+             c(0.25, 0.5, 1))
+
+# Potential versions of harvest control rules
+HCR <- c('ns1', 'simpleThresh')
+
+# expand out the potential values for the combinations of each proxy
+# method and its potential values
+egF <- lapply(1:length(Fprox), 
+              function(x) expand.grid(Fprox[[x]], Fval[[x]]))
+egB <- lapply(1:length(Bprox), 
+              function(x) expand.grid(Bprox[[x]], Bval[[x]]))
+
+# Concatonate the lists of expanded combinations for proxy methods and
+# potential values
+fopt <- do.call(rbind, egF)
+bopt <- do.call(rbind, egB)
+
+# Make the proxy methods into a list and determine the number of rows
+# in each of the list components. Add an additional list element for
+# each of the harvest control rules
+lst <- list(fopt, bopt)
+nrlist <- sapply(lst, function(x) 1:nrow(x))
+nrlist[[3]] <- 1:length(HCR)
+
+# Determine row IDs for all combination of reference point calculations
+# and harvest control rule methods
+id <- expand.grid(nrlist)
+
+# Generate a data frame containing all combinations of proxy methods
+# and control rules by using the ID to index
+methdf <- cbind( HCR[id[,3]], lst[[1]][id[,1],], lst[[2]][id[,2],])
+names(methdf) <- c('HCR', 'FREF_TYP', 'FREF_VAL', 'BREF_TYP',
+                'BREF_VAL')
+
+# next identify which of the rows are superfluous given the HCR
+# (e.g., you only need a biomass-based reference point if you're
+# using a simple threshold biomass-based control rule)
+wthresh <- methdf$HCR == 'simpleThresh'
+methdf$FREF_TYP[wthresh] <- NA
+methdf$FREF_VAL[wthresh] <- NA
+
+mgtproc <- unique(methdf)
+
+
--- processes/runSim.R
+++ processes/runSim.R
@@ -31,126 +31,128 @@ source('processes/get_containers.R')
 # begin the model loop
 for(r in 1:nrep){
 
+  for(m in 1:nrow(mgtproc)){
   
-  # First few years of fishery information
-  F_full[1:(ncaayear + fyear + nburn +1)] <- rlnorm(ncaayear + 
-                                                      fyear + nburn + 1, 
-                                                    log(0.2), 0.1)
-
-  # initialize the model with numbers and mortality rates
-  # in the first n (fyear-1) years.
+    # First few years of fishery information
+    F_full[1:(ncaayear + fyear + nburn +1)] <- rlnorm(ncaayear + 
+                                                        fyear + nburn + 1, 
+                                                      log(0.2), 0.1)
   
-  
-  initN <- get_init(nage=nage, N0=2e7, F_full=F_full[1], M=M)
-  J1N[1:(fyear-1),] <- rep(initN, each=(fyear-1))
-  laa[1:(fyear-1),] <- rep(get_lengthAtAge(type='vonB', par=laa_par, 
-                                       ages=fage:page),
-                           each=(fyear-1))
-  waa[1:(fyear-1),] <- get_weightAtAge(type='aLb', par=waa_par, 
-                                       laa=laa[1:(fyear-1),],
-                                       inputUnit='kg')
-  Z[1:(fyear-1),] <- rep(F_full[1]+M, each=(fyear-1))
-
-
-
-  for(y in fyear:nyear){
-  
-    # calculate length-at-age in year y
-    laa[y,] <- get_lengthAtAge(type=laa_typ, par=laa_par, 
-                               ages=fage:page)
-    # other option here is get_size which tries to take temperature
-    # into account. Needs some work there though.
+    # initialize the model with numbers and mortality rates
+    # in the first n (fyear-1) years.
     
-    # calculate weight-at-age in year y
-    waa[y,] <- get_weightAtAge(type=waa_typ, par=waa_par, 
-                               laa=laa[y,], inputUnit='kg')
     
-    # calculate maturity in year y
-    mat[y,] <- get_maturity(type=mat_typ, par=mat_par, laa=laa[y,])
-    
-    # calculate recruits in year y based on the SSB in years previous
-    # (depending on the lag) and temperature
-    SSB[y] <- sum(J1N[y-fage,] * mat[y,] * waa[y-fage,])  
-
-    Rpar <- get_recruitment_par(par=srpar, stochastic=FALSE)
-    Rout <- get_recruits(type=Rpar$type, par=Rpar, S=SSB[y],
-                         tempY=temp[y], resid0 = residR[y-1])
-    R[y] <- Rout['R']
-    residR[y] <- Rout['resid']
-    R[y] <- ifelse(R[y] < 0, 0, R[y])
-    
-    # calculate the selectivity in year y (changes if the laa changes)
-    slxC[y,] <- get_slx(type=selC_typ, par=selC, laa=laa[y,])
-    
-    # get Z for the current year
-    Z[y,] <- F_full[y]*slxC[y,] + M
-    
-    # calculate what the Jan 1 population numbers are for year y, which
-    # depend on the numbers and mortality rate in the previous year and
-    # on the recruitment this year
-    J1N[y,] <- get_J1Ny(J1Ny0=J1N[y-1,], Zy0=Z[y-1,], R[y])
-    
-    # calculate the predicted catch in year y, the catch weight and the
-    # proportions of catch numbers-at-age
-    CN[y,] <- get_catch(F_full=F_full[y], M=M, N=J1N[y,], selC=slxC[y,])
-    sumCW[y] <- CN[y,] %*% waa[y,]    # (dot product)
-    paaCN[y,] <- CN[y,] / sum(CN[y,])
-  
-    # calculate the predicted survey index in year y and the predicted
-    # survey proportions-at-age
-    IN[y,] <- get_survey(F_full=F_full[y], M=M, N=J1N[y,], slxC[y,], 
-                         slxI=1, timeI=timeI, qI=qI)
-    sumIN[y] <- sum(IN[y,])
-    paaIN[y,] <- IN[y,] / sum(IN[y,])
-    
-    # calculate effort based on catchability and the implemented fishing
-    # mortality. Effort not typically derived ... could go the other way
-    # around and implement E as a policy and calculate F.
-    effort[y] <- F_full[y] / qC
-    obs_effort[y] <- get_error_idx(type=oe_effort_typ, idx=effort[y], 
-                                   par=oe_effort)
-    
-    # Get observation error data for the assessment model
-    obs_sumCW[y] <- get_error_idx(type=oe_sumCW_typ, idx=sumCW[y], par=oe_sumCW)
-    obs_paaCN[y,] <- get_error_paa(type=oe_paaCN_typ, paa=paaCN[y,], par=oe_paaCN)
-    obs_sumIN[y] <- get_error_idx(type=oe_sumIN_typ, idx=sumIN[y], par=oe_sumIN)
-    obs_paaIN[y,] <- get_error_paa(type=oe_paaIN_typ, paa=paaIN[y,], par=oe_paaIN)
+    initN <- get_init(nage=nage, N0=2e7, F_full=F_full[1], M=M)
+    J1N[1:(fyear-1),] <- rep(initN, each=(fyear-1))
+    laa[1:(fyear-1),] <- rep(get_lengthAtAge(type='vonB', par=laa_par, 
+                                         ages=fage:page),
+                             each=(fyear-1))
+    waa[1:(fyear-1),] <- get_weightAtAge(type='aLb', par=waa_par, 
+                                         laa=laa[1:(fyear-1),],
+                                         inputUnit='kg')
+    Z[1:(fyear-1),] <- rep(F_full[1]+M, each=(fyear-1))
   
   
-    # if burn-in period is over...
-    if(y > ncaayear + fyear + nburn){
-
-      # prepare data & run assessment model
-      source('processes/get_tmb_setup.R')
-      source('assessment/caa.R')
+  
+    for(y in fyear:nyear){
+    
+      # calculate length-at-age in year y
+      laa[y,] <- get_lengthAtAge(type=laa_typ, par=laa_par, 
+                                 ages=fage:page)
+      # other option here is get_size which tries to take temperature
+      # into account. Needs some work there though.
       
-      # Fill the arrays with results
-      y2 <- y - (ncaayear + fyear + nburn)
-      source('processes/fill_repArrays.R')
+      # calculate weight-at-age in year y
+      waa[y,] <- get_weightAtAge(type=waa_typ, par=waa_par, 
+                                 laa=laa[y,], inputUnit='kg')
       
-      # Get fishing mortality for next year's management
-      fbrpy <- get_FBRP(type=fbrpTyp, par=fbrpLevel,
-                        sel=endv(rep$slxC), waa=endv(rep$waa), 
-                        M=M[1])
-      # note must convert matrices to vectors when using tail() function
-      # to get the appropriate behavior
-      bbrpy <- get_BBRP(type=bbrpTyp, par=bbrpLevel,
-                        sel=endv(rep$slxC), waa=endv(rep$waa), 
-                        M=endv(rep$M), mat=mat[y,], 
-                        R=rep$R, B=SSBhat, Rfun=mean)
-
-      # apply the harvest control rule
-      nextF <- get_nextF(type=hrcTyp, Fmsy=fbrpy, Bmsy=bbrpy, 
-                         SSB=endv(SSBhat))
-      if(y < nyear){
-        F_full[y+1] <- nextF
+      # calculate maturity in year y
+      mat[y,] <- get_maturity(type=mat_typ, par=mat_par, laa=laa[y,])
+      
+      # calculate recruits in year y based on the SSB in years previous
+      # (depending on the lag) and temperature
+      SSB[y] <- sum(J1N[y-fage,] * mat[y,] * waa[y-fage,])  
+  
+      Rpar <- get_recruitment_par(par=srpar, stochastic=FALSE)
+      Rout <- get_recruits(type=Rpar$type, par=Rpar, S=SSB[y],
+                           tempY=temp[y], resid0 = residR[y-1])
+      R[y] <- Rout['R']
+      residR[y] <- Rout['resid']
+      R[y] <- ifelse(R[y] < 0, 0, R[y])
+      
+      # calculate the selectivity in year y (changes if the laa changes)
+      slxC[y,] <- get_slx(type=selC_typ, par=selC, laa=laa[y,])
+      
+      # get Z for the current year
+      Z[y,] <- F_full[y]*slxC[y,] + M
+      
+      # calculate what the Jan 1 population numbers are for year y, which
+      # depend on the numbers and mortality rate in the previous year and
+      # on the recruitment this year
+      J1N[y,] <- get_J1Ny(J1Ny0=J1N[y-1,], Zy0=Z[y-1,], R[y])
+      
+      # calculate the predicted catch in year y, the catch weight and the
+      # proportions of catch numbers-at-age
+      CN[y,] <- get_catch(F_full=F_full[y], M=M, N=J1N[y,], selC=slxC[y,])
+      sumCW[y] <- CN[y,] %*% waa[y,]    # (dot product)
+      paaCN[y,] <- CN[y,] / sum(CN[y,])
+    
+      # calculate the predicted survey index in year y and the predicted
+      # survey proportions-at-age
+      IN[y,] <- get_survey(F_full=F_full[y], M=M, N=J1N[y,], slxC[y,], 
+                           slxI=1, timeI=timeI, qI=qI)
+      sumIN[y] <- sum(IN[y,])
+      paaIN[y,] <- IN[y,] / sum(IN[y,])
+      
+      # calculate effort based on catchability and the implemented fishing
+      # mortality. Effort not typically derived ... could go the other way
+      # around and implement E as a policy and calculate F.
+      effort[y] <- F_full[y] / qC
+      obs_effort[y] <- get_error_idx(type=oe_effort_typ, idx=effort[y], 
+                                     par=oe_effort)
+      
+      # Get observation error data for the assessment model
+      obs_sumCW[y] <- get_error_idx(type=oe_sumCW_typ, idx=sumCW[y], par=oe_sumCW)
+      obs_paaCN[y,] <- get_error_paa(type=oe_paaCN_typ, paa=paaCN[y,], par=oe_paaCN)
+      obs_sumIN[y] <- get_error_idx(type=oe_sumIN_typ, idx=sumIN[y], par=oe_sumIN)
+      obs_paaIN[y,] <- get_error_paa(type=oe_paaIN_typ, paa=paaIN[y,], par=oe_paaIN)
+    
+    
+      # if burn-in period is over...
+      if(y > ncaayear + fyear + nburn){
+  
+        # prepare data & run assessment model
+        source('processes/get_tmb_setup.R')
+        source('assessment/caa.R')
+        
+        # Fill the arrays with results
+        y2 <- y - (ncaayear + fyear + nburn)
+        source('processes/fill_repArrays.R')
+        
+        # Get fishing mortality for next year's management
+        fbrpy <- get_FBRP(type=fbrpTyp, par=fbrpLevel,
+                          sel=endv(rep$slxC), waa=endv(rep$waa), 
+                          M=M[1])
+        # note must convert matrices to vectors when using tail() function
+        # to get the appropriate behavior
+        bbrpy <- get_BBRP(type=bbrpTyp, par=bbrpLevel,
+                          sel=endv(rep$slxC), waa=endv(rep$waa), 
+                          M=endv(rep$M), mat=mat[y,], 
+                          R=rep$R, B=SSBhat, Rfun=mean)
+  
+        # apply the harvest control rule
+        nextF <- get_nextF(type=hrcTyp, Fmsy=fbrpy, Bmsy=bbrpy, 
+                           SSB=endv(SSBhat))
+        if(y < nyear){
+          F_full[y+1] <- nextF
+        }
+  
       }
-
+      
+      
+     
+      
     }
-    
-    
-   
-    
   }
 }
 
