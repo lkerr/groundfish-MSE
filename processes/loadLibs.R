aa97ce27e47432cbe88901026e67af25cf6d0e90
samtruesdell <samuel.truesdell@maine.edu>
2018-05-28 21:33:12 -0400

    add function to install packages on windows machines if libraries are not there.

--- processes/loadLibs.R
+++ processes/loadLibs.R
@@ -24,6 +24,21 @@ if(platform == 'Linux'){
   
 }else{
   
+  # check.packages function: install and load multiple R packages.
+  # Check to see if packages are installed. Install them if they are not, 
+  # then load them into the R session.
+  # https://gist.github.com/smithdanielle/9913897
+  check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+      install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+  }
+  
+  # Usage example
+  pkg<-c("msm", "tmvtnorm", "TMB")
+  check.packages(pkg)
+  
   require(msm)
   require(tmvtnorm)
   require(TMB)
