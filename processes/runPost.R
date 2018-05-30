36a8c77154cda65921c4f1e90b5687e7a06c1d59
Samuel Truesdell <struesdell@neptune.org>
2018-05-30 08:56:39 -0400

    concatonated simulation output and generated plots

--- processes/runPost.R
+++ processes/runPost.R
@@ -1,5 +1,15 @@
 
 
 
-list.files('results/sim/)
+fl <- list.files('results/sim/', full.names=TRUE)
 
+flLst <- list()
+for(i in 1:length(fl)){
+  load(fl[i])
+  flLst[[i]] <- omval
+}
+
+omval <- get_simcat(x=flLst)
+names(omval) <- names(flLst[[1]])
+
+get_plots(omval, dirOut='results/fig/')
