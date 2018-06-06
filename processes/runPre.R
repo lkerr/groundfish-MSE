b1a71bcd13a8b2397776c0216474323bfecbe012
samtruesdell <samuel.truesdell@maine.edu>
2018-06-05 20:39:26 -0400

    New script for setting things up before the simulation. Right now only has code to compile the TMB model.

---
+++ processes/runPre.R
@@ -0,0 +1,25 @@
+
+
+# Code to compile TMB code before running the operating model. Doing this
+# because it's probably a little better to compile once and then access the
+# executable only -- certainly for when this is running on the HPCC as an
+# array so you aren't trying to compile the same cpp file with multiple
+# programs at once. Could include in the assessment R file, but if it's
+# separate here it is easier for the HPCC to deal with.
+
+
+# load the required libraries
+source('processes/loadLibs.R')
+
+# Ensure that TMB will use the Rtools compiler (only windows ... and 
+# not necessary on all machines)
+if(platform != 'Linux'){
+  path_current <- Sys.getenv('PATH')
+  path_new <- paste0('c:\\Rtools\\bin;c:\\Rtools\\mingw_32\\bin;',
+                     path_current)
+  Sys.setenv(PATH=path_new)
+}
+
+# compile the c++ file and make available to R
+TMB::compile("assessment/caa.cpp")
+
