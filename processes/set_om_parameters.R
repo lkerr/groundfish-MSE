b6d94f2ceeacf91b87861d9246091fa540b4c39e
Samuel Truesdell <struesdell@neptune.org>
2018-08-14 09:31:53 -0400

    Change the location of the subsetting CMIP5 operation

--- processes/set_om_parameters.R
+++ processes/set_om_parameters.R
@@ -27,6 +27,9 @@ fyear <- 5
 # number of burn-in years (before the pre-2000 non-assessment period)
 nburn <- 50
 
+# maximum year predicted into the future
+mxyear <- 2050
+
 # number of burn years for saving OM results (after management
 # procedure has started)
 # rburn <- 35
@@ -42,6 +45,7 @@ cmip5model <- 'CMCC_CM'
 # Load the cmip5 temperature data
 cmip5 <- read.table(file='data/data_raw/NEUS_CMIP5_annual_means.txt',
                     header=TRUE, skip=2)
+cmip5 <- subset(cmip5, nyear <= mxyear)
 
 nyear <- nrow(cmip5) + nburn
 # nyear <- 100 + nburn
