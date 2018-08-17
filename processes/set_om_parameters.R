02af838eb980d2d41a58c9766d90fd9ccd79f839
Samuel Truesdell <struesdell@neptune.org>
2018-08-17 10:53:15 -0400

    run.

--- processes/set_om_parameters.R
+++ processes/set_om_parameters.R
@@ -87,7 +87,7 @@ load('data/data_processed/SR/cod/BHTS.Rdata') #srpar
 # include stochasticity in recruitment parameter estimates?
 Rstoch_par <- FALSE
 # include stochasticity in annual recruitment estimate?
-Rstoch_ann <- FALSE
+Rstoch_ann <- TRUE
 
 
 
