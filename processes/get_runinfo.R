7a797b80dafcc4ad346b933371b84a5fed6ff98c
samtruesdell <36001916+samtruesdell@users.noreply.github.com>
2018-11-06 13:32:36 -0500

    Update get_runinfo.R
    update for test

--- processes/get_runinfo.R
+++ processes/get_runinfo.R
@@ -8,7 +8,7 @@ platform <- Sys.info()['sysname']
 # Determine whether or not this is a run on the HPCC by checking for the
 # existence of the folder Rlib. Duplicate of code in runSim.R but this is
 # necessary because runPre.R is run separately when run on the HPCC.
-if(file.exists('../../COCA_HPCC')){
+if(file.exists('../../HPCC')){
   runClass <- 'HPCC'
 }else{
   runClass <- 'Local'
