e02080c82ee4e5f206e5f4f58796ceeeafa64ca0
Samuel Truesdell <struesdell@neptune.org>
2018-11-09 09:55:49 -0500

    Updated system ID to identify whether program is running on the HPCC or not.

--- processes/get_runinfo.R
+++ processes/get_runinfo.R
@@ -8,14 +8,13 @@ platform <- Sys.info()['sysname']
 # Determine whether or not this is a run on the HPCC by checking the name of
 # the node
 if(platform == 'Linux'){
-  # Identify the name of the node
-  # nn <- system('uname --nodename')
-  # if(nn == 'ghpcc06'){
-  #   runClass <- 'HPCC'
-  # }else{
-  #   runClass <- 'Local'
-  # }
-  runClass <- 'HPCC'
+  # Dig into the environmental variables
+  evndir <- system('printenv LSF_ENVDIR')
+  if(evndir == '/lsf/conf'){
+    runClass <- 'HPCC'
+  }else{
+    runClass <- 'Local'
+  }
 }else{
   runClass <- 'Local'
 }
