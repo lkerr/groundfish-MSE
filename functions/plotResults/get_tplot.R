0f87ea02d5c73f435d7c705a2ba11374ce3eb0e6
Samuel Truesdell <struesdell@neptune.org>
2018-12-11 12:20:48 -0500

    included y limits in trajectory function

--- functions/plotResults/get_tplot.R
+++ functions/plotResults/get_tplot.R
@@ -2,10 +2,10 @@
 
 # Plots for simulation trajectories
 
-get_tplot <- function(x, yrs, mpName, PMname){
+get_tplot <- function(x, yrs, mpName, PMname, ylim=NULL){
   
   
-  plot(x ~ yrs, type='o', pch=16, xlab='', ylab='')
+  plot(x ~ yrs, type='o', pch=16, xlab='', ylab='', ylim=ylim)
   mtext(side=1:3, line=c(3,3,1), cex=1.25,
         c('Year', PMname, mpName))
   
