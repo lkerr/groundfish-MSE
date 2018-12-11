68408eac8c93b8e894859a3763ed61c24118c295
Samuel Truesdell <struesdell@neptune.org>
2018-12-11 12:20:01 -0500

    - Included F and SSB proxies in the trajectory plots
    - adjusted the temperature time series plot so it plots the anomoly directly
    
    - fixed bug in the ranges of the boxplot trajectories
    
    - Included plot for the median of each MP over time

--- functions/plotResults/get_plots.R
+++ functions/plotResults/get_plots.R
@@ -28,7 +28,7 @@ get_plots <- function(x, dirIn, dirOut){
                              "mxGradCAA",
                              "relE_qI", "relE_qC", "relE_selC",
                              "relE_ipop_mean", "relE_ipop_dev",
-                             "relE_R_dev"))
+                             "relE_R_dev", "FPROXY", "SSBPROXY"))
   
   # Year names
   yridx <- which(nm %in% "YEAR")
@@ -113,8 +113,8 @@ get_plots <- function(x, dirIn, dirOut){
   jpeg(paste0(dirOut, 'tempts.jpg.'),
        width=480*1.75, height=480, pointsize=12*1.5)
     yrs <- (mxyear - length(temp)+1):mxyear
-    get_tempTSPlot(tempts = temp, yrs = yrs,
-                   fmyear=fmyear, anomStd = anomStd)
+    get_tempTSPlot(tanom = Tanom, yrs = yrs,
+                   fmyear=fmyear, tanomStd = anomStd)
   dev.off()
  
   # Plot describing how growth changed over time
@@ -145,6 +145,7 @@ get_plots <- function(x, dirIn, dirOut){
   repidx <-sample(1:dim(x[[trajidx[1]]])[1], 
                   size=min(5, dim(x[[trajidx[1]]])[1]))
 
+  
   for(i in trajidx){
 
     tempPM <- x[[i]]
@@ -158,12 +159,15 @@ get_plots <- function(x, dirIn, dirOut){
       bpstats[[mp]] <- boxplot(x = tempPMmp, plot=FALSE)$stats
     }
     
-    # Now get the range of the statistics to use in the loop.
-    yrg <- range(unlist(bpstats), na.rm=TRUE)
+    # Now get the range of the statistics to use in the loop (boxplots).
+    yrgbx <- range(unlist(bpstats), na.rm=TRUE)
+    
+    # Get y range for common axes among the reps (simulation trajectories)
+    yrgrsim <- range(tempPM[repidx,,], na.rm=TRUE)
  
     # Account for 0-1 nature of binary variabiles (e.g., overfished status)
-    if(is.na(yrg[1])){
-      yrg <- c(0, 1)
+    if(is.na(yrgbx[1])){
+      yrgbx <- c(0, 1)
     }
     
     dir.create(file.path(dirOut, 'Traj', PMname), showWarnings=FALSE)
@@ -178,20 +182,20 @@ get_plots <- function(x, dirIn, dirOut){
         
         par(mar=c(4,4,1,1))
         get_tbxplot(x = tempPMmp, PMname=PMname, yrs=x$YEAR,
-                    printOutliers=FALSE, yrg=yrg)
+                    printOutliers=FALSE, yrg=yrgbx)
       
       dev.off()
       
-      
-      # Do just 5 trajectories as examples
+     
+      # Do (up to) 5 trajectories as examples
       for(r in repidx){
-  
+
         jpeg(paste0(dirOut, 'Traj/', PMname, '/mp', mp, 'rep', r, '.jpg.'),
              width=480*1.75, height=480, pointsize=12*1.5)
        
           get_tplot(x=tempPMmp[r,], yrs = x$YEAR, 
                     mpName=paste('MP', mp), 
-                    PMname=PMname)
+                    PMname=PMname, ylim=yrgrsim)
           # plot(tempPM[r,mp,], type='o')
         
         dev.off()
@@ -199,8 +203,17 @@ get_plots <- function(x, dirIn, dirOut){
       }
     }
     
-    #yridx -- label
+    # Trajectories for the medians of each MP over time
     
+    # Get the medians of each performance measure over time
+    mpMed <- apply(tempPM, c(2,3), median, na.rm=TRUE)
+    
+    # Make the plot
+    jpeg(paste0(dirOut, 'Traj/', PMname, '/MPmedTraj.jpg.'),
+         width=480*1.75, height=480, pointsize=12*1.5)
+      par(mar=c(4,4,1,1))
+      get_mpMedTraj(mpMedMat = mpMed, ylab=nm[i])
+    dev.off()
   }
   
   
