f565a578f7a510750374a1c0b63f9f7031a681f7
Samuel Truesdell <struesdell@neptune.org>
2018-12-21 08:25:46 -0500

    small edit --> produce error message

--- functions/assessment/get_bounds.R
+++ functions/assessment/get_bounds.R
@@ -4,6 +4,9 @@
 # true values. Not implemented for 2d structures.
 # 
 # x: the true paramter value(s) - could be a number or a vector
+# 
+# type: either "upper" or "lower", indicating whether an upper or lower
+#       bound is desired
 #
 # p: the extent to which the bound goes. An extent of 1.0 means
 #    that the bound stretches from 0 to 2* the true value (assuming
@@ -13,6 +16,9 @@
 
 get_bounds <- function(x, type, p){
   
+    if(!type %in% c('upper', 'lower')){
+      stop('get_bounds: type should be either upper or lower')
+    }
     
     if(type == 'lower'){
       
