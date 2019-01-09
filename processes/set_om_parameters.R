a6ece35310b5cc4966ca9664d4dad64e720db931
Samuel Truesdell <struesdell@neptune.org>
2019-01-09 17:52:24 -0500

    run

--- processes/set_om_parameters.R
+++ processes/set_om_parameters.R
@@ -48,7 +48,7 @@ burnFsd <- 0.2
 #### Temperature information ####
 
 ## Do you want to include temperature projections (in S-R, growth, etc.)
-useTemp <- FALSE
+useTemp <- TRUE
 
 ## Temperature information ##
 cmip5model <- 'CMCC_CM'
