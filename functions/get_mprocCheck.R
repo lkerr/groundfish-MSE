57b00179e7d1338c17a64a9cd1f458ef469e2c9c
Samuel Truesdell <struesdell@neptune.org>
2019-01-16 15:08:33 -0500

    not really a change just want to move branches.

--- functions/get_mprocCheck.R
+++ functions/get_mprocCheck.R
@@ -71,6 +71,16 @@ get_mprocCheck <- function(mproc){
              paste0('value out of range in RPInt column of mproc.txt:',
              ' must be both an integer and greater than 0'))
   }
+# browser()  
+#   if(any(!is.na(mproc$RFUN_NM == 'hindcastMean' & 
+#          ( (mproc$FREF_TYP == 'FMSY' & mproc$FREF_PAR0 < 100) | 
+#            (mproc$BREF_TYP == 'SIM' & mproc$BREF_PAR0 < 100) )))){
+#     msg <- c(msg,
+#              pasteo('number of years for hindcast is probably too low',
+#                     'for stable estimate of FMSY or BMSY. Try setting',
+#                     'FREF_PAR0 (if FREF_TYP==FMSY) or BREF_PAR0 (if ',
+#                     'FREF_TYP==SIM) to something larger than 100.'))
+#   }
   
   # If there were any errors then 
   if(length(msg) > 0){
@@ -85,36 +95,36 @@ get_mprocCheck <- function(mproc){
 
 # (just random stuff -- not actual sets of trials!)
 
-## correct
-# mproc1 <- data.frame(ASSESSCLASS = c('CAA', 'CAA', 'PLANB'),
-                    # HCR = c('slide', 'simplethresh', 'constF'),
-                    # FREF_TYP = c('YPR', 'SPR', 'Fmed'),
-                    # FREF_PAR0 = c(0.1, 0.5, 0.1),
-                    # BREF_TYP = c('SIM', 'SIM', 'RSSBR'),
-                    # BREF_PAR0 = c(NA, NA, NA),
-                    # RFUN_NM = c('L5SAMP', 'MEAN', 'recT'),
-                    # RPInt = c(1, 3, 5))
-# 
-## incorrect
-# mproc2 <- data.frame(ASSESSCLASS = c('CAAXXX', 'CAA', 'PLANB'),
-#                      HCR = c('slide', 'simplethresh', 'constF'),
-#                      FREF_TYP = c('YPR', 'SPR', 'Fmed'),
-#                      FREF_PAR0 = c(0.1, 0.5, 0.1),
-#                      BREF_TYP = c('SIM', 'SIMXXX', 'RSSBR'),
-#                      BREF_PAR0 = c(NA, NA, NA),
-#                      RFUN_NM = c('L5SAMPXXX', 'MEAN', 'recT'),
-#                      RPInt = c(1, 3, 5))
-# 
-# mproc3 <- data.frame(ASSESSCLASS = c('CAA', 'CAA', 'PLANB'),
-#                      HCR = c('slide', 'simplethresh', 'constF'),
-#                      FREF_TYP = c('YPR', 'SPR', 'Fmed'),
-#                      FREF_PAR0 = c(0.1, 0.5, 999),
-#                      BREF_TYP = c('SIM', 'SIM', 'RSSBR'),
-#                      BREF_PAR0 = c(25.999, NA, NA),
-#                      RFUN_NM = c('L5SAMP', 'MEAN', 'recT'),
-#                      RPInt = c(1, 3, 5.999))
-# 
-# 
-# get_mprocCheck(mproc1)
-# get_mprocCheck(mproc2)
-# get_mprocCheck(mproc3)
+# correct
+mproc1 <- data.frame(ASSESSCLASS = c('CAA', 'CAA', 'PLANB'),
+                     HCR = c('slide', 'simplethresh', 'constF'),
+                     FREF_TYP = c('YPR', 'SPR', 'Fmed'),
+                     FREF_PAR0 = c(0.1, 0.5, 0.1),
+                     BREF_TYP = c('SIM', 'SIM', 'RSSBR'),
+                     BREF_PAR0 = c(NA, NA, NA),
+                     RFUN_NM = c('hindcastMean', 'forecast', NA),
+                     RPInt = c(1, 3, 5))
+
+# incorrect
+mproc2 <- data.frame(ASSESSCLASS = c('CAAXXX', 'CAA', 'PLANB'),
+                     HCR = c('slide', 'simplethresh', 'constF'),
+                     FREF_TYP = c('YPR', 'SPR', 'Fmed'),
+                     FREF_PAR0 = c(0.1, 0.5, 0.1),
+                     BREF_TYP = c('SIM', 'SIMXXX', 'RSSBR'),
+                     BREF_PAR0 = c(NA, NA, NA),
+                     RFUN_NM = c('L5SAMPXXX', 'MEAN', 'recT'),
+                     RPInt = c(1, 3, 5))
+
+mproc3 <- data.frame(ASSESSCLASS = c('CAA', 'CAA', 'PLANB'),
+                     HCR = c('slide', 'simplethresh', 'constF'),
+                     FREF_TYP = c('YPR', 'SPR', 'FMSY'),
+                     FREF_PAR0 = c(0.1, 0.5, 99),
+                     BREF_TYP = c('SIM', 'SIM', 'RSSBR'),
+                     BREF_PAR0 = c(25.999, NA, NA),
+                     RFUN_NM = c('L5SAMP', 'MEAN', 'recT'),
+                     RPInt = c(1, 3, 5.999))
+
+
+get_mprocCheck(mproc1)
+get_mprocCheck(mproc2)
+get_mprocCheck(mproc3)
