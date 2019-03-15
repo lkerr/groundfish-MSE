a231ea7ba1330fbf5ff737feb0985c1bf7512a37
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-03-15 11:21:37 -0400

    Add roadmap for reading-in production function code.

--- preprocessing/economic/production_targeting.R
+++ preprocessing/economic/production_targeting.R
@@ -17,21 +17,38 @@ production_source<-"sample_PRODREGdata_gillnets_fy2012_forML.dta"
 # read in the datasets
 targeting <- read.dta13(file.path(rawpath, targeting_source))
 production <- read.dta13(file.path(rawpath, production_source))
-# read in the estimated coefficients
-
-
-
-
-
-
 
 # I think we don't want to drop any data out of the datasets until right before the simulations.
 # May make sense to sort data here
 
-
 # We will want to always have the datasets merged regression coefficients here.
 # save processed data
-save(targeting, file=file.path(savepath, "econ_targeting.Rdata"))
-save(production, file=file.path(savepath, "econ_production.Rdata"))
+save(targeting, file=file.path(savepath, "econ_targeting.RData"))
+save(production, file=file.path(savepath, "econ_production.RData"))
+
+
+##########################
+# BEGIN readin of econometric model of production coefficients 
+##########################
+# read in the estimated coefficients from txt files
+
+# zero out the insignificant ones 
+
+# reshape them to a wide format
+
+# parse the "title" variable to facilitate merging to production dataset
+
+
+# Desired output   
+## 40ish rows (2 gears x2 time periods x 10ish stocks)
+## 3 key columns (post, gearcat, spstock)
+## 29 columns: "real variables", 12 categoricals for months,  12 categoricals for years
+ 
+
+
+
+
+
+
 
 
