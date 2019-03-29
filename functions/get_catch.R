4e304a028bb851060097f2d9040d2af8f5983c36
Jonathan Cummings <jcummings@umassd.edu>
2019-03-29 16:25:35 -0400

    Add a realized catch parameter to the get_catch function and create placeholders for the different realized catch models.

--- functions/get_catch.R
+++ functions/get_catch.R
@@ -8,25 +8,32 @@
 # M: annual natural mortality
 # N: vector of abundance by age (after growth and recruitment)
 # selC: vector of fishery selectivity by age
+# realizedCatchModel: realized catch model option (see set_om_parameters.R)
 
 
-
-get_catch <- function(F_full, M, N, selC){
-  
+get_catch <- function(F_full, M, N, selC, realizedCatchModel){
   if(length(N) != length(selC)){
     stop('length N must be == length sel')
-  }
+  } # end if
   
   # calculate Z
   Z <- selC * F_full + M
+    
+  # Baranov catch eqn to calculate ACL
+  ACL <- (selC * F_full / Z) * N * (1 - exp(-Z))
+    
+  if(realizedCatchModel==1){
+    # "Convert" ACL to catch
+    C<-ACL
+    
+    return(C)
+  } # End realizedCatchModel==1 - ACL
   
-  # Baranov catch eqn
-  C <- (selC * F_full / Z) * N * (1 - exp(-Z))
-  
-  return(C)
-  
+  if(realizedCatchModel==2){} # End realizedCatchModel==2 - Single-species
+  if(realizedCatchModel==3){} # End realizedCatchModel==3 - Multi-species
+  if(realizedCatchModel==4){} # End realizedCatchModel==4 - Adv multi-species
+  if(realizedCatchModel==5){} # End realizedCatchModel==5 - Econ catch shares
+  if(realizedCatchModel==6){} # End realizedCatchModel==6 - Econ non-tradable
+
 }
-
-
-
-
+  
