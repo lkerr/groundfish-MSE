1b2c573bd8819894a6e8ff0e3319ad37be32824a
Jonathan Cummings <jcummings@umassd.edu>
2019-04-02 15:34:07 -0400

    Use regression results to predict catch for realized catch models 1, 2, and 3

--- functions/get_catch.R
+++ functions/get_catch.R
@@ -11,7 +11,7 @@
 # realizedCatchModel: realized catch model option (see set_om_parameters.R)
 
 
-get_catch <- function(F_full, M, N, selC, realizedCatchModel){
+get_catch <- function(F_full, M, N, selC, realizedCatchModel=1){
   if(length(N) != length(selC)){
     stop('length N must be == length sel')
   } # end if
@@ -21,7 +21,8 @@ get_catch <- function(F_full, M, N, selC, realizedCatchModel){
     
   # Baranov catch eqn to calculate ACL
   ACL <- (selC * F_full / Z) * N * (1 - exp(-Z))
-    
+  
+  # 1 - ACL:  Direct 1:1 relationship between ACL and catch, where the catch==F==ACL
   if(realizedCatchModel==1){
     # "Convert" ACL to catch
     C<-ACL
@@ -29,20 +30,31 @@ get_catch <- function(F_full, M, N, selC, realizedCatchModel){
     return(C)
   } # End realizedCatchModel==1 - ACL
   
+  # 2 - Single-species:  Catch for a given species is modeled as a function of the ACL for that species Catch=lm(Catch~ACL)
   if(realizedCatchModel==2){
-    load('data/data_processed/catchHistory/realizedCatchModels.RData') # Not working
+    load('data/data_processed/catchHistory/realizedCatchModels.RData')
     C<-predict(reg_cod,data.frame(ACL_cod=ACL))
+    
+    return(C)
   } # End realizedCatchModel==2 - Single-species
   
+  # 3 - Multi-species:  Catch for a all species is modeled as a function of the ACL for all species Catch[i]=lm(Catch[N]~ACL[N])
   if(realizedCatchModel==3){
-    load("~/data/data_processed/catchHistory/realizedCatchModels.RData")
+    load('data/data_processed/catchHistory/realizedCatchModels.RData')
+    
+    C<-predict(mvreg_cod,data.frame(ACL_cod=ACL))
+    
+    return(C)
   } # End realizedCatchModel==3 - Multi-species
   
+  # 4 - Adv multi-species: Thorson approach to multiple species catch modeling
   if(realizedCatchModel==4){
   } # End realizedCatchModel==4 - Adv multi-species
   
+  # 5 - Econ catch shares: Description needed from Min-Yang or Anna
   if(realizedCatchModel==5){} # End realizedCatchModel==5 - Econ catch shares
   
+  # 6 - Econ non-tradable input control: Description needed from Min-Yang or Anna  
   if(realizedCatchModel==6){} # End realizedCatchModel==6 - Econ non-tradable
 
 }
