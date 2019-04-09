b1a60bf1b4e4cbe0269a6ebe3f777636d7669cd7
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-04-09 10:39:14 -0400

    renamed the function. "get_check_fishery_open" to "get_fishery_next_period."  It's a better name.

---
+++ functions/economic/get_fishery_next_period.R
@@ -0,0 +1,16 @@
+# A function to deal with the end of the year
+# accumulated_catch: catch for that day of all stocks by the fleet
+# fish_stats: data frame containing stock name, logical for open/closed, catch limit, and cumulative catch
+ 
+
+
+get_fishery_next_period <- function(accumulated_catch,fish_stats){
+  
+  fs<-fish_stats[c("spstock2","open","acl")]
+    accumulated_catch<-merge(fs,accumulated_catch, by=c("spstock2"), all.x=TRUE)
+  accumulated_catch$open<-accumulated_catch$cumul_catch<accumulated_catch$acl
+  return(accumulated_catch)
+}
+
+
+
