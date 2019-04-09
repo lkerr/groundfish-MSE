a9ffdfe248019e7b688452d0e29d9906ff5af672
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-04-09 15:58:44 -0400

    better comment.

--- functions/economic/get_fishery_next_period.R
+++ functions/economic/get_fishery_next_period.R
@@ -1,4 +1,4 @@
-# A function to deal with the end of the year
+# A function to deal with the end of the day loop
 # accumulated_catch: catch for that day of all stocks by the fleet
 # fish_stats: data frame containing stock name, logical for open/closed, catch limit, and cumulative catch
  
