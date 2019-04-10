a6d5728d195d10d486a0cb254ffcfdb768c3c962
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-04-10 09:34:23 -0400

    Finish trimming out unnecessary data fields. Verified working.

--- preprocessing/economic/production_targeting_data.R
+++ preprocessing/economic/production_targeting_data.R
@@ -161,10 +161,10 @@ if(ncol(targeting_dataset) !=check){
 
 datavars=c("exp_rev_total","distance","das_charge","fuelprice_distance","start_of_season","crew","price_lb_lag1","mean_wind","mean_wind_2","permitted","lapermit","das_charge_len","max_wind","max_wind_2","fuelprice","fuelprice_len","wkly_crew_wage")
 betavars=paste0("beta_",datavars)
-idvars=c("hullnum2", "date","spstock2", "doffy")
-necessary=c("id", "multiplier", "q", "price_lb_lag1")
+idvars=c("id", "hullnum2", "date","spstock2", "doffy")
+necessary=c( "multiplier", "q", "price_lb_lag1")
 useful=c("gearcat","post","h_hat")
-mysubs=c(idvars,useful,datavars,betavars)
+mysubs=c(idvars,necessary,useful,datavars,betavars)
 
 
 targeting_dataset<-targeting_dataset[mysubs]
