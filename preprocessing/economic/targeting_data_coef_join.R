ba1cb59adb603e8650e7f6ea1c1d24f7dbd30340
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-04-10 10:37:27 -0400

    Split the targeting data, processing from the economic data processing.

---
+++ preprocessing/economic/targeting_data_coef_join.R
@@ -0,0 +1,195 @@
+# This pre-processing file:
+  # 1. Pulls in the production equation coefficients and data
+  # 2. Joins them together (based on spstock2, gearcat, and post).  We may not want to do that.
+
+  # 3. Pulls in the targeting equation coefficients and data
+  # 4. Joins them together. 
+# This is a little stupid, because it make a big dataset, instead of making a pair of matrices (one for data and one for coefficients). But whatever.
+
+rm(list=ls())
+if(!require(readstata13)) {  
+  install.packages("readstata13")
+  require(readstata13)}
+if(!require(tidyr)) {  
+  install.packages("tidyr")
+  require(tidyr)}
+if(!require(dplyr)) {  
+  install.packages("dplyr")
+  require(dplyr)}
+
+
+# file paths for the raw and final directories
+
+rawpath <- 'data/data_raw/econ'
+savepath <- 'data/data_processed/econ'
+
+#Files to read -- sample data for now.
+targeting_source<-"sample_DCdata_fys2009_2010_forML.dta"
+
+#Load in targeting coefficients
+load(file.path(savepath,"targeting_coefs.RData"))
+
+# read in the dataset
+targeting <- read.dta13(file.path(rawpath, targeting_source))
+
+
+targeting$startfy = paste("5", "1",targeting$gffishingyear,sep=".")
+targeting$startfy = as.Date(paste("5", "1",targeting$gffishingyear,sep="."), "%m.%d.%Y")
+targeting$doffy=as.numeric(difftime(targeting$date,targeting$startfy, units="days")+1)
+
+#Follow naming conventions
+targeting$spstock2<-tolower(targeting$spstock)
+targeting$spstock2<- gsub("_","",targeting$spstock2)
+
+colnames(targeting)[colnames(targeting)=="LApermit"] <- "lapermit"
+
+
+
+
+
+
+# merge targeting dataset and coefficients
+# Note, you'll have some NAs for some rows, especially no fish.
+targeting_dataset<-left_join(targeting,targeting_coefs,by=c("gearcat","spstock2"))
+targeting$spstock2<- gsub("gb","GB",targeting$spstock2)
+targeting$spstock2<- gsub("ccgom","CCGOM",targeting$spstock2)
+targeting$spstock2<- gsub("gom","GOM",targeting$spstock2)
+targeting$spstock2<- gsub("snema","SNEMA",targeting$spstock2)
+
+
+
+#I expect the number of columns in targeting dataset to be number in targeting, plus number in targeting coefs, minus 2 (because I had 2 match columns)
+
+check<- ncol(targeting)+ncol(targeting_coefs)-2
+if(ncol(targeting_dataset) !=check){
+  warning("Lost some Columns from the targeting dataset")
+}
+
+
+
+datavars=c("exp_rev_total","distance","das_charge","fuelprice_distance","start_of_season","crew","price_lb_lag1","mean_wind","mean_wind_2","permitted","lapermit","das_charge_len","max_wind","max_wind_2","fuelprice","fuelprice_len","wkly_crew_wage")
+betavars=paste0("beta_",datavars)
+idvars=c("id", "hullnum2", "date","spstock2", "doffy")
+necessary=c( "multiplier", "q", "price_lb_lag1")
+useful=c("gearcat","post","h_hat")
+mysubs=c(idvars,necessary,useful,datavars,betavars)
+
+
+targeting_dataset<-targeting_dataset[mysubs]
+
+
+
+
+save(targeting_dataset, file=file.path(savepath, "full_targeting.RData"))
+
+rm(list=ls())
+
+
+
+
+
+
+
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# production <- read.dta13(file.path(rawpath, production_source))
+# production$startfy = paste("5", "1",production$gffishingyear,sep=".")
+# production$startfy = as.Date(paste("5", "1",production$gffishingyear,sep="."), "%m.%d.%Y")
+# production$doffy=as.numeric(difftime(production$date,production$startfy, units="days")+1)
+# 
+# ###############################################################
+# #THIS IS JUST FOR TESTING, REMOVE THIS LATER.
+# production$gffishingyear <- sample(2004:2015,nrow(production), replace=T)
+# #THIS IS JUST FOR TESTING, REMOVE THIS LATER.
+# ###############################################################                        
+# 
+# #clean up production dataset, expand month and year factor variables.
+# production$gearcat<-tolower(production$gearcat)
+# production$spstock2<-tolower(production$spstock)
+# production$spstock2<- gsub("_","",production$spstock2)
+# 
+# production$spstock2<-tolower(production$spstock)
+# production$spstock2<- gsub("_","",production$spstock2)
+# production$spstock2<- gsub("gb","GB",production$spstock2)
+# production$spstock2<- gsub("ccgom","CCGOM",production$spstock2)
+# production$spstock2<- gsub("gom","GOM",production$spstock2)
+# production$spstock2<- gsub("snema","SNEMA",production$spstock2)
+# 
+# 
+# 
+# mygfyear<-as.data.frame(factor(production$gffishingyear))
+# mymonth<-as.data.frame(factor(production$month))
+# 
+# mymonth<-as.data.frame(model.matrix(~ . + 0, data=mymonth, contrasts.arg = lapply(mymonth, contrasts, contrasts=FALSE)))
+# 
+# mygfyear<-as.data.frame(model.matrix(~ . + 0, data=mygfyear, contrasts.arg = lapply(mygfyear, contrasts, contrasts=FALSE)))
+# 
+# colnames(mymonth)<-paste0("months",1:12)
+# colnames(mygfyear)<-paste0("fy",2004:2015)
+# 
+# production<-cbind(production, mymonth,mygfyear)
+# load(file.path(savepath,"production_coefs.RData"))
+# 
+# # merge production dataset and coefficients
+# production_dataset<-inner_join(production,production_coefs,by=c("gearcat","spstock2","post"))
+# 
+# #do some error checking on the merge
+# # I expect the exact number of rows in "production_dataset" as there are in production. Anything else is a problem.
+# if(nrow(production_dataset) != nrow(production)){
+#   warning("Lost some Rows from the production dataset")
+# }
+# #I expect the number of columns in production dataset to be number in production, plus number in production coefs, minus 3 (because I had 3 match columns)
+# 
+# check<- ncol(production)+ncol(production_coefs)-3
+# if(ncol(production_dataset) !=check){
+#   warning("Lost some Columns from the production dataset")
+# }
+# 
+# 
+# 
+# 
+# #Keep a subset of the variables
+# datavars=c("log_crew","log_trip_days","logh_cumul","primary","secondary")
+# betavars=paste0("beta_",datavars)
+# test<-colnames(production_dataset)
+# 
+# fydums<-test[grepl("^fy20", test)]
+# fycoefs<-test[grepl("^beta_fy20", test)]
+# 
+# monthdums<-test[grepl("^months", test)]
+# monthcoefs<-test[grepl("^beta_month", test)]
+# idvars=c("hullnum2", "id", "date","spstock2", "doffy")
+# necessary=c("multiplier", "q", "rmse", "price_lb_lag1","emean")
+# useful=c("gearcat","post", "logh")
+# mysubs=c(idvars, useful, datavars,betavars, fydums, monthdums, fycoefs, monthcoefs, necessary)
+# 
+# production_dataset<-production_dataset[mysubs]
+# 
+# 
+# 
+# # This is the place where you would drop-out the "post" variable
+# post_only<-production_coefs[which(production_coefs$post==1),]
+# post_production_dataset<-inner_join(production,post_only,by=c("gearcat","spstock2"))
+# 
+# pre_only<-production_coefs[which(production_coefs$post==0),]
+# pre_production_dataset<-inner_join(production,pre_only,by=c("gearcat","spstock2"))
+# 
+# save(production_dataset, file=file.path(savepath, "full_production.RData"))
+# save(post_production_dataset, file=file.path(savepath, "post_production.RData"))
+# save(pre_production_dataset, file=file.path(savepath, "pre_production.RData"))
+# 
+
+
+
