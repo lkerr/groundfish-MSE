ca3a74b567e5d744dfbae4f637c715e161f2153e
Sam Truesdell <struesdell@gmri.org>
2019-05-30 14:46:45 -0400

    run

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -50,7 +50,7 @@ nburn <- 50
 #### Temperature information ####
 
 ## Do you want to include temperature projections (in S-R, growth, etc.)
-useTemp <- FALSE
+useTemp <- TRUE
 
 ## Temperature information ##
 cmip5model <- 'CMCC_CM'
