5e4df197f8874b01f885abe3321388aa62aff34f
Sam Truesdell <struesdell@gmri.org>
2019-05-30 14:02:08 -0400

    run

--- modelParameters/stockParameters/codGBError.R
+++ modelParameters/stockParameters/codGBError.R
@@ -3,7 +3,7 @@
 
 # Average and sd F before the management period begins. Mean on real scale
 # but distribution is lognormal. SD is lognormal SD.                                              
-burnFmsyScalar <- 0.5
+burnFmsyScalar <- 1
 burnFsd <- 0.3
 
 
