c8761ca891b46f2fc18f565d2d7477cabe740dba
Samuel Truesdell <struesdell@neptune.org>
2019-06-14 14:33:14 -0400

    run

--- modelParameters/stockParameters/codGB.R
+++ modelParameters/stockParameters/codGB.R
@@ -105,15 +105,15 @@ R_dev_temp <- TRUE
 #### Error parameters ####
 
 # observation error levels
-oe_sumCW <- 0.05
+oe_sumCW <- 0.25#0.05
 oe_sumCW_typ <- 'lognorm'
-oe_paaCN <- 1000
+oe_paaCN <- 25#1000
 oe_paaCN_typ <- 'multinomial'
-oe_sumIN <- 0.2
+oe_sumIN <- 0.4#0.2
 oe_sumIN_typ <- 'lognorm'
-oe_paaIN <- 1000
+oe_paaIN <- 25#1000
 oe_paaIN_typ <- 'multinomial'
-oe_effort <- 0.01
+oe_effort <- 0.2#0.01
 oe_effort_typ <- 'lognorm'
 
 # process error levels  ###################################  !!!!!!!!!!!!!!
