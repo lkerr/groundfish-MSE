9bceab0870d65d9ac2ed131546db654491065fa7
Min-Yang.Lee <Min-Yang.Lee@noaa.gov>
2019-06-28 09:08:21 -0400

    testing out a two-stock model.

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -23,7 +23,7 @@ mprocTest <- FALSE
 # stockExclude variable. Do not include the extension.R. For example,
 # stockExclude <- haddockGB will leave haddockGB.R out of the analysis.
 # stockExclude <- NULL indludes all stocks.
-stockExclude <- c('haddockGB', 'codGBR0', 'codGBSteep')
+stockExclude <- c('codGB_R0', 'codGB_Steep')
 
 
 #### Structural parameters ####
