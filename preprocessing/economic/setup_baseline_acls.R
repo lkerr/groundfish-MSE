3f15fbf4a32947b4e0102827b3a05cfa7288f1e0
Min-Yang.Lee <Min-Yang.Lee@noaa.gov>
2019-07-02 16:27:08 -0400

    Janky code to read in ACLs.  This should come out of the pre-processing folder. Needs to be run 1x per model run (I think).

--- preprocessing/economic/setup_baseline_acls.R
+++ preprocessing/economic/setup_baseline_acls.R
@@ -8,15 +8,28 @@ savepath <- 'data/data_processed/econ/'
 acls<-"catch_acls.csv" 
 
 # read in the estimated coefficients from txt files
-econ_setup <- read.csv(file.path(savepath,acls), sep=",", header=TRUE,stringsAsFactors=FALSE)
+econ_baseline <- read.csv(file.path(savepath,acls), sep=",", header=TRUE,stringsAsFactors=FALSE)
 
 # For stocks that are not modeled explicitly, we want to set the "fake" catch limit for the sector vessels
 # Equal to "what they have historically used. This means we either use their "levels" or fraction of the total
 
-econ_setup$sector_total_util_frac<-econ_setup$sectorCatch/econ_setup$totalACL
-econ_setup$sector_util_frac<-econ_setup$sectorCatch/econ_setup$sectorACL
+econ_baseline$sector_total_util_frac<-econ_baseline$sectorCatch/econ_baseline$totalACL
+econ_baseline$sector_util_frac<-econ_baseline$sectorCatch/econ_baseline$sectorACL
 
-econ_setup<-econ_setup[c("spstock2","totalACL","sectorACL","otherCatch","sectorCatch","otherACL","sector_total_util_frac","sector_util_frac")]
+econ_baseline<-econ_baseline[c("spstock2","totalACL","sectorACL","otherCatch","sectorCatch","otherACL","sector_total_util_frac","sector_util_frac")]
 
-
-
+####Still need to add "ACLs" for
+# RedSilverOffshoreHake
+# Other
+# SummerFlounder
+# Monkfish
+# SquidMackerelButterfishHerring
+# SeaScallop
+# Skates
+# SpinyDogfish
+#####
+#This needs to be run "at the beginning."
+# Every year, we'll set ACLs as 'default' based on these.  
+# Then we'll pull in from the stock assessments with a merge/overwrite?
+# May be useful to have an inputs and outputs setup, like with the containers 
+# --these are essentially OM parameters
