1cb132f0fd1be13748791d52a4bf84f4b90ed349
Min-Yang.Lee <Min-Yang.Lee@noaa.gov>
2019-07-09 16:08:55 -0400

    Simple option for management advice without error.

--- functions/popdy/get_implementationF.R
+++ functions/popdy/get_implementationF.R
@@ -3,11 +3,12 @@
 get_implementationF <- function(type, stock){
 
   within(stock, {
-    
-  
-    if(type == 'adviceWithError'){
-    
+    if(type == 'advicenoError'){
       if(y < nyear){
+      F_full[y+1]<- F_fullAdvice[y+1]
+      }
+    } else if(type == 'adviceWithError'){
+    if(y < nyear){
         # Borrowed error_idx function from survey function bank
         F_full[y+1] <- get_error_idx(type = ie_typ, 
                                      idx = F_fullAdvice[y+1], 
