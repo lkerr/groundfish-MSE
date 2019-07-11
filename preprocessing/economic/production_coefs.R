a7b9fd5bac6196177d0c0a9a60c1e8b5985dc0c4
Min-Yang.Lee <Min-Yang.Lee@noaa.gov>
2019-07-11 14:17:46 -0400

    split apart importing of production and targeting regressions.

--- preprocessing/economic/production_coefs.R
+++ preprocessing/economic/production_coefs.R
@@ -20,9 +20,7 @@ savepath <- '/data/data_processed/econ'
 rawpath <- 'C:/Users/Min-Yang.Lee/Documents/groundfish-MSE/data/data_raw/econ'
 savepath <- 'C:/Users/Min-Yang.Lee/Documents/groundfish-MSE/data/data_processed/econ'
 
-gillnet_targeting_coef_source<-"nlogit_gillnet_pre_coefs.txt" #(I'll just pull the first GILLNET and FIRST TRAWL coefs)
 
-targeting_coef_source<-"asclogits_forR.txt" #(I'll just pull the first GILLNET and FIRST TRAWL coefs)
 production_coef_pre<-"production_regs_actual_pre_forR.txt"
 production_coef_post<-"production_regs_actual_post_forR.txt"
 
@@ -134,128 +132,6 @@ production_coefs[is.na(production_coefs)] <- 0
 ############ In the data, fill any corresponding NAs also with zeros -- this would mostly be biomass or fishing year
 
 save(production_coefs, file=file.path(savepath, "production_coefs.RData"))
-
-
- nlogit_gillnet <- read.csv(file.path(rawpath,gillnet_targeting_coef_source), sep="\t", header=TRUE,stringsAsFactors=FALSE)
- nlogit_gillnet<-nlogit_gillnet[,-3]
- colnames(nlogit_gillnet)<-c("eq_co","value")
- #parse the combined "eq_co" column
- out <- do.call(rbind,strsplit(as.character(nlogit_gillnet$eq_co),':'))
- nlogit_gillnet<-cbind(out,nlogit_gillnet)
- #Add extra columns for gear and pre/post, just in case
- nlogit_gillnet<-cbind("gillnet","0",nlogit_gillnet)
- colnames(nlogit_gillnet)<-c("gear", "post","equation","variable","eq_co","value")
- 
- nlogit_gillnet$variable<-as.character(nlogit_gillnet$variable)
- nlogit_gillnet$variable[which(nlogit_gillnet$variable=="_cons")]<-"constant"
- nlogit_gillnet$variable2<- paste0("beta_", nlogit_gillnet$variable)
- 
-  #This is a good place to stop until I see the data.
- 
- 
-
-# This is code to pull out ASC logit coefficients 
-# #Repeat for the ASCLogit coefficients (not done yet)
-asc_coefs <- read.csv(file.path(stupid,rawpath,targeting_coef_source), sep="\t", header=TRUE,stringsAsFactors=FALSE)
-asc_coefs<-asc_coefs[-1,]
-
-# I'm just extracting the 1st set of regression results, these are in columns 1:5.
-asc_coefs<-asc_coefs[,1:5]
-
-
-asc_coefs<-zero_out(asc_coefs,thresh)
-asc_coefs<-droppval(asc_coefs)
-
-# fix up X (coef name)
-asc_coefs$X<-tolower(asc_coefs$X)
-
-
-
-asc_coefs$X<-gsub(",","", asc_coefs$X)
-asc_coefs$X<-trimws(asc_coefs$X, which=c("both"))
-
-asc_coefs$X<-gsub("\t)","", asc_coefs$X)
-asc_coefs$X<-gsub("one-day lag","", asc_coefs$X)
-asc_coefs$X<-gsub("\\(deflated\\)","", asc_coefs$X)
-asc_coefs$X<-trimws(asc_coefs$X, which=c("both"))
-
-asc_coefs$X[asc_coefs$X=="price/lb"] <-"price_lb_lag1"
-
-asc_coefs$X[asc_coefs$X=="total expected revenues (expected revenues*multiplier)"] <-"exp_rev_total"
-asc_coefs$X[asc_coefs$X=="distance (in miles) from port to month-specific stock area"] <-"distance"
-asc_coefs$X[asc_coefs$X=="das charge"] <-"das_charge"
-asc_coefs$X[asc_coefs$X=="number of crew"] <-"crew"
-asc_coefs$X[asc_coefs$X=="start of fishing season indicator (months 1 and 2)"] <-"start_of_season"
-asc_coefs$X[asc_coefs$X=="open-access/permit to fish"] <-"permitted"
-asc_coefs$X[asc_coefs$X=="limited-access permit to fish"] <-"lapermit"
-
-asc_coefs$X[asc_coefs$X=="fuel price*distance"] <-"fuelprice_distance"
-asc_coefs$X[asc_coefs$X=="fuel price*vessel length"] <-"fuelprice_len"
-asc_coefs$X[asc_coefs$X=="fuel price"] <-"fuelprice"
-
-asc_coefs$X[asc_coefs$X=="das charge*vessel length"] <-"das_charge_len"
-asc_coefs$X[asc_coefs$X=="max wind speed (m/s)"] <-"max_wind"
-asc_coefs$X[asc_coefs$X=="max wind speed squared"] <-"max_wind_2"
-
-asc_coefs$X[asc_coefs$X=="avg. wind speed (m/s)"] <-"mean_wind"
-asc_coefs$X[asc_coefs$X=="avg. wind speed squared"] <-"mean_wind_2"
-
-
-asc_coefs$X<-gsub("fuel price*vessel length","fuelprice_len ", asc_coefs$X)
-
-
-asc_coefs$X<-gsub("average weekly wages for non-crew work","wkly_crew_wage", asc_coefs$X)
-
-
-
-
-
-stocklist<-c("american_lobster", "cod_gb", "cod_gom", "haddock_gom", "haddock_gb", "monkfish", "other", "pollock", "skates", "spiny_dogfish", "white_hake", "yellowtail_flounder_ccgom","american_plaice_flounder", "red_silver_offshore_hake","redfish","sea_scallop","squid_mackerel_butterfish_herrin","winter_flounder_gb","winter_flounder_gom","witch_flounder","yellowtail_flounder_gb", "yellowtail_flounder_snema","summer_flounder")
-
-
-#Delete this line for the final version 
-#stocklist<-c("american_lobster", "cod_gb", "cod_gom")
-
-asc_coefs$spstock2<-0
-
-for (ws in stocklist){
-  wr<-grep(ws,asc_coefs$X)
-  asc_coefs$spstock2[(wr+1):(wr+3)]<-ws
-  asc_coefs<-asc_coefs[-wr,]
-}
-
-
-ALL<-asc_coefs[which(asc_coefs$spstock2==0),]
-ALL<-ALL[!names(ALL) %in% c("spstock2")]
-rownames(ALL)<-ALL[,1]
-ALL<-ALL[,-1]
-
-ALL<-as.data.frame(t(ALL))
-ALL$gearcat<-rownames(ALL)
-rownames(ALL)<-NULL
-
-
-
-stocks<-asc_coefs[-which(asc_coefs$spstock2==0),]
-stocks<-gather(stocks,'GILLNETS','TRAWL', key="gearcat", value="coef")
-stocks<-spread(stocks,X,coef)
-#transpose and send to dataframe, fix naming, and characters
-
-targeting_coefs<-inner_join(stocks,ALL, by="gearcat")
-
-targeting_coefs$spstock2<- gsub("_","",targeting_coefs$spstock2)
-
-
-colnames(targeting_coefs)[3:ncol(targeting_coefs)]<-paste0("beta_",colnames(targeting_coefs[3:ncol(targeting_coefs)]))
-targeting_coefs$spstock2[which(targeting_coefs$spstock2=="squidmackerelbutterfishherrin")]<-"squidmackerelbutterfishherring"
-
-
-#Force NAs to zero. This is legit. I promise.
-targeting_coefs[is.na(targeting_coefs)]<-0
-
-save(targeting_coefs, file=file.path(savepath, "targeting_coefs.RData"))
-
-
 
 
 
