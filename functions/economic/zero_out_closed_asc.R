c1cee8ce052af68830dd62869336aa003e51cda3
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-15 08:09:23 -0400

    Rename zero_out_closed to zero_out_closed_asc to remind that this is only appropriate for a logit with IIA.  Properly recompute probabilities by setting the non-chosen option to zero and redistributing the probability to all others.

---
+++ functions/economic/zero_out_closed_asc.R
@@ -0,0 +1,30 @@
+# A function to zero out trips that are closed after estimating an ASCLOGIT.
+#
+# tds: working targeting dataset with asclogit coefficients 
+# open_hold: a dataset of hullnum2 and spstock2 with one extra column (open=1 if open and =0 if closed).
+# I need to fix this. After zeroing out, the probabilities must be recomputed.
+#  
+
+
+zero_out_closed_asc <- function(tds,open_hold){
+  tds<-left_join(tds,open_hold, by=c("spstock2"="spstock2"))
+  tds$open[which(tds$spstock2=="nofish")]<-"TRUE"
+  #tds<-merge(tds,open_hold, by=c("spstock2"))
+  tds$prhat[tds$open=="FALSE"]<-0
+  
+  tds<-tds%>% 
+    group_by(id) %>% 
+    mutate(prsum=sum(prhat))
+  
+  tds$prhat<- tds$prhat/tds$prsum
+  
+  return(tds)
+}
+
+# testing
+tds <- tds %>%
+  select(prhat, prhat2,open, everything())
+
+
+#
+tds$open[which(tds$spstock2=="codGB")]<-"FALSE"
