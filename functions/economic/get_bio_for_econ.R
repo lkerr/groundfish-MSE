1f4cca465a7a543811a3207b97f47318c52d5cca
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-17 09:19:15 -0400

    Better comments.

--- functions/economic/get_bio_for_econ.R
+++ functions/economic/get_bio_for_econ.R
@@ -10,7 +10,7 @@ get_bio_for_econ=function(stock,basecase){
   sn<-lapply(X = stock, FUN = `[[`, "stockName")
   
   #This stacks all the the metrics into individual lists
-  ACL<-lapply(X = stock, FUN = `[[`, "ACL")
+  ACL<-lapply(X = stock, FUN = `[[`, "ACL_kg") #This needs to be changed to "ACL" once the containers change.
   ACL<-lapply(X = ACL, FUN = `[`, y)
   
   SSB<-lapply(X = stock, FUN = `[[`, "SSB")
@@ -29,6 +29,7 @@ get_bio_for_econ=function(stock,basecase){
   # Hack the ACL to 1e6 mt if it is null
   ACL<-replace(ACL, sapply(ACL, is.null), 1e6) 
   
+  #Stack these into a dataframe and give the columns good names
   biocontain<-do.call(rbind,lapply(sn,data.frame))
   biocontain<-cbind(c(1:nrow(biocontain)),biocontain)
   biocontain<-cbind(biocontain,do.call(rbind,lapply(trawlsurvey,data.frame)))
@@ -37,10 +38,11 @@ get_bio_for_econ=function(stock,basecase){
   biocontain<-cbind(biocontain,1)
   colnames(biocontain)<-c("stocklist_index","stockName","trawlsurvey","SSB","ACL","bio_model")
   biocontain$stockName<- as.character(biocontain$stockName)
-
+  #Parse the stockName column
   biocontain<-separate(biocontain,stockName,into=c("spstock2","variant"), sep="_", remove=FALSE, fill="right")
   biocontain$spstock2<- as.character(biocontain$spstock2)
   
+  #Pull in baseline ACLs from historical data, mark rows that have a biological model, and compute the sector and non-sector ACLs
   rownames(biocontain)<- c()
   biocontain<-merge(biocontain,basecase,by="spstock2",all=TRUE)
   biocontain<-within(biocontain, ACL[is.na(ACL)] <- baselineACL_mt[is.na(ACL)])
