c829f03c63fd3527e827c4d3cc50c44c644b5896
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-23 12:02:20 -0400

    Add an extra column to the targeting dataset.

--- processes/loadEcon.R
+++ processes/loadEcon.R
@@ -4,10 +4,20 @@ econdatapath <- 'data/data_processed/econ'
 load(file.path(econdatapath,"full_targeting.RData"))
 load(file.path(econdatapath,"full_production.RData"))
 
-
 production_dataset<-production_dataset[which(production_dataset$gffishingyear %in% baseEconYrs),]
 targeting_dataset<-targeting_dataset[which(targeting_dataset$gffishingyear %in% baseEconYrs),]
 
+# Add nchoices column to the targeting dataset
+targeting_dataset<- targeting_dataset   %>%
+  group_by(id) %>%
+  mutate(nchoices=n())
+
+##################for  testing purposes only
+#targeting_dataset$prhat<-targeting_dataset$pr
+############for testing only
+
+
+targeting_dataset<-data.table(targeting_dataset)
 #split the production and targeting datasets into a list of datasets
 production_dataset<-split(production_dataset, production_dataset$doffy)
 targeting_dataset<-split(targeting_dataset, targeting_dataset$doffy)
