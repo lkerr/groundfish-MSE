9d2e1b8adce5f0bef0f2ccf7fb8655e4074fe6a9
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-24 16:24:12 -0400

    comment out the production dataset.

--- processes/loadEcon.R
+++ processes/loadEcon.R
@@ -2,11 +2,8 @@
 
 econdatapath <- 'data/data_processed/econ'
 load(file.path(econdatapath,"full_targeting.RData"))
-load(file.path(econdatapath,"full_production.RData"))
-production_dataset[is.na(production_dataset)]<-0
 targeting_dataset[is.na(targeting_dataset)]<-0
 
-production_dataset<-production_dataset[which(production_dataset$gffishingyear %in% baseEconYrs),]
 targeting_dataset<-targeting_dataset[which(targeting_dataset$gffishingyear %in% baseEconYrs),]
 
 # Add nchoices column to the targeting dataset
@@ -18,7 +15,17 @@ targeting_dataset<- targeting_dataset   %>%
 targeting_dataset$prhat<-targeting_dataset$pr
 
 targeting_dataset<-data.table(targeting_dataset)
-#split the production and targeting datasets into a list of datasets
-production_dataset<-split(production_dataset, production_dataset$doffy)
+#split the targeting dataset into a list of datasets
 targeting_dataset<-split(targeting_dataset, targeting_dataset$doffy)
+
+
+#load(file.path(econdatapath,"full_production.RData"))
+#production_dataset[is.na(production_dataset)]<-0
+#production_dataset<-production_dataset[which(production_dataset$gffishingyear %in% baseEconYrs),]
+#production_dataset<-split(production_dataset, production_dataset$doffy)
+
+
+
+
+
 
