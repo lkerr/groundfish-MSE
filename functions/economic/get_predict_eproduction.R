1cdcfb7c667c876e70e09ec6dc0bc6c0574d32b8
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-25 13:29:09 -0400

    Functions return a data.table with an extra column (skips the need to left_join or cbind).

--- functions/economic/get_predict_eproduction.R
+++ functions/economic/get_predict_eproduction.R
@@ -5,7 +5,7 @@
 # prod_ds: Combined dataset of production data and coefficients recovered 
 # 	from a log-log regression.
 # logh=q+XB, where X is q, log crew, log trip days, log cumulative harvest, primary, secondary, and dummies for month and fishing year.
-# 
+# Returns a data.table with an extra column in it.
 
 get_predict_eproduction <- function(prod_ds){
   #This is the original version, comment the next line out (or remove)
@@ -25,21 +25,17 @@ get_predict_eproduction <- function(prod_ds){
   Z<-as.matrix(prod_ds[, ..datavars])
   A<-as.matrix(prod_ds[,..alphavars])
   
-  prod_ds$logh_hat<-rowSums(Z*A)
-  prod_ds$logh_hat=prod_ds$logh_hat+prod_ds$q
+  prod_ds$harvest_sim<-rowSums(Z*A)
+  prod_ds$harvest_sim=prod_ds$harvest_sim+prod_ds$q
   
   #production
   # bad way to smear
-  #prod_ds$harvest_sim<- exp(prod_ds$logh_hat + fx)*exp((prod_ds$rmse^2)/2)
+  #prod_ds$harvest_sim<- exp(prod_ds$harvest_sim + fx)*exp((prod_ds$rmse^2)/2)
   
   #good way to smear
-  prod_ds$harvest_sim<- (exp(prod_ds$logh_hat))*prod_ds$emean 
+  prod_ds$harvest_sim<- (exp(prod_ds$harvest_sim))*prod_ds$emean 
   
 
-  #selectvars<-c("hullnum", "date", "spstock2","exp_rev_sim","exp_rev_total_sim","harvest_sim")
-  selectvars<-c("hullnum", "date", "spstock2","harvest_sim")
-  
-  prod_out<-prod_ds[,..selectvars]
-  return(prod_out)
+  return(prod_ds)
 }
 
--- functions/economic/get_predict_etargeting.R
+++ functions/economic/get_predict_etargeting.R
@@ -8,6 +8,7 @@
 # D. Compute prob = exp(xb)/total exp(xb)
 
 #.Unsure if the NA are handled properly.  Also unsure if the "No Fish" option will be properly constructed (Troubleshoot: if the utility function equation is fixed AND I get a matrix of NAs, then the utility for the no-fish option isn't properly being constructed. I'm pretty sure that this gets set =0 (so exp(0)=1). There are no coefficients for no fish model.
+# returns a data.table with extra columns in it (xb, expu, totexpu, and prhat).
 
 get_predict_etargeting <- function(tds){
   # pull the betas into matrix, pull the data variables into a matrix and mulitiply them.  
@@ -24,7 +25,7 @@ X<-as.matrix(tds[, ..datavars])
 
 B<-as.matrix(tds[, ..betavars])
 
-tds$xb=rowSums(X*B)
+tds$xb<-rowSums(X*B)
 tds$expu<-exp(tds$xb)
 tds<-as.data.table(tds)
 
