1a67e374142e7454cfa79bb5665d36f3d47338a0
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-07-30 12:01:08 -0400

    Janky code to clean data.

--- preprocessing/economic/targeting_data_coef_join.R
+++ preprocessing/economic/targeting_data_coef_join.R
@@ -25,7 +25,9 @@ econrawpath <- './data/data_raw/econ'
 econsavepath <- './data/data_processed/econ'
 
 #Files to read -- sample data for now.
+#targeting_source<-"sample_PRODREGdata_fys2009_2010_forML.dta"
 targeting_source<-"sample_DCdata_fys2009_2010_forML.dta"
+
 
 #Load in targeting coefficients
 targeting_coefs<-readRDS(file.path(econsavepath,"targeting_coefs.Rds"))
@@ -64,19 +66,24 @@ colnames(targeting)[colnames(targeting)=="LApermit"] <- "lapermit"
 # Crew and trip_days is coded as zero for no-fish. We will set these to NA.
 # trawl_survey_weight is coded as NA if it's missing. Nothing to do here.
 
-targeting$crew[targeting$crew==0]<-NA
-targeting$trip_days[targeting$trip_days==0]<-NA
+#targeting$crew[targeting$crew==0]<-NA
+#targeting$trip_days[targeting$trip_days==0]<-NA
 
 # Take Logs of these three
-targeting$log_crew<-log(targeting$crew)
-targeting$log_trip_days<-log(targeting$trip_days)
-targeting$log_trawl_survey_weight<-log(targeting$trawl_survey_weight)
+# targeting$log_crew<-log(targeting$crew)
+# targeting$log_trip_days<-log(targeting$trip_days)
+# targeting$log_trawl_survey_weight<-log(targeting$trawl_survey_weight)
 
 # Set the log-values to zero.This is kludgy 
 
 targeting$log_crew[is.na(targeting$log_crew)]<-0
 targeting$log_trip_days[is.na(targeting$log_trip_days)]<-0
 targeting$log_trawl_survey_weight[is.na(targeting$log_trawl_survey_weight)]<-0
+
+#handle the no fish option
+targeting_dataset$log_avg_crew[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$log_trip_days[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$log_crew[which(targeting_dataset$spstock2=="nofish")]<-0
 
 
 targeting$primary<-1
@@ -106,9 +113,11 @@ if(count_rows_post !=(count_rows)){
   warning("Lost some Rows from the production-join")
 }
 
-
 # merge targeting dataset and coefficients
 # Note, you'll have some NAs for some rows, especially no fish.
+# Currently, I only have targeting coefficients for the pre-period.
+#targeting_dataset<-left_join(targeting,targeting_coefs,by=c("gearcat","spstock2","post"))
+
 targeting_dataset<-left_join(targeting,targeting_coefs,by=c("gearcat","spstock2"))
 cols_final2<-ncol(targeting_dataset)
 count_rows_post2<-nrow(targeting_dataset)
@@ -161,7 +170,7 @@ targeting_dataset$fy2013<-as.numeric(targeting_dataset$gffishingyear==2013)
 targeting_dataset$fy2014<-as.numeric(targeting_dataset$gffishingyear==2014)
 targeting_dataset$fy2015<-as.numeric(targeting_dataset$gffishingyear==2015)
 
-
+#there will be na values here because the nofish option is full of nas.  push these to zeros.
 
 targeting_dataset[is.na(targeting_dataset)]<-0
 
@@ -187,16 +196,42 @@ targeting_dataset<-as.data.table(targeting_dataset)
 
 keycols<-c("gffishingyear","date", "hullnum", "id","spstock2")
 setorderv(targeting_dataset, keycols)
-saveRDS(targeting_dataset, file=file.path(econsavepath, "giant_targeting.Rds"))
 
+spstock_equation=c("exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
+choice_equation=c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len", "das_price_mean", "das_price_mean_len")
 
+targeting_vars=c(spstock_equation, choice_equation)
 
-targeting_vars=c("exp_rev_total","das_charge","fuelprice_distance","mean_wind","mean_wind_noreast","permitted","lapermit","distance","wkly_crew_wage","len","fuelprice","fuelprice_len","start_of_season","partial_closure","constant")
 betavars=paste0("beta_",targeting_vars)
+betavars=c(betavars,"beta_constant")
 
-production_vars=c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight")
+#stop here. Assert that all the targeting and beta variables are zero for spstock2=="nofish"
+test<-targeting_dataset[which(spstock2=="nofish")]
+#table gffishingyear
+# we will convert all the RHS variables and corresponding coefficients to zero for spstock=="nofish". This will force the eventual prediction of xb=0 and exp(xb)=1. This seems a little silly, but will be faster than doing setting them to 1 later on in the loop.
+#also, you should really have written an lapply here.so lame
+
+targeting_dataset$exp_rev_total[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$fuelprice_distance[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$distance[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$mean_wind[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$mean_wind_noreast[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$permitted[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$choice_prev_fish[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$partial_closure[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$start_of_season[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$wkly_crew_wage[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$len[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$fuelprice[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$fuelprice_len[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$das_price_mean[which(targeting_dataset$spstock2=="nofish")]<-0
+targeting_dataset$das_price_mean_len[which(targeting_dataset$spstock2=="nofish")]<-0
+
+
+
+
+production_vars=c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","constant")
 alphavars=paste0("alpha_",production_vars)
-alphavars=c(alphavars,"alpha_constant")
 
 td_cols<-colnames(targeting_dataset)
 
@@ -209,16 +244,18 @@ monthcoefs<-td_cols[grepl("^alpha_month", td_cols)]
 
 
 idvars=c("id", "hullnum", "date","spstock2", "doffy")
-necessary=c("multiplier", "q", "gffishingyear", "emean","price_lb_lag1", "nchoices", "idflag")
-useful=c("gearcat","post","h_hat","pr")
+necessary=c("landing_multiplier_dollars", "catch_multiplier_dollars", "q", "gffishingyear", "emean","price_lb_lag1", "nchoices", "idflag")
+useful=c("gearcat","post","h_hat","pr","choice")
 mysubs=c(idvars,necessary,useful,fydums, monthdums, fycoefs, monthcoefs, targeting_vars,betavars,production_vars, alphavars)
 #mysubs=c(idvars,necessary,useful,targeting_vars,betavars)
+#mysubs=c(idvars,production_vars, alphavars, fydums, monthdums, fycoefs, monthcoefs,"q", "emean")
 
 
 
 targeting_dataset<-targeting_dataset[, ..mysubs]
 #The targeting_dataset should have no NA values
 sum(is.na(targeting_dataset))==0
+
 # If that fails, use this to figure out which colSums(is.na(targeting_dataset))
 keycols<-c("gffishingyear","date", "hullnum", "id","spstock2")
 setorderv(targeting_dataset, keycols)
@@ -226,7 +263,7 @@ setorderv(targeting_dataset, keycols)
 
 saveRDS(targeting_dataset, file=file.path(econsavepath, "full_targeting.Rds"))
 
-  print(unique(warnings()))
+#  print(unique(warnings()))
 
 #rm(list=ls())
 
