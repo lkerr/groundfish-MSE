b826afb0199cff6e6d34777dbc837996021fa5ce
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-08-23 12:47:08 -0400

    Speedups.

--- functions/economic/get_joint_production.R
+++ functions/economic/get_joint_production.R
@@ -1,5 +1,3 @@
-
-
 # A function to handle jointness in production (technical interactions) 
 #
 # Inputs
@@ -9,14 +7,16 @@
 # a modified version of the wt data table withextra/overwritten columns
 # the c_ columns start as "catch multipliers", but end up as "catch"
 # the l_ columns start as "landings multipliers", but end up as "landings"
+# The p_columns start as "lag prices", and end up as "lag revenue ==expected revenue, by stock"
+# The q_columns start as "quota prices", and end up as "quota costs, by stock"
+# the r_columns start as "prices", and end up as "actual revenue, by stock"
 # exp_rev_total is updated as sum lag_price*landings minus sum quota_price*catch
 # actual_rev_total is updated as sum price*landings minus sum quota_price*catch
 
 
 
 get_joint_production <- function(wt,spstock_names){
-  
-  
+
   catches<-paste0("c_",spstock_names)
   landings<-paste0("l_",spstock_names)
   
@@ -24,40 +24,51 @@ get_joint_production <- function(wt,spstock_names){
   lagp<-paste0("p_",spstock_names)
   prices<-paste0("r_",spstock_names)
   
-  
-  #overwrite the c_ multiplier with catch in pounds
-  for(j in catches){
-    set(wt, i=NULL, j=j, value=wt[[j]]*wt[['harvest_sim']])
+  #overwrite the c_ multiplier and the l_multipliers with catch and landings in pounds
+  # overwrite the quotaprices with quotacost
+  # overwrite the lagp with expected revenue
+  #overwrite the prices with actual revenue
+  for(idx in 1:length(spstock_names)){
+    set(wt, i = NULL, j = catches[idx], value = wt[[catches[idx]]]*wt[['harvest_sim']])
+    set(wt, i = NULL, j = landings[idx], value = wt[[landings[idx]]]*wt[['harvest_sim']])
+    
+    set(wt, i = NULL, j = quotaprices[idx], value = wt[[quotaprices[idx]]]*wt[[catches[idx]]])
+    set(wt, i = NULL, j = lagp[idx], value = wt[[lagp[idx]]]*wt[[landings[idx]]])
+    set(wt, i = NULL, j = prices[idx], value = wt[[prices[idx]]]*wt[[landings[idx]]])
   }
-  #overwrite the l_ multiplier with landings in pounds
-  for(j in landings){
-    set(wt, i=NULL, j=j, value=wt[[j]]*wt[['harvest_sim']])
-  }
   
-  #compute quota costs of a choice  
-  Z<-as.matrix(wt[, ..quotaprices])
-  A<-as.matrix(wt[,..catches])
-  wt[, quota_cost:=rowSums(Z*A)]
-  
-  #compute total expected revenues in 1000s of dollars
-  Z<-as.matrix(wt[, ..landings])
-  A<-as.matrix(wt[,..lagp])
-  A<-Z*A
-  
-  wt[, exp_rev_total:=(rowSums(A)-quota_cost)]
-  #wt$exp_rev_total<-(rowSums(A)-wt$quota_cost)/1000
-  #Right here, you could replace the columns (p_*) of wt with the columns of A (also p_*) if you wanted to save expected revenue by species
-  
-  
-  #compute actual revenues
-  A<-as.matrix(wt[,..prices])
-  A<-Z*A
-  wt[, actual_rev_total:=(rowSums(A)-quota_cost)]
-  
-  #wt$actual_rev_total<-(rowSums(A)-wt$quota_cost)
-  #Right here, you could replace the columns (r_*) of wt with the columns of A (also r_*) if you wanted to save actual revenue by species
   
 
+
+  ##compute quota costs, actual revenue, and expected revenue   
+  ##Assemble formulas for
+  
+  my.erformula<-NULL 
+  my.arformula<-NULL 
+  my.qformula<-NULL 
+  
+  for(idx in 1:length(spstock_names)){
+    my.erformula<- paste0(my.erformula,"+",lagp[idx],"+")
+    my.arformula<- paste0(my.arformula,"+",prices[idx],"+")
+    my.qformula<- paste0(my.qformula,"+",quotaprices[idx],"+")
+    
+  }
+  my.erformula<-substr(my.erformula,1,nchar(my.qformula)-1)
+  my.arformula<-substr(my.arformula,1,nchar(my.arformula)-1)
+  my.qformula<-substr(my.qformula,1,nchar(my.qformula)-1)
+  
+  my.erformula<-paste0(my.erformula,"-quota_cost")
+  my.arformula<-paste0(my.arformula,"-quota_cost")
+  
+  my.erformula<-str2lang(my.erformula)
+  my.arformula<-str2lang(my.arformula)
+  my.qformula<-str2lang(my.qformula)
+  
+  wt[, quota_cost:=eval(my.qformula)]
+  wt[, exp_rev_total:=eval(my.erformula)]
+  wt[, actual_rev_total:=eval(my.arformula)]
+  
   return(wt)
+  
 }
 
