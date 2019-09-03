2bf574856ee94f0e50c3e0789888e6b25d5cd4a7
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-09-03 15:31:01 -0400

    more changes to the way data gets loaded in.

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -96,9 +96,10 @@ pounds_per_kg<-2.20462
 kg_per_mt<-1000
 # Set the economic years that we'll use for simulation.  Right now, we'll pass  in 1 year.
 
-#baseEconYr<-2015
+first_econ_yr<-2010
+last_econ_yr<-2015
+
 econdatapath <- 'data/data_processed/econ'
-econdatafile<-paste0("full_targeting_datalist.Rds")
 
 
 #multiplier_loc<-"sim_multipliers_pre.Rds"
@@ -107,15 +108,17 @@ output_price_loc<-"sim_prices_post.Rds"
 input_price_loc<-"sim_post_vessel_stock_prices.Rds"
 
 
+##############Stocks in the Economic Model #############################
 spstock2s<-c("americanlobster","americanplaiceflounder","codGB","codGOM","haddockGB","haddockGOM","monkfish", "other","pollock","redsilveroffshorehake","redfish","seascallop","skates","spinydogfish","squidmackerelbutterfishherring","summerflounder","whitehake","winterflounderGB","winterflounderGOM","witchflounder","yellowtailflounderCCGOM", "yellowtailflounderGB","yellowtailflounderSNEMA")
 
-
+##############Independent variables in the targeting equation ##########################
 spstock_equation=c("exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
 #choice_equation=c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len", "das_price_mean", "das_price_mean_len")
 choice_equation=c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len")
 
-
+##############Independent variables in the Production equation ##########################
 production_vars=c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","constant")
+
 
 
 #### Output ####
