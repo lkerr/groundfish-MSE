ab302b02d685d0740238d9f379d36842cdf0b563
Ashley Weston <aweston@neptune.gmri.org>
2019-09-06 10:37:27 -0400

    Scenario 2 for LK

--- modelParameters/stockParameters/codGB_Error.R
+++ modelParameters/stockParameters/codGB_Error.R
@@ -97,7 +97,7 @@ pe_R <- 5.839408e-01
 # implementation error of fishing mortality
 ie_F <- 0
 ie_typ <- 'lognorm'
-ie_bias <- 0 #-0.1 # % bias in implementation error
+ie_bias <- -0.13 #0 #-0.1 # % bias in implementation error
 
 # Observation bias (1 is no bias, 0.9 is a -10% bias, etc.)
 ob_sumCW <- 1
