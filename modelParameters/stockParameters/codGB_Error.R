9dd31dc5b84cc9964954ba4bea1481f77afebec9
Lisa Kerr <lkerr@neptune.org>
2019-09-06 11:53:27 -0400

    Running both MPs with Implementation model

--- modelParameters/stockParameters/codGB_Error.R
+++ modelParameters/stockParameters/codGB_Error.R
@@ -3,7 +3,7 @@
 
 # Average and sd F before the management period begins. Mean on real scale
 # but distribution is lognormal. SD is lognormal SD.                                              
-burnFmsyScalar <- 2
+burnFmsyScalar <- 4
 burnFsd <- 0.3
 
 
