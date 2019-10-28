49c17bac4b6d05bd011481c9173c61cbab42dc23
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-10-28 15:57:36 -0400

    fix naming for the input data files.

--- processes/setupEconType.R
+++ processes/setupEconType.R
@@ -9,26 +9,26 @@ myvars<-c("LandZero","CatchZero","EconType","EconData")
 econtype<-econtype[myvars]
 econ_data_stub<-econtype["EconData"]
 if (econ_data_stub=='validation'){
-  multiplier_loc<-"sim_multipliers_post.Rds"
-  output_price_loc<-"sim_prices_post.Rds"
-  input_price_loc<-"sim_post_vessel_stock_prices.Rds"
+  multiplier_loc<-"sim_multipliers_post_valid.Rds"
+  output_price_loc<-"output_prices_post_valid.Rds"
+  input_price_loc<-"input_prices_post_valid.Rds"
   production_vars<- production_vars_post
   spstock_equation<-spstock_equation_pre
   choice_equation<-choice_equation_pre
   # Read in Economic Production Data. These are relatively lists of data.tables. So it makes sense to read in once and then list-subset [[]] later on.
   
 } else if (econ_data_stub=='counterfactual'){
-  multiplier_loc<-"sim_multipliers_pre.Rds"
-  output_price_loc<-"sim_prices_post.Rds"
-  input_price_loc<-"sim_post_vessel_stock_prices.Rds"
+  multiplier_loc<-"sim_multipliers_pre_CF.Rds"
+  output_price_loc<-"output_prices_post_CF.Rds"
+  input_price_loc<-"input_prices_post_CF.Rds"
   production_vars<-production_vars_pre
   spstock_equation<-spstock_equation_pre
   choice_equation<-choice_equation_pre
   
 } else if (econ_data_stub=='full_targeting'){
-  multiplier_loc<-"sim_multipliers_post.Rds"
-  output_price_loc<-"sim_prices_post.Rds"
-  input_price_loc<-"sim_post_vessel_stock_prices.Rds"
+  multiplier_loc<-"sim_multipliers_post_MSE.Rds"
+  output_price_loc<-"output_prices_post_MSE.Rds"
+  input_price_loc<-"input_prices_post_valid.Rds"
   production_vars<-production_vars_post
   spstock_equation<-spstock_equation_pre
   choice_equation<-choice_equation_pre
