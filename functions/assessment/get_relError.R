6dc89a4de66f076e778ab4763a298cb773fa09bd
Ashley Weston <aweston@neptune.gmri.org>
2019-11-21 12:15:12 -0500

    percent relative error updates

--- functions/assessment/get_relError.R
+++ functions/assessment/get_relError.R
@@ -23,18 +23,18 @@ get_relError <- function(stock){
     } 
     
     if(mproc[m,'ASSESSCLASS'] == 'ASAP' & y > fmyearIdx-1){
-      relE_SSB[y-1] <- mean(get_relE(SSBhat, get_dwindow(SSB, sty, y-1)))
-      relE_CW[y-1] <- mean(get_relE(res$catch.pred, get_dwindow(sumCW, sty, y-1)))
-      relE_IN[y-1] <- mean(get_relE(res$index.pred$ind01, get_dwindow(sumIN, sty, y-1)))
-      relE_qI[y-1] = get_relE(log(res$q.indices), log(qI))
-      relE_qC[y-1] = get_relE(NA, log(qC))                           #AEW?
-      relE_selCs0[y-1] = get_relE(res$sel.input.mats$index.sel.ini[10,1], log(selC['s0'])) #check
-      relE_selCs1[y-1] = get_relE(res$sel.input.mats$index.sel.ini[11,1], log(selC['s1'])) #check  
-      relE_ipop_mean[y-1] = get_relE(NA, log_ipop_mean)       #??
-      relE_ipop_dev[y-1] = mean(get_relE(NA, ipop_dev))               #??
-      relE_R_dev[y-1] = mean(get_relE(res$SR.resids$logR.dev, log(R_dev)))
-      relE_R[y-1] <- mean(get_relE(res$SR.resids$recruits, get_dwindow(R, sty, y-1))) 
-      relE_F[y-1] <- mean(get_relE(res$F.report, get_dwindow(F_full, sty, y-1))) 
+      relE_SSB[y] <- get_relE(tail(res$SSB,1), SSB[y])
+      relE_CW[y] <- get_relE(res$catch.pred[37], sumCW[y])
+      relE_IN[y] <- get_relE(tail(res$index.pred$ind01,1), sumIN[y])
+      relE_qI[y] <- get_relE(log(res$q.indices), log(qI))
+      #relE_qC[y] <- get_relE(NA, log(qC))                           
+      #relE_selCs0[y] <- get_relE(res$sel.input.mats$index.sel.ini[10,1], log(selC['s0'])) #initial not estimates
+      #relE_selCs1[y] <- get_relE(res$sel.input.mats$index.sel.ini[11,1], log(selC['s1'])) #inital not estimates
+      #relE_ipop_mean[y] <- get_relE(NA, log_ipop_mean)       # initial population
+      #relE_ipop_dev[y] <- get_relE(NA, ipop_dev)             #
+      #relE_R_dev[y] <- get_relE(tail(res$SR.resids$logR.dev,1), log(R_dev[y]))
+      relE_R[y] <- get_relE(tail(res$SR.resids$recruits,1), R[y])
+      relE_F[y] <- get_relE(tail(res$F.report,1), F_full[y])
     } 
   })
 
