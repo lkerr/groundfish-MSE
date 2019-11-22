0d1ad71addcae00cfa9b2b3771d6dccadec44154
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2019-11-22 15:38:43 -0500

    set the mprocfile to mprocEcon.csv

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -10,9 +10,9 @@
 simpleTemperature <- FALSE
 
 # Which management procedures csv do you want to read:
-mprocfile<-"mproc.csv"
+#mprocfile<-"mproc.csv"
 #mprocfile<-"mprocTest.csv"
-#mprocfile<-"mprocEcon.csv"
+mprocfile<-"mprocEcon.csv"
 
 #### Stock parameters ####
 
