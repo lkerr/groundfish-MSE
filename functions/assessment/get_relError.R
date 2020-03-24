6607d2cf5c3402d2ea77e907b410b26d44d50c19
Ashley Weston <aweston@neptune.gmri.org>
2020-03-24 15:05:12 -0400

    change how ASAP % relative errors are calculated

--- functions/assessment/get_relError.R
+++ functions/assessment/get_relError.R
@@ -23,18 +23,21 @@ get_relError <- function(stock){
     } 
     
     if(mproc[m,'ASSESSCLASS'] == 'ASAP' & y > fmyearIdx-1){
-      relE_SSB[y-1] <- get_relE(tail(res$SSB,1), SSB[y-1])
-      relE_CW[y-1] <- get_relE(res$catch.pred[37], sumCW[y-1])
-      relE_IN[y-1] <- get_relE(tail(res$index.pred$ind01,1), sumIN[y-1])
+      # terminal year
+      # relE_SSB[y-1] <- get_relE(tail(res$SSB,1), SSB[y])
+      # relE_CW[y-1] <- get_relE(res$catch.pred[37], sumCW[y-1])
+      # relE_IN[y-1] <- get_relE(tail(res$index.pred$ind01,1), sumIN[y-1])
+      # relE_qI[y-1] <- get_relE(log(res$q.indices), log(qI))
+      # relE_R[y-1] <- get_relE(tail(res$SR.resids$recruits,1), R[y-1])
+      # relE_F[y-1] <- get_relE(tail(res$F.report,1), F_full[y-1])
+      
+      # average over each assessment time series
+      relE_SSB[y-1] <- mean(get_relE(res$SSB, SSB[(y - ncaayear + 1):(y)]))
+      relE_CW[y-1] <- mean(get_relE(res$catch.pred, sumCW[(y - ncaayear):(y-1)]))
+      relE_IN[y-1] <- mean(get_relE(res$index.pred$ind01, sumIN[(y - ncaayear):(y-1)]))
       relE_qI[y-1] <- get_relE(log(res$q.indices), log(qI))
-      #relE_qC[y] <- get_relE(NA, log(qC))                           
-      #relE_selCs0[y] <- get_relE(res$sel.input.mats$index.sel.ini[10,1], log(selC['s0'])) #initial not estimates
-      #relE_selCs1[y] <- get_relE(res$sel.input.mats$index.sel.ini[11,1], log(selC['s1'])) #inital not estimates
-      #relE_ipop_mean[y] <- get_relE(NA, log_ipop_mean)       # initial population
-      #relE_ipop_dev[y] <- get_relE(NA, ipop_dev)             #
-      #relE_R_dev[y] <- get_relE(tail(res$SR.resids$logR.dev,1), log(R_dev[y]))
-      relE_R[y-1] <- get_relE(tail(res$SR.resids$recruits,1), R[y-1])
-      relE_F[y-1] <- get_relE(tail(res$F.report,1), F_full[y-1])
+      relE_R[y-1] <- mean(get_relE(res$SR.resids$recruits, R[(y - ncaayear):(y-1)]))
+      relE_F[y-1] <- mean(get_relE(res$F.report, F_full[(y - ncaayear):(y-1)]))
     } 
   })
 
