a02ab3065ef4f3e565c3bf2debbe152cd9468211
Mackenzie Mazur <mmazur@gmri.org>
2020-07-23 11:31:55 -0400

    Added an error message if the assessment history file does not contain 4 columns

--- functions/assessment/get_HistAssess.R
+++ functions/assessment/get_HistAssess.R
@@ -6,6 +6,9 @@
 get_HistAssess <- function(stock) {
   # read file for the stock with historic assessment information
   assessdat <- read.csv(paste('./data/data_raw/AssessmentHistory/',stockNames[i], '.csv', sep = ''))
+  if(ncol(assessdat) != 4){
+    stop('Check that Assessment History file contains appropriate data.')
+  }
   assess_st_yr <-   fmyearIdx-length(assessdat$Year)
   assessdat$MSEyr <- seq(assess_st_yr, (fmyearIdx-1))
   
