289a6144a75bacb4ffc4347e97760645a5e40454
minyanglee <Min-Yang.Lee@noaa.gov>
2020-08-11 14:46:31 -0400

    working code to test and troubleshoot the production and targeting parts of the simulations.

--- scratch/econscratch/test_predict_etargeting_and_production.R
+++ scratch/econscratch/test_predict_etargeting_and_production.R
@@ -21,29 +21,89 @@ rm(list=ls())
 set.seed(2) 
 
 source('processes/runSetup.R')
-library(microbenchmark)
-#library(Rcpp)
-econsavepath <- 'scratch/econscratch'
-############################################################
-############################################################
-# Pull in temporary dataset that contains economic data clean them up a little (this is temporary cleaning, so it belongs here)
-############################################################
-############################################################
+source('processes/genBaselineACLs_Econonly.R')
 
-load(file.path(econsavepath,"temp_biop.RData"))
-#make sure there is a nofish in bio_params_for_econ
 
-#scratchdir<-"/home/mlee/Documents/projects/GroundfishCOCA/groundfish-MSE/scratch/econscratch"
-#Rcpp::sourceCpp(file.path(scratchdir,"matsums.cpp"))
 
-m<-1
 
-econtype<-mproc[m,]
-myvars<-c("LandZero","CatchZero","EconType")
-econtype<-econtype[myvars]
+top_loop_start<-Sys.time()
+
+
+
+####################These are temporary changes for testing ####################
+mproc_bak<-mproc
+mproc<-mproc_bak[2:13,] #selects validation, counterfactual and counterfactual_single 
+mproc<-mproc_bak[2:5,] #selects validation  
+#mproc<-mproc_bak[17:20,] #selects "validation single"  
+
+nrep<-1
+# yrs contains the calendar years, the calendar year corresponding to y is yrs[y].  we want to go 'indexwise' through the year loop.
+# I want to start the economic model at fmyear=2010 and temporarily end it in 2011
+start_sim<-2010
+end_sim<-2015
+
+fyear<-which(yrs == start_sim)
+nyear<-which(yrs == end_sim)
+
+
+####################End Temporary changes for testing ####################
+
+
+#set the rng state based on system time.  Store the random state.  
+
+#set the rng state based on system time.  Store the random state.  
+# if we use a plain old date (seconds since Jan 1, 1970), the number is actually too large, but we can just rebase to seconds since Jan 1, 2018.
+
+start<-Sys.time()-as.POSIXct("2018-01-01 00:00:00",tz="","%Y-%m-%d %H:%M:%S")
+start<-as.double(start)*100
+set.seed(start)
+
+oldseed_ALL <- .Random.seed
+showProgBar<-TRUE    
+
+####################End Parameter and storage Setup ####################
+#This depends on mproc, fyear, and nyear. So it should be run *after* it is reset. I could be put in the runSetup.R script. But since I'm  adjusting fyear and nyear temporarily, I need it here (for now).
+
+source('processes/setupYearIndexing_Econ.R')
+
+# for(r in 1:nrep){
+# 
+# for(m in 1:nrow(mproc)){
+#   model = mproc$EconData[m]
+#   
+#   
+#   tchars<-nchar(model)
+#   modelno<-substr(model,tchars,tchars)
+#   
+#   manage_counter<-0
+  
+r<-1
+m<-4
+
+
+  source('processes/setupEconType.R')
+
+y<-fyear
+
+source('processes/withinYearAdmin.R')
+
+
+
+source('processes/loadEcon2.R')
+
+# subset just the current year of regulations
+econ_baseline_yearly<-econ_baseline[gffishingyear==yrs[y]]
+bio_params_for_econ <- get_bio_for_econ_only(stock,econ_baseline_yearly)
+
+
+#Instead of doing this line, you'll want to copy/paste pieces from it and run them by hand.
+# source('processes/runEcon_moduleonly.R')
+
 ############################################################
 ############################################################
 # Not sure if there's a cleaner place to put this.  This sets up a container data.frame for each year of the economic model. 
+
+### Probably need to add the trawl survey (trawlsurvey) index here and then push over trawl survey values into the targeting dataset.  But you might do that in outside this function.
 ############################################################
 ############################################################
 
@@ -53,81 +113,60 @@ fishery_holder$stockarea_open<-as.logical("TRUE")
 fishery_holder$cumul_catch_pounds<-0
 fishery_holder$targeted<-0
 
-revenue_holder<-as.list(NULL)
-#source('processes/loadEcon.R')
+#set up a list to hold the expected revenue by date, hullnum, and target spstock2
+annual_revenue_holder<-list()
+
+#set up a list to hold the date, spstock2, and aggregate metrics, like open/closed status and cumulative catch
+annual_fishery_status_holder<-list()
 
 
-readin<-0
-
-merge.prod<-0
-construct.months<-0
-predict.production<-0
-merge.inputs<-0
-merge.outputs<-0
-merge.multipliers<-0
-cleanup1<-0
-cleanup2<-0
-joint.product<-0
-e.targeting<-0
-
-top_loop_start<-Sys.time()
-
-myyear<-2015
-mydraw<-myyear-2009
-
-
-econdatafile<-paste0(econ_data_stub,myyear,".Rds")
-
-targeting_dataset<-readRDS(file.path(econdatapath,econdatafile))
+#Initialize the trips data.table. 
+  if(y == fmyearIdx){
+  keepcols<-c("hullnum","spstock2","choice_prev_fish")
+  trips<-copy(targeting_dataset[[1]])
+  trips<-trips[, ..keepcols]
+  trips<-trips[spstock2!="nofish"]
+  colnames(trips)[3]<-"targeted"
+  trips<-trips[targeted==1]
+  
+  }
 
 
 
-
-
-
-
-day<-1
-
-
-
-
-#Initialize the trips data.table. I need it to store the previous choice.
-keepcols<-c("hullnum","spstock2","choice_prev_fish")
-trips<-copy(targeting_dataset[[day]])
-trips<-trips[, ..keepcols]
-trips<-trips[spstock2!="nofish"]
-colnames(trips)[3]<-"targeted"
-
-
-
+day<-10
 
 set.seed(2)
-day<-100
+#day<-100
 
-start<-Sys.time()
 working_targeting<-copy(targeting_dataset[[day]])
-readin<-readin+Sys.time()-start
+working_targeting$OG_choice_prev_fish<-working_targeting$choice_prev_fish
 
-  start<-Sys.time()
-  working_targeting<-get_predict_eproduction(working_targeting)
-  working_targeting$harvest_sim[working_targeting$spstock2=="nofish"]<-0
+working_targeting<-get_predict_eproduction(working_targeting)
   
   
-  working_targeting[, delta:=abs(harvest_sim-h_hat)]
+working_targeting[spstock2=="nofish", harvest_sim:=0L]
+working_targeting[, delta:=harvest_sim-h_hat]
+
+
+
   summary(working_targeting$delta)
   setcolorder(working_targeting, c("delta", "harvest_sim","h_hat"))
   setorder(working_targeting, -delta)
   
   #alphass<-grep("^alpha",colnames(working_targeting) , value=TRUE)
 
-  predict.production<-predict.production+Sys.time()-start
-  
-  
-  # Keep or update choice_prev_fish
-  # 
-  # working_targeting[, choice_prev_fish:=0]
-  # working_targeting<-working_targeting[trips, choice_prev_fish:=targeted , on=c("hullnum","spstock2")]
-  # 
+
+    # Keep or update choice_prev_fish
+     # working_targeting[trips, `:=` (tx2=targeted, tx=targeted) , on=c("hullnum","spstock2")]
+     # 
+     # working_targeting[is.na(tx2), tx2 := 0]     
+     # working_targeting[is.na(tx), tx := choice_prev_fish]     
+     # working_targeting[, ttx := sum(tx),by=id]
+     # working_targeting[ttx>=2, tx := tx2]
+     # working_targeting[, choice_prev_fish :=tx]
+     # 
+     # dropcol<-c('ttx', 'tx2', 'tx')
+     # working_targeting[, (dropcol) := NULL]
   
   
   #zero_out_targets will set the catch and landings multipliers to zero depending on the value of underACL, stockarea_open, and mproc$EconType
@@ -136,22 +175,39 @@ readin<-readin+Sys.time()-start
 
   
   
-  working_targeting<-joint_adjust_allocated_mults(working_targeting,fishery_holder, econtype)
-  working_targeting<-joint_adjust_others(working_targeting,fishery_holder, econtype)
+    working_targeting<-joint_adjust_allocated_mults(working_targeting,fishery_holder, econtype)
+    working_targeting<-joint_adjust_others(working_targeting,fishery_holder, econtype)
+  #TESTED TO HERE.
+  # In testing, we have to zero out the dl_ variables
+    
+    
+  dl_vars<-grep("^dl_",colnames(working_targeting) , value=TRUE)
+  working_targeting[,(dl_vars):=NA]
+    
   working_targeting<-get_joint_production(working_targeting,spstock2s) 
   working_targeting[, exp_rev_total:=exp_rev_total/1000]
-  working_targeting$exp_rev_total[working_targeting$spstock2=="nofish"]<-0
+  working_targeting[spstock2=="nofish", exp_rev_total:=0L]
   working_targeting[, deltar:=abs(exp_rev_total-exp_rev_bak)]
   summary(working_targeting$deltar)
   setcolorder(working_targeting, c("deltar", "exp_rev_total", "exp_rev_bak"))
-  
+  setorder(working_targeting,-deltar)
   joint.product<-joint.product+Sys.time()-start
   
   
-  start<-Sys.time()
+    
+  
   
   # Predict targeting
   trips<-get_predict_etargeting(working_targeting)
+  
+  trips[, deltapr:=abs(prhat-pr_hat_2)]
+  trips[, relpr:=deltapr/pr_hat_2]
+  
+  trips[, deltaxb:=abs(xb-xb_hat_2)]
+  setcolorder(trips, c("deltapr", "relpr","deltaxb", "xb", "xb_hat_2", "prhat","pr_hat_2"))
+  setorder(trips,-relpr)
+  
+  
   e.targeting<-e.targeting+Sys.time()-start
   
   # Predict targeting
@@ -195,35 +251,4 @@ readin<-readin+Sys.time()-start
   revenue_holder[[day]]<-trips[, ..savelist]
   cleanup2<-cleanup2+Sys.time()-start
   
-}
 
-top_loop_end<-Sys.time()
-big_loop<-top_loop_end-top_loop_start
-
-
-
-readin
-
-merge.prod
-construct.months
-predict.production
-merge.inputs
-merge.outputs
-merge.multipliers
-cleanup1
-cleanup2
-joint.product
-e.targeting
-big_loop
-
-fishery_holder$removals_mt<-fishery_holder$cumul_catch_pounds/(pounds_per_kg*kg_per_mt)+fishery_holder$nonsector_catch_mt
-
-
-
-# 
-#   trips2<-trips[spstock2!="nofish"]
-# 
-#   working_targeting<-copy(targeting_dataset[[2]])
-#   working_targeting[trips2, choice_prev_fish2:=targeted , on=c("hullnum","spstock2")]
-#   setcolorder(working_targeting, c("hullnum", "spstock2", "choice_prev_fish","choice_prev_fish2"))
-#   
