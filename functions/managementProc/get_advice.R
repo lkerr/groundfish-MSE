7498dc99733737c3ab6f48a4cb1d652205f2a895
Mackenzie Mazur <mmazur@gmri.org>
2020-08-13 15:28:52 -0400

    Only run CAA if assessment type is CAA

--- functions/managementProc/get_advice.R
+++ functions/managementProc/get_advice.R
@@ -8,7 +8,8 @@ get_advice <- function(stock){
   
   #### Run assessment model ####
   # Run the CAA assessment
-  tempStock <- get_caa(stock = tempStock)
+  if(mproc[m,'ASSESSCLASS'] == 'CAA'){
+  tempStock <- get_caa(stock = tempStock)}
   
   # Run the PlanB assessment
   tempStock <- get_planB(stock = tempStock)
@@ -36,29 +37,6 @@ get_advice <- function(stock){
       SSBaa <- rep$J1N * get_dwindow(waa, sty, y-1) * get_dwindow(mat, sty, y-1)
       SSBhat <- apply(SSBaa, 1, sum)
     }
-    
-    if(mproc[m,'rhoadjust'] == 'TRUE'){
-      dSSB<-as.data.frame(tempStock$SSBhat)
-      dSSB$Year<-(y-(stock$ncaayear-1)):y
-      colnames(dSSB)<-c('SSB','Year')
-      tempStock<-within(tempStock,{
-        assign(paste('SSB',y,sep=''),dSSB)
-        if(y > 167){
-          peels<-y-167
-          if(peels>7){peels<-7}
-          for (p in (y-peels):(y-1)){
-            SSBold<-get(paste('SSB',p,sep=''))$SSB[get(paste('SSB',p,sep=''))$Year==p]
-            SSBnew<-get(paste('SSB',y,sep=''))$SSB[get(paste('SSB',p,sep=''))$Year==p]
-            assign(paste('rho',p,sep=''),(SSBold-SSBnew)/SSBnew)
-          }
-          plist <- mget(paste('rho',(y-peels):(y-1),sep=''))
-          pcols <- do.call('cbind', plist)
-          MohnsRho <- rowSums(pcols) / peels
-          SSBhat[length(SSBhat)]<-SSBhat[length(SSBhat)]/(MohnsRho+1)
-        }
-      })
-    })
-    
     
     # Vary the parpop depending on the type of assessment model
     # (can't have just one because one of the models might not
@@ -102,7 +80,27 @@ get_advice <- function(stock){
       })
     }
     
-  
+    if(mproc[m,'rhoadjust'] == 'TRUE'){
+      dSSB<-as.data.frame(tempStock$SSBhat)
+      dSSB$Year<-(y-(stock$ncaayear-1)):y
+      colnames(dSSB)<-c('SSB','Year')
+      tempStock<-within(tempStock,{
+        assign(paste('SSB',y,sep=''),dSSB)
+        if(y > 167){
+          peels<-y-167
+          if(peels>7){peels<-7}
+          for (p in (y-peels):(y-1)){
+            SSBold<-get(paste('SSB',p,sep=''))$SSB[get(paste('SSB',p,sep=''))$Year==p]
+            SSBnew<-get(paste('SSB',y,sep=''))$SSB[get(paste('SSB',p,sep=''))$Year==p]
+            assign(paste('rho',p,sep=''),(SSBold-SSBnew)/SSBnew)
+          }
+          plist <- mget(paste('rho',(y-peels):(y-1),sep=''))
+          pcols <- do.call('cbind', plist)
+          MohnsRho <- rowSums(pcols) / peels
+          SSBhat[length(SSBhat)]<-SSBhat[length(SSBhat)]/(MohnsRho+1)
+        }
+      })
+    })
     # Environmental parameters
     parenv <- list(tempY = temp,
                    Tanom = Tanom,
