cfe76ffb4f875ed12e87b28c9c3e6f6edfafc27d
minyanglee <Min-Yang.Lee@noaa.gov>
2020-08-14 20:08:20 -0400

    Large changes to running the economic model. Change the setup of the table that holds the state dependence.  Update it properly. Merges are fairly small, so we use the data.table::merge instead of the bracket syntax.  Keep the exp_rev_tot and exp_rev_tot_das names across all the models.  Keep extra columns for troubleshooting.

--- processes/runEcon_moduleonly.R
+++ processes/runEcon_moduleonly.R
@@ -30,14 +30,17 @@ annual_revenue_holder<-list()
 annual_fishery_status_holder<-list()
 
 
-#Initialize the trips data.table. 
+#Initialize the most_recent_target data.table. 
+#This could move to preprocessing; I'll need to set one up for the entire simulation dataset (all 6 years)
+# You need to save it as a .RDS and then read.  And you need to figure out what to do with your merge statements In order to keep *all*
+# most_recent_target<-readRDS()
   if(y == fmyearIdx){
   keepcols<-c("hullnum","spstock2","choice_prev_fish")
-  trips<-copy(targeting_dataset[[1]])
-  trips<-trips[, ..keepcols]
-  trips<-trips[spstock2!="nofish"]
-  colnames(trips)[3]<-"targeted"
-  trips<-trips[targeted==1]
+  most_recent_target<-copy(targeting_dataset[[1]])
+  most_recent_target<-most_recent_target[, ..keepcols]
+  most_recent_target<-most_recent_target[spstock2!="nofish"]
+  colnames(most_recent_target)[3]<-"targeted"
+  most_recent_target<-most_recent_target[targeted==1]
   
   }
 
@@ -55,39 +58,41 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
 
 # Uncomment this to use AB's existing h_hats
 #  working_targeting[,harvest_sim :=h_hat]
-    # Keep or update choice_prev_fish
-     working_targeting[trips, `:=` (tx2=targeted, tx=targeted) , on=c("hullnum","spstock2")]
+
+# Update choice_prev_fish
+    working_targeting[most_recent_target, choice_prev_fish:=targeted, on=c("hullnum","spstock2")]
+    working_targeting[is.na(choice_prev_fish), choice_prev_fish := 0]
      
-     working_targeting[is.na(tx2), tx2 := 0]     
-     working_targeting[is.na(tx), tx := choice_prev_fish]     
-     working_targeting[, ttx := sum(tx),by=id]
-     working_targeting[ttx>=2, tx := tx2]
-     working_targeting[, choice_prev_fish :=tx]
+    # working_targeting$choice_prev_fish<-working_targeting$OG_choice_prev_fish
      
-     dropcol<-c('ttx', 'tx2', 'tx')
-     working_targeting[, (dropcol) := NULL]
-     
-     
-    #zero_out_targets will set the catch and landings multipliers to zero depending on the value of underACL, stockarea_open, and mproc$EconType
+    #These functions will set the catch and landings multipliers to zero depending on the value of underACL, stockarea_open, and mproc$EconType
 
 
     working_targeting<-joint_adjust_allocated_mults(working_targeting,fishery_holder, econtype)
     working_targeting<-joint_adjust_others(working_targeting,fishery_holder, econtype)
     working_targeting<-get_joint_production(working_targeting,spstock2s) 
     
-    # if modelno=2, then we need to subtract das costs from exp_rev_total right here.
-    if (modelno=="2"){
+    # adjust for DAS costs.
+    # subtract off das_costs from expected and actual  revenue.
+    # The _das variable is computed exactly the same as the exp_rev_total variable. 
+    # If necessary the DAS and/or QUOTA costs should be set to zero either in the data  
     working_targeting[, exp_rev_total:=exp_rev_total-das_cost]
     working_targeting[, actual_rev_total:=actual_rev_total-das_cost]
-    }
+
+    working_targeting[spstock2=="nofish", exp_rev_total:=0L]
+    working_targeting[spstock2=="nofish", actual_rev_total:=0L]
     
+    #rescale
     working_targeting[, exp_rev_total:=exp_rev_total/1000]
     working_targeting[, actual_rev_total:=actual_rev_total/1000]
-    working_targeting[spstock2=="nofish", exp_rev_total:=0L]
+    #fill the _das variables
+    working_targeting[, exp_rev_total_das:=exp_rev_total]
+    working_targeting[, actual_rev_total_das:=actual_rev_total]
+    
     
 
     
-    # Predict targeting
+  # Predict targeting
   trips<-get_predict_etargeting(working_targeting)
     
     # Predict targeting
@@ -99,7 +104,8 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
   # The probability of selection is equal to prhat
     trips<-get_random_draw_tripsDT(trips)
   #drop out trip that did not fish (they have no landings or catch). 
-
+    #trips<-trips[spstock2!="nofish"]
+    
     catches<-get_reshape_catches(trips)
     landings<-get_reshape_landings(trips)
     
@@ -118,12 +124,11 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
   fishery_holder[, daily_pounds_caught :=NULL]
 
   fishery_holder<-get_fishery_next_period_areaclose(fishery_holder)
-  savelist<-c("id","hullnum","spstock2","doffy","exp_rev_total","actual_rev_total", "gearcat")
+  savelist<-c("id","hullnum","spstock2","doffy","exp_rev_total","exp_rev_total_das","actual_rev_total", "gearcat")
+  savelist<-c("id","hullnum","spstock2","doffy","exp_rev_total","exp_rev_total_das", "actual_rev_total", "gearcat","choice_prev_fish","OG_choice_prev_fish")
   
-  savelist<-c("id","hullnum","spstock2","doffy","exp_rev_total","actual_rev_total", "gearcat","choice_prev_fish","OG_choice_prev_fish")
   mm<-c(grep("^c_",colnames(trips), value=TRUE),grep("^l_",colnames(trips), value=TRUE),grep("^r_",colnames(trips), value=TRUE))
   savelist=c(savelist,mm)
-
   # Save the trip-level and fishery level metrics to the appropriate places
   annual_revenue_holder[[day]]<-trips[, ..savelist]
   
@@ -134,8 +139,22 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
   annual_fishery_status_holder[[day]]$doffy<-day
   
   # prepare the trips data.table for the next iteration
-  trips<-trips[spstock2!="nofish"]
-  trips<-trips[, c("spstock2","hullnum", "targeted")]
+  trips<-trips[, c("spstock2","hullnum", "targeted","choice_prev_fish","OG_choice_prev_fish")]
+ 
+  #merge in the previous value of choice_prev_fished
+  trips<-merge(trips,most_recent_target, by=c("hullnum","targeted"), all=TRUE, suffixes=c("T","MRT"))
+  #overwrite with the previous the target stock is no_fish
+  most_recent_target<-copy(trips)
+  
+  
+  setnames(most_recent_target,"spstock2T","spstock2", skip_absent=TRUE)
+  most_recent_target[spstock2=="nofish", spstock2 :=spstock2MRT]
+  most_recent_target[is.na(spstock2), spstock2 :=spstock2MRT]
+  
+  
+  keepcols<-c("hullnum","spstock2","targeted")
+  most_recent_target<-most_recent_target[, ..keepcols]
+
 }
 
   fishery_holder[, removals_mt:=cumul_catch_pounds/(pounds_per_kg*kg_per_mt)+nonsector_catch_mt]
@@ -146,6 +165,9 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
   annual_revenue_holder$m<-model
   annual_revenue_holder$y<-y
   annual_revenue_holder$year<-yrs[y]
+  #perhaps
+  # annual_revenue_holder<-c(annual_revenue_holder,econtype)
+  
   revenue_holder[[yearitercounter]]<-annual_revenue_holder
   
   rm(annual_revenue_holder)
@@ -156,6 +178,9 @@ working_targeting [, harvest_sim:= ifelse(is.na(dl_primary), harvest_sim, ifelse
   annual_fishery_status_holder$m<-model
   annual_fishery_status_holder$y<-y
   annual_fishery_status_holder$year<-yrs[y]
+  
+  #perhaps
+  # annual_fishery_status_holder<-c(annual_fishery_status_holder,econtype)
   fishery_output_holder[[yearitercounter]]<-annual_fishery_status_holder
   rm(annual_fishery_status_holder)
   
