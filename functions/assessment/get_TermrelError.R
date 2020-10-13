8ca4edeb55070e36cd147fc0b75c30a42099ffd5
Mackenzie Mazur <mmazur@gmri.org>
2020-10-12 23:17:40 -0400

    pause calculation for now

--- functions/assessment/get_TermrelError.R
+++ functions/assessment/get_TermrelError.R
@@ -12,7 +12,7 @@ get_TermrelError <- function(stock){
       relTermE_SSB <- get_relE(tail(res$SSB,1), SSB[y])
       relTermE_CW <- get_relE(res$catch.pred[length(res$catch.pred)], sumCW[y-1])
       relTermE_IN <- get_relE(tail(res$index.pred$ind01,1), sumIN[y-1])
-      relTermE_qI <- get_relE(log(res$q.indices), log(qI))
+      #relTermE_qI <- get_relE(log(res$q.indices), log(qI))
       relTermE_R <- get_relE(tail(res$SR.resids$recruits,1), R[y-1])
       relTermE_F <- get_relE(tail(res$F.report,1), F_full[y-1])
     }
