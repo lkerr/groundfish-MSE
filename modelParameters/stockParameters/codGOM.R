183ef29a7dde179c855202e2cb112ee03e8b14c2
Mackenzie Mazur <mmazur@gmri.org>
2020-10-16 12:40:37 -0400

    Council 2

--- modelParameters/stockParameters/codGOM.R
+++ modelParameters/stockParameters/codGOM.R
@@ -33,12 +33,12 @@ mat_typ <- 'input'
 
 
 # natural mortality
-M <- 0.2
-M_typ <- 'const'
+#M <- 0.2
+#M_typ <- 'const'
 init_M <- 0.2 #same for M = 0.2 and M-ramp scenarios
-#M <- 0.4
-#M_typ <- 'ramp'
-M_mis<-FALSE
+M <- 0.4
+M_typ <- 'ramp'
+M_mis<-TRUE
 M_mis_val<-0.2
 
 # initial numbers at-age parameters
@@ -134,11 +134,11 @@ ie_bias <- 0 # % bias in implementation error (F_Full + F_Full*ie_bias)
 
 
 # Observation bias (1 is no bias, 0.9 is a -10% bias, etc.) (sumCW*ob_sumCW) (range 0.01-1)
-ob_sumCW <- 0.44 #0.44 for bias
+ob_sumCW <- 1 #0.44 for bias
 ob_sumIN <- 1
 
 # catch observation bias (codCW + codCW*C_mult)
-C_mult <- 1.25 #1.25 for bias, 0 for no bias
+C_mult <- 0 #1.25 for bias, 0 for no bias
 
 Change_point2<-FALSE
 Change_point_yr<-2025
