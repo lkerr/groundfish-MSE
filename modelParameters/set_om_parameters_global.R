4a062c083ff61cc7175c520aadd2cdfb8415135b
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2020-10-28 20:02:59 -0400

    add another list to hold prhats

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -195,8 +195,8 @@ yearcounter<-0
 #Set up a list to hold the economic results
 revenue_holder<-list()
 fishery_output_holder<-list()
+fishery_prhat_holder<-list()
 
 #these two lists will hold a vectors that concatenates (r, m, y, calyear, .Random.seed). They should be r*m*y in length.
 begin_rng_holder<-list()
 end_rng_holder<-list()
-
