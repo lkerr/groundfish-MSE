2c2937ee8b32a0f4c3d5ba6151f49627f4601569
Mackenzie Mazur <mmazur@neptune.org>
2020-11-01 08:44:24 -0500

    BaseCase

--- functions/assessment/get_relError.R
+++ functions/assessment/get_relError.R
@@ -32,13 +32,12 @@ get_relError <- function(stock){
       # relE_F[y-1] <- get_relE(tail(res$F.report,1), F_full[y-1])
 
       # average over each assessment time series
-      relE_SSB[y-1] <- mean(get_relE(res$SSB, get_dwindow(SSB, sty, y-1)))
-      # browser()
-      relE_CW[y-1] <- mean(get_relE(res$catch.pred, get_dwindow(sumCW, sty, y-1)))
-      relE_IN[y-1] <- mean(get_relE(res$index.pred$ind01, get_dwindow(sumIN, sty, y-1)))
+      relE_SSB[y-1] <- mean(get_relE(res$SSB, SSB[132:(y-1)]))
+      relE_CW[y-1] <- mean(get_relE(res$catch.pred, sumCW[132:(y-1)]))
+      relE_IN[y-1] <- mean(get_relE(res$index.pred$ind01, sumIN[132:(y-1)]))
       #relE_qI[y-1] <- get_relE(log(res$q.indices), log(qI))
-      relE_R[y-1] <- mean(get_relE(res$N.age[,1], get_dwindow(R, sty, y-1)))
-      relE_F[y-1] <- mean(get_relE(res$F.report, get_dwindow(F_full, sty, y-1)))
+      relE_R[y-1] <- mean(get_relE(res$N.age[,1], R[132:(y-1)]))
+      relE_F[y-1] <- mean(get_relE(res$F.report, F_full[132:(y-1)]))
 
     }
 
