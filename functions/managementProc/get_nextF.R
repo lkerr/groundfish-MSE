473a7b6f0c613749355be182ae416d1d1bd5f182
Mackenzie Mazur <mmazur@neptune.org>
2020-11-10 16:18:58 -0500

    Council 7

--- functions/managementProc/get_nextF.R
+++ functions/managementProc/get_nextF.R
@@ -205,7 +205,7 @@ get_nextF <- function(parmgt, parpop, parenv, RPlast, evalRP, stockEnv){
             mincatchv<-rbind(mincatchv,catchadd)
           }
         }
-        if (catchproj[2]<min(tail(mincatchv$Catch,10))){catchproj[2]<-min(tail(mincatchv$Catch,10))}
+        if (stockEnv$catchproj[2]<min(tail(mincatchv$Catch,10))){stockEnv$catchproj[2]<-min(tail(mincatchv$Catch,10))}
         F <- get_F(x = stockEnv$catchproj[2],
                    Nv = stockEnv$J1N[y,], 
                    slxCv = stockEnv$slxC[y,], 
