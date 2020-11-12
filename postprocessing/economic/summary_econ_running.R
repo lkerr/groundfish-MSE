8d568b3b7fb904a9a7c454b3ce2a0f5cb91de82b
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2020-11-12 09:28:29 -0500

    add code to zip and copy when done.

--- postprocessing/economic/summary_econ_running.R
+++ postprocessing/economic/summary_econ_running.R
@@ -5,36 +5,26 @@
 # The result csv's may or may not contain an entire replicate; but they will contain entire fishing years.
 library(data.table)
 
+remote_location <- "/net/ftp/pub/dropoff/mlee/anna/summary/"
+stub<-"counterfactual_closeown"
+stub<-"validation"
+stub<-"counterfactual_closemult"
 
-#Follow this naming pattern
-#monthly_summary_validation_modelstuffhere.csv
-#stock_status_validation_modelstuffhere.csv
+econ_out_csv<-paste0("monthly_summary_",stub,".csv")
+stock_status_out_csv<-paste0("stock_status_",stub,".csv")
+pr_hat_out_csv<-paste0("pr_hat_",stub,".csv")
 
-#filenames for results
-econ_out_csv<-"monthly_summary_counterfactual_closeown1.csv"
-stock_status_out_csv<-"stock_status_counterfactual_closeown1.csv"
-pr_hat_out_csv<-"pr_hat_counterfactual_closeown1.csv"
-
-#econ_out_csv<-"monthly_summary_counterfactual_closemult1.csv"
-#stock_status_out_csv<-"stock_status_counterfactual_closemult1.csv"
-#pr_hat_out_csv<-"pr_hat_counterfactual_closemult1.csv"
-#only first 48 replicates off the first closemult model run.
-
-
-#econ_out_csv<-"monthly_summary_validation1.csv"
-#stock_status_out_csv<-"stock_status_validation1.csv"
-#pr_hat_out_csv<-"pr_hat_counterfactual_validation1.csv"
 
 #selects most recent file path in groundfish_MSE folder that starts with "results_" 
 file_path = file.info(list.files(path=".",pattern="^results_*",include.dirs=TRUE,full.names = T,recursive=TRUE))
 file_path= rownames(file_path)[which.max(file_path$mtime)]
-file_path="./results_2020-10-26-14-40-06"
+file_path="./results_2020-11-09-14-12-15"
 
 #selects just the econ_2020 datasets
 file_names = list.files(path=file.path(file_path, "econ","raw"), pattern = "econ_2020", full.names = TRUE)
 cutout<-100
 
-cutout<-71
+#cutout<-71
 
 
 # need to write code for lines 32-73 that works on the pr_hat data
@@ -138,51 +128,62 @@ write.csv(stock_status, file.path(file_path, stock_status_out_csv))
 
 
 #read in prhats
+# 
+# 
+# 
+# 
+# file_names = list.files(path=file.path(file_path, "econ","raw"), pattern = "prhat_", full.names = TRUE)
+# 
+# 
+# #binding into a data.table
+# prhat<-list()
+# 
+# 
+# #don't read the last file name
+# file_nums<-length(file_names)
+# 
+# for(file in 1:file_nums){
+#   
+#   
+#   sim<-fread(file_names[file], stringsAsFactors = FALSE)
+#   
+#   setnames(sim, old="r", new="replicate")
+#   setnames(sim, old="m", new="model")
+#   
+#   
+#   
+#   
+#   
+#   #generate date from doffy and extract month only 
+#   sim[, month := month(as.Date(doffy, origin=paste0(year, "-04-30"))), by=year]
+#   #start fishing year May = 1 
+#   sim[, month := ifelse(month < 5, month+8, month-4)]
+#   sim[ ,c("doffy", "y") := NULL]
+#   
+#   sim = sim[, lapply(.SD, sum, na.rm=TRUE), by=list(year, month, replicate, model,gearcat,hullnum)]
+#   
+#   prhat[[file]]<-sim
+# }
+# 
+# 
+# 
+# 
+# prhat<-rbindlist(prhat,use.names=TRUE,fill=TRUE) 
+# 
+# prhat <- prhat[replicate<=cutout]
+# 
+# write.csv(prhat, file.path(file_path, pr_hat_out_csv))
+# 
 
 
+# YOU SHOULD WRITE A little bit of code here that zips up the output csvs and saves to the network ftp.
+# and a little code that runs an sha1sum
+ 
 
+ zipcommand <-paste0("zip ",remote_location,stub,"_summary.zip ", file_path,"/*.csv" )
+ sha1command <-paste0("sha1sum ",remote_location,stub,"_summary.zip > ", remote_location,stub,"_summary.zip.sha1")
 
-file_names = list.files(path=file.path(file_path, "econ","raw"), pattern = "prhat_", full.names = TRUE)
-
-
-#binding into a data.table
-prhat<-list()
-
-
-#don't read the last file name
-file_nums<-length(file_names)
-
-for(file in 1:file_nums){
-  
-  
-  sim<-fread(file_names[file], stringsAsFactors = FALSE)
-  
-  setnames(sim, old="r", new="replicate")
-  setnames(sim, old="m", new="model")
-  
-  
-  
-  
-  
-  #generate date from doffy and extract month only 
-  sim[, month := month(as.Date(doffy, origin=paste0(year, "-04-30"))), by=year]
-  #start fishing year May = 1 
-  sim[, month := ifelse(month < 5, month+8, month-4)]
-  sim[ ,c("doffy", "y") := NULL]
-  
-  sim = sim[, lapply(.SD, sum, na.rm=TRUE), by=list(year, month, replicate, model,gearcat,hullnum)]
-  
-  prhat[[file]]<-sim
-}
-
-
-
-
-prhat<-rbindlist(prhat,use.names=TRUE,fill=TRUE) 
-
-prhat <- prhat[replicate<=cutout]
-
-write.csv(prhat, file.path(file_path, pr_hat_out_csv))
-
+system(zipcommand)
+system(sha1command)
 
 
