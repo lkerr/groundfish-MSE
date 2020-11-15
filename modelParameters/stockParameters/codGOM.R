995b8e6ce67adf32265bf7092267c1963e778778
Mackenzie Mazur <mmazur@neptune.org>
2020-11-15 17:51:22 -0500

    Council 6

--- modelParameters/stockParameters/codGOM.R
+++ modelParameters/stockParameters/codGOM.R
@@ -75,7 +75,7 @@ selC_typ <- 'input'
 ##For Hockey-stick (Default) Option##
 Rpar <- c(SSB_star = 6300, #mt  #from GOM COD 2019 AGEPRO M=0.2
       cR = 1,# dont need to convert
-      Rnyr= 20)#recruitment is drawn from the recruitment distribution of the last X years (x=Rnyr)
+      Rnyr= 10)#recruitment is drawn from the recruitment distribution of the last X years (x=Rnyr)
 #Rpar <- c(SSB_star = 7900, #mt  #from GOM COD 2019 MRAMP 
           #cR = 1,
           #Rnyr=20) # dont need to convert
@@ -124,6 +124,8 @@ oe_paaIN_typ <- 'multinomial'
 oe_effort <- 0.01
 oe_effort_typ <- 'lognorm'
 
+highobserrec<-FALSE
+
 # process error levels  #####o##############################  !!!!!!!!!!!!!!
 pe_R <- 0.01 # cannot be zero 
 pe_IA <- 0.18
@@ -135,15 +137,15 @@ ie_bias <- 0 # % bias in implementation error (F_Full + F_Full*ie_bias)
 
 
 # Observation bias (1 is no bias, 0.9 is a -10% bias, etc.) (sumCW*ob_sumCW) (range 0.01-1)
-ob_sumCW <- 0.44 #0.44 for bias
+ob_sumCW <- 1 #0.44 for bias
 ob_sumIN <- 1
 
 # catch observation bias (codCW + codCW*C_mult)
-C_mult <- 1.25 #1.25 for bias, 0 for no bias
+C_mult <-  0#1.25 for bias, 0 for no bias
 
 Change_point2<-FALSE
 Change_point_yr<-2025
-Change_point3<-TRUE
+Change_point3<-FALSE
 Change_point_yr1<-2020
 Change_point_yr2<-2022
 
