5ce8e74162b9f200341b4e334cbeeeddf0ec1d3b
Mackenzie Mazur <mmazur@neptune.org>
2020-12-02 06:55:47 -0500

    Returned Rfun function back

--- functions/managementProc/get_BBRP.R
+++ functions/managementProc/get_BBRP.R
@@ -46,9 +46,7 @@ get_BBRP <- function(parmgt, parpop, parenv, Rfun_lst, FBRP,
     # the list Rfun_BmsySim which is created in the processes folder.
     Rfun <- Rfun_lst[[parmgt$RFUN_NM]]
 
-    funR <- Rfun(parpop = parpop, 
-                 parmgt= parmgt,
-                 ny = parmgt$BREF_PAR0)
+    funR <- Rfun(Rest=tail(parpop$R,parmgt$BREF_PAR0))
    
     B <- ssbrFmax$SSBvalue * funR
 
