3b192f58aa176fdff452568dbd10bca78ca2d32f
Sam Truesdell <struesdell@gmri.org>
2020-12-13 19:10:04 -0500

    run

--- modelParameters/stockParameters/codGB_Error.R
+++ modelParameters/stockParameters/codGB_Error.R
@@ -98,7 +98,7 @@ oe_effort <- 0.01
 oe_effort_typ <- 'lognorm'
 
 # process error levels  ###################################  !!!!!!!!!!!!!!
-pe_R <- 0.001#5.819307e-01 #5.839408e-01
+pe_R <- 0.0001#5.819307e-01 #5.839408e-01
 
 # implementation error of fishing mortality
 ie_F <- 0
