93ba5ebd3a2a6c5326f6d941d87b43d505e0b933
Mackenzie Mazur <mmazur@neptune.org>
2020-12-23 16:18:17 -0500

    Council 1

--- functions/managementProc/get_advice.R
+++ functions/managementProc/get_advice.R
@@ -164,8 +164,7 @@ get_advice <- function(stock){
       Mohns_Rho_R[y] <- rowSums(pcols) / peels
       cat('Rho calculated.')
       if(mproc[m,'rhoadjust'] == 'TRUE'){
-        tempStock<-within(tempStock,{
-          parpop$SSBhat[length(parpop$SSBhat)]<-parpop$SSBhat[length(parpop$SSBhat)]/(Mohns_Rho_SSB[y]+1)})}})}
+          parpop$SSBhat[length(parpop$SSBhat)]<-parpop$SSBhat[length(parpop$SSBhat)]/(Mohns_Rho_SSB[y]+1)}})}
 
     # Environmental parameters
     parenv <- list(tempY = temp,
