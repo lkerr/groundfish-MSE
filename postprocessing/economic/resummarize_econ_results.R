e7e3a14c8aecbe4952821f4a6013e468e50bbeda
minyanglee <Min-Yang.Lee@noaa.gov>
2021-01-06 08:18:59 -0500

    tidy up a bit.

--- postprocessing/economic/resummarize_econ_results.R
+++ postprocessing/economic/resummarize_econ_results.R
@@ -8,10 +8,11 @@ library(data.table)
 # Naming outputs 
 
 #Uncomment one of these stub names
-#stub<-"counterfactual_closeownA"
-stub<-"validationA"
-#stub<-"counterfactual_closemultA"
+#stub<-"counterfactual_closeown1A"
+stub<-"validation1A"
+#stub<-"counterfactual_closemult1A"
 
+# Assemble the names of the csv where we will store data
 econ_out_csv<-paste0("monthly_summary_",stub,".csv")
 stock_status_out_csv<-paste0("stock_status_",stub,".csv")
 pr_hat_out_csv<-paste0("pr_hat_",stub,".csv")
@@ -58,15 +59,7 @@ sim[, trips := 1]
 setnames(sim, old="r", new="replicate")
 setnames(sim, old="m", new="model")
 
-#selecting one replicate for daily resolution 
-#daily_summary = sim[sim$replicate==1]
-#daily_summary [ ,c("id","hullnum", "y") := NULL]
-#daily_summary = daily_summary [, lapply(.SD, sum, na.rm=TRUE), by=list(year, doffy, spstock2, gearcat, replicate, model)]
-
-#write.csv(daily_summary, paste0(file_path, "daily_summary.csv"))
-
 #monthly resolution data 
-
 #generate date from doffy and extract month only 
 sim[, month := month(as.Date(doffy, origin=paste0(year, "-04-30"))), by=year]
 #start fishing year May = 1 
@@ -96,56 +89,12 @@ monthly_summary <- monthly_summary[replicate<=maxrep]
 write.csv(monthly_summary, file.path(file_path, econ_out_csv))
 
 
-
-
-
+############################################
+############################################
+# Probably dont need to do this 
+############################################
+############################################
 #read in stock status
-
-
-
-
-file_names = list.files(path=file.path(file_path, "econ","raw"), pattern = "econ_stock_status", full.names = TRUE)
-
-
-#binding into a data.table
-stock_status<-list()
-
-
-#don't read the last file name
-file_nums<-length(file_names)
-
-for(file in 1:file_nums){
-  
-  
-  sim<-fread(file_names[file], stringsAsFactors = FALSE)
-
-  setnames(sim, old="r", new="replicate")
- # setnames(sim, old="m", new="model")
-  
-  stock_status[[file]]<-sim
-}
-
-
-
-stock_status <- rbindlist(stock_status,use.names=TRUE,fill=TRUE) 
-stock_status <- stock_status[replicate<=maxrep]
-
-stock_status[,c("bio_model","SSB","mults_allocated","sectorACL"):=NULL]
-# 1 row per  per year, spstock2, replicate, model,
-# Either the last day of the year OR the first day where the stock_area was closed or the acl was reached
-
-stock_status<-stock_status[(underACL==FALSE | doffy==365 | stockarea_open==FALSE)]
-
-stock_status<-stock_status[, head(.SD, 1), by = c("year","spstock2","replicate","m")]
-
-write.csv(stock_status, file.path(file_path, stock_status_out_csv))
-
-
-
-
-
-
-
 
 # #read in prhats
 # 
