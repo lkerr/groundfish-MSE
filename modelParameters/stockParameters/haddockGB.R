b027beeed64f16b13dc52ec3cf512c82e2f0eff2
Mackenzie Mazur <mmazur@neptune.org>
2021-01-24 16:03:32 -0500

    Council 33

--- modelParameters/stockParameters/haddockGB.R
+++ modelParameters/stockParameters/haddockGB.R
@@ -1,7 +1,7 @@
 # Average and sd F before the management period begins. Mean on real scale
 # but distribution is lognormal. SD is lognormal SD.
 burnFmsyScalar <- 3
-burnFsd <- 0.3
+burnFsd <- 0.4
 
 
 # first age and plus age
@@ -35,7 +35,7 @@ M_mis<-FALSE
 #M_mis_val<-0.4
 
 # initial numbers at-age parameters
-initN_par <- c(nage = page, N0 = 1e4, F_full = 0.05, M = M)
+initN_par <- c(nage = page, N0 = 2e5, F_full = 0.05, M = M)
 initN_type <- 'expDecline'
 
 
