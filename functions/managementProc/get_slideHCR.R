857005f8a5e7abeab4b973703d62bc9fe98a2f83
Mackenzie Mazur <mmazur@neptune.org>
2021-03-15 13:23:06 -0400

    Changed ramped HCR parameterization so that it decreases below threshold

--- functions/managementProc/get_slideHCR.R
+++ functions/managementProc/get_slideHCR.R
@@ -1,10 +1,7 @@
 
 
 
-# Function that returns fishing mortality advice based on the control rule 
-# outlined in the Technical Guidance on the Use of Precautionary
-# Approaches to Implementing National Standard 1 of the MSA (see Gabriel
-# and Mace 1999, Proceedings, 5th NMFS NSAW
+# Function that returns fishing mortality advice based on the ramped control rule.
 # 
 # parpop: list containing population parameters. Must include named element
 #         "B" which is a vector of biomass history. Only the last number
@@ -18,18 +15,9 @@
 
 get_slideHCR <- function(parpop, Fmsy, Bmsy){
   
-  # c is defined as the maximum of 1-M or 1/2 (Gabriel and Mace 1999)
-  c <- max(1 - tail(parpop$M, 1), 1/2)
-  
-  # If estimated biomass is under Bmsy then use the linear relationship
-  # that goes through (Bmsy, Fmsy) and the origin; if estimated biomass
-  # is above Bmsy then use Fmsy
-  
-  if(tail(parpop$SSBhat, 1) <= c*Bmsy){
+  if(tail(parpop$SSBhat, 1) <= Bmsy){
     
-    # See Gabriel and Mace "A review of biological reference points in the
-    # contrxt of the prcautionary approach" p.40
-    F <- Fmsy * tail(parpop$SSBhat, 1) / (c*Bmsy)
+    F <- Fmsy * tail(parpop$SSBhat, 1) / Bmsy
     
   }else{
     
