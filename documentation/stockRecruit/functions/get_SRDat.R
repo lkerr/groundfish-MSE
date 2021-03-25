f5d8dde0b530250d527a66bb557c53da8f7f4aa4
Sam Truesdell <struesdell@gmri.org>
2021-03-25 11:55:50 -0400

    changes related to modeling GOM cod

--- documentation/stockRecruit/functions/get_SRDat.R
+++ documentation/stockRecruit/functions/get_SRDat.R
@@ -1,13 +1,14 @@
-get_SRDat <- function(recDat, stock){
-  
+get_SRDat <- function(recDat, stock, stockArea){
+
   # Subset the data to extract the stock of interest
-  tmpRec <- subset(recDat, STOCK == stock)
-  
+  tmpRec <- subset(recDat, STOCK == stock & STOCKAREA == stockArea)
+
   # Create new data frame appropriately offset such that the SSB is matched
   # with the correct number of recruits
   nr <- nrow(tmpRec)
   tmpRecOff <- tmpRec
   tmpRecOff$REC <- c(tmpRec$REC[2:nr], NA)
+  tmpRecOff <- tmpRecOff[,-which(names(tmpRecOff) == 'REC000S')]
   
   return(tmpRecOff)
 }
