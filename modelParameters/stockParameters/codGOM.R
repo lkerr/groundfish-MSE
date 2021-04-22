7d8d8fb0545400f29b12f22752b7c1a1fafcaaec
Mackenzie Mazur <mmazur@neptune.org>
2021-04-22 09:00:49 -0400

    Council 7

--- modelParameters/stockParameters/codGOM.R
+++ modelParameters/stockParameters/codGOM.R
@@ -63,12 +63,12 @@ selC_typ <- 'input'
 #           SSBRF0 = 0.01822) # calculated as S0/R0 (81202.9/4456870) (arbitrary because h = 1)
 # R_typ <- 'BHSteep'
 ##For Hockey-stick (Default) Option##
-Rpar <- c(SSB_star = 6300, #mt  #from GOM COD 2019 AGEPRO M=0.2
-      cR = 1,# dont need to convert
-      Rnyr= 20)#recruitment is drawn from the recruitment distribution of the last X years (x=Rnyr)
-#Rpar <- c(SSB_star = 7900,#mt  #from GOM COD 2019 MRAMP 
-#          cR = 1,
-#          Rnyr=20) # dont need to convert
+#Rpar <- c(SSB_star = 6300, #mt  #from GOM COD 2019 AGEPRO M=0.2
+#      cR = 1,# dont need to convert
+#      Rnyr= 20)#recruitment is drawn from the recruitment distribution of the last X years (x=Rnyr)
+Rpar <- c(SSB_star = 7900,#mt  #from GOM COD 2019 MRAMP 
+          cR = 1,
+          Rnyr=20) # dont need to convert
 R_typ <- 'HS'
 ###For BH SR with relationship with temperature###
 #R_typ <- 'BH'
