14974974fa488095aa9703d84afa78e728bf93a3
Mackenzie Mazur <mmazur@neptune.org>
2021-05-01 15:08:16 -0400

    Updated code for F REE Plot

--- postprocessing/Plots/FREEPlot.R
+++ postprocessing/Plots/FREEPlot.R
@@ -1,11 +1,13 @@
-#####F REE Plot####
-Scenarios<-c(6,32,58,116)
+#####F_full REE Plot####
+#Scenarios<-c(6,32,58,116)
+Scenarios<-c(5,6,7,8)
 ####First Sims####
 library(matrixStats)
 library(dplyr)
 library(ggplot2)
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[1],"/sim",sep=""))
-
+library(ggthemes)
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -14,21 +16,27 @@ for (k in 1:length(sims)){
 }
 sims<-na.omit(sims)
 
-Catchsim<-matrix(NA,nrow=20,ncol=length(sims))
+Catchsim<-matrix(NA,nrow=11,ncol=length(sims))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$relE_F[170:189]
+  Ftrue<-omvalGlobal[[1]]$F_full[169:189]
+  for (i in seq(170,190,2)){
+    Fest<-omvalGlobal[[1]]$Fest[i,]
+    Fest<-na.omit(Fest)
+    Fest<-tail(Fest,1)
+    Catchsim[((i-168)/2),k]<-((Fest-Ftrue[i-169])/Ftrue[i-169])*100
+  }
 }
 
 Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-2020:2039
+Year<-seq(2019,2039,2)
 df<-as.data.frame(cbind(Catchsim,Year))
 df$HCR<-Scenarios[1]
 
 ####Second Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[2],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -37,23 +45,29 @@ for (k in 1:length(sims)){
 }
 sims<-na.omit(sims)
 
-Catchsim<-matrix(NA,nrow=20,ncol=length(sims))
+Catchsim<-matrix(NA,nrow=11,ncol=length(sims))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$relE_F[170:189]
+  Ftrue<-omvalGlobal[[1]]$F_full[169:189]
+  for (i in seq(170,190,2)){
+    Fest<-omvalGlobal[[1]]$Fest[i,]
+    Fest<-na.omit(Fest)
+    Fest<-tail(Fest,1)
+    Catchsim[((i-168)/2),k]<-((Fest-Ftrue[i-169])/Ftrue[i-169])*100
+  }
 }
 
 Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-2020:2039
+Year<-seq(2019,2039,2)
 df2<-as.data.frame(cbind(Catchsim,Year))
 df2$HCR<-Scenarios[2]
 
 df<-full_join(df,df2)
 
 ####Third Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[3],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -62,22 +76,28 @@ for (k in 1:length(sims)){
 }
 sims<-na.omit(sims)
 
-Catchsim<-matrix(NA,nrow=20,ncol=length(sims))
+Catchsim<-matrix(NA,nrow=11,ncol=length(sims))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$relE_F[170:189]
+  Ftrue<-omvalGlobal[[1]]$F_full[169:189]
+  for (i in seq(170,190,2)){
+    Fest<-omvalGlobal[[1]]$Fest[i,]
+    Fest<-na.omit(Fest)
+    Fest<-tail(Fest,1)
+    Catchsim[((i-168)/2),k]<-((Fest-Ftrue[i-169])/Ftrue[i-169])*100
+  }
 }
 
 Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-2020:2039
+Year<-seq(2019,2039,2)
 df2<-as.data.frame(cbind(Catchsim,Year))
 df2$HCR<-Scenarios[3]
 
 df<-full_join(df,df2)
 ####Fourth Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[4],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -86,28 +106,35 @@ for (k in 1:length(sims)){
 }
 sims<-na.omit(sims)
 
-Catchsim<-matrix(NA,nrow=20,ncol=length(sims))
+Catchsim<-matrix(NA,nrow=11,ncol=length(sims))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$relE_F[170:189]
+  Ftrue<-omvalGlobal[[1]]$F_full[169:189]
+  for (i in seq(170,190,2)){
+    Fest<-omvalGlobal[[1]]$Fest[i,]
+    Fest<-na.omit(Fest)
+    Fest<-tail(Fest,1)
+    Catchsim[((i-168)/2),k]<-((Fest-Ftrue[i-169])/Ftrue[i-169])*100
+  }
 }
 
 Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-2020:2039
+Year<-seq(2019,2039,2)
 df2<-as.data.frame(cbind(Catchsim,Year))
 df2$HCR<-Scenarios[4]
 
 df<-full_join(df,df2)
 
-df$HCR[df$HCR==Scenarios[1]]<-'Ramped'
+df$HCR[df$HCR==Scenarios[1]]<-'Ramp'
 df$HCR[df$HCR==Scenarios[2]]<-'P*'
-df$HCR[df$HCR==Scenarios[3]]<-'Step in F'
-df$HCR[df$HCR==Scenarios[4]]<-'Ramped with variation constraint'
+df$HCR[df$HCR==Scenarios[3]]<-'F-step'
+df$HCR[df$HCR==Scenarios[4]]<-'Constrained ramp'
 df$HCR<-as.factor(df$HCR)
-df$HCR<-ordered(df$HCR,levels=c('Ramped','P*','Step in F','Ramped with variation constraint'))
+df$HCR<-ordered(df$HCR,levels=c('Ramp','P*','F-step','Constrained ramp'))
 
-ggplot(df)+geom_line(aes(x=Year,y=Catchsim,color=HCR))+
+ggplot(df)+geom_line(aes(x=Year,y=Catchsim,color=HCR),size=1)+
   theme_classic()+theme(text=element_text(size=18),legend.position='right')+
-  ylab('%REE F')+ ylim(min(-15,min(df$Catchsim)),max(15,max(df$Catchsim)))+
-  scale_color_manual(values=c("#EA4F12","#EACA00","#407331","#00608A"))
+  ylab('%REE F')+ ylim(min(-15,min(df$Catchsim)),15)+
+  scale_color_colorblind()+
+  scale_x_continuous(limits = c(2020,2040))
