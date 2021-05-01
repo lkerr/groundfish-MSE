b4062b0a28ff0b8efee5ff54bff0d9ea89b94cad
Mackenzie Mazur <mmazur@neptune.org>
2021-05-01 17:48:53 -0400

    Updated Mohn's Rho F plot code

--- postprocessing/Plots/MohnsRhoFplot.R
+++ postprocessing/Plots/MohnsRhoFplot.R
@@ -1,11 +1,13 @@
 #####Mohn's Rho for F Plot####
-Scenarios<-c(6,32,58,116)
+#Scenarios<-c(6,32,58,116)
+Scenarios<-c(5,6,7,8)
 ####First Sims####
 library(matrixStats)
 library(dplyr)
 library(ggplot2)
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[1],"/sim",sep=""))
-
+library(ggthemes)
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -27,8 +29,8 @@ df<-as.data.frame(cbind(Catchsim,Year))
 df$HCR<-Scenarios[1]
 
 ####Second Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[2],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -52,8 +54,8 @@ df2$HCR<-Scenarios[2]
 df<-full_join(df,df2)
 
 ####Third Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[3],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -76,8 +78,8 @@ df2$HCR<-Scenarios[3]
 df<-full_join(df,df2)
 
 ####Fourth Sims####
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[4],"/sim",sep=""))
-
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
+#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -100,17 +102,17 @@ df2$HCR<-Scenarios[4]
 
 df<-full_join(df,df2)
 
-df$HCR[df$HCR==Scenarios[1]]<-'Ramped'
+df$HCR[df$HCR==Scenarios[1]]<-'Ramp'
 df$HCR[df$HCR==Scenarios[2]]<-'P*'
-df$HCR[df$HCR==Scenarios[3]]<-'Step in F'
-df$HCR[df$HCR==Scenarios[4]]<-'Ramped with variation constraint'
+df$HCR[df$HCR==Scenarios[3]]<-'F-step'
+df$HCR[df$HCR==Scenarios[4]]<-'Constrained ramp'
 df$HCR<-as.factor(df$HCR)
-df$HCR<-ordered(df$HCR,levels=c('Ramped','P*','Step in F','Ramped with variation constraint'))
+df$HCR<-ordered(df$HCR,levels=c('Ramp','P*','F-step','Constrained ramp'))
 
 df<-na.omit(df)
 
-ggplot(df)+geom_line(aes(x=Year,y=Catchsim,color=HCR))+
+ggplot(df)+geom_line(aes(x=Year,y=Catchsim,color=HCR),size=1)+
   theme_classic()+theme(text=element_text(size=18),legend.position='right')+
   ylab('Mohns Rho for F')+ylim(-0.5,0.5)+
-  scale_color_manual(values=c("#EA4F12","#EACA00","#407331","#00608A"))
-
+  scale_color_colorblind()+
+  scale_x_continuous(limits = c(2020,2040))
