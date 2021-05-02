f02bdc7da4fd9986d47c63d9440b041e9b6f0a2f
Mackenzie Mazur <mmazur@neptune.org>
2021-05-01 21:57:15 -0400

    Updated Kobe plot code

--- postprocessing/Plots/Kobe_Plot.R
+++ postprocessing/Plots/Kobe_Plot.R
@@ -1,17 +1,18 @@
 #Kobe Plot
-Scenarios<-c(6,32,58,116)
+Scenarios<-c(1,2,3,4)
 ####Set up files####
 library(matrixStats)
 library(dplyr)
 library(ggrepel)
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[1],"/sim",sep=""))
+library(ggthemes)
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
 
 sims <- list.files()
 
-Freal<-matrix(NA,ncol=length(sims),nrow=21)
-Fproxy<-matrix(NA,ncol=length(sims),nrow=21)
-SSBreal<-matrix(NA,ncol=length(sims),nrow=21)
-SSBproxy<-matrix(NA,ncol=length(sims),nrow=21)
+Freal<-matrix(NA,ncol=length(sims),nrow=22)
+Fproxy<-matrix(NA,ncol=length(sims),nrow=22)
+SSBreal<-matrix(NA,ncol=length(sims),nrow=22)
+SSBproxy<-matrix(NA,ncol=length(sims),nrow=22)
 
 for (k in 1:length(sims)){
   if (file.size(sims[k])==0){
@@ -20,16 +21,16 @@ for (k in 1:length(sims)){
 sims<-na.omit(sims)
 
 ####True Values (From Operating Model)####
-setwd("C:/Users/mmazur/Desktop/Council_Sims")
+setwd("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims")
 tempwd <- getwd()
 setwd(paste(tempwd,"/Sim_",Scenarios[1],"/sim",sep=""))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Freal[,k]<-omvalGlobal[[1]]$F_full[170:190]
-  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[170:190]
-  SSBreal[,k]<-omvalGlobal[[1]]$SSB[170:190]
-  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[170:190]
+  Freal[,k]<-omvalGlobal[[1]]$F_full[169:190]
+  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[169:190]
+  SSBreal[,k]<-omvalGlobal[[1]]$SSB[169:190]
+  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[169:190]
 }
 
 Freal<-rowMedians(Freal,na.rm=T)
@@ -39,18 +40,18 @@ Fratioreal<-Freal/Fproxy
 SSBreal<-rowMedians(SSBreal,na.rm=T)
 SSBproxy<-rowMedians(SSBproxy,na.rm=T)
 SSBratioreal<-SSBreal/SSBproxy
-Year<-2020:2040
+Year<-2019:2040
 Dftrue<-as.data.frame(cbind(SSBratioreal,Fratioreal,Year))
 Dftrue$Scenario<-Scenarios[1]
 
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[2],"/sim",sep=""))
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
 
 sims <- list.files()
 
-Freal<-matrix(NA,ncol=length(sims),nrow=21)
-Fproxy<-matrix(NA,ncol=length(sims),nrow=21)
-SSBreal<-matrix(NA,ncol=length(sims),nrow=21)
-SSBproxy<-matrix(NA,ncol=length(sims),nrow=21)
+Freal<-matrix(NA,ncol=length(sims),nrow=22)
+Fproxy<-matrix(NA,ncol=length(sims),nrow=22)
+SSBreal<-matrix(NA,ncol=length(sims),nrow=22)
+SSBproxy<-matrix(NA,ncol=length(sims),nrow=22)
 
 for (k in 1:length(sims)){
   if (file.size(sims[k])==0){
@@ -59,16 +60,16 @@ for (k in 1:length(sims)){
 sims<-na.omit(sims)
 
 ####True Values (From Operating Model)####
-setwd("C:/Users/mmazur/Desktop/Council_Sims")
+setwd("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims")
 tempwd <- getwd()
 setwd(paste(tempwd,"/Sim_",Scenarios[2],"/sim",sep=""))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Freal[,k]<-omvalGlobal[[1]]$F_full[170:190]
-  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[170:190]
-  SSBreal[,k]<-omvalGlobal[[1]]$SSB[170:190]
-  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[170:190]
+  Freal[,k]<-omvalGlobal[[1]]$F_full[169:190]
+  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[169:190]
+  SSBreal[,k]<-omvalGlobal[[1]]$SSB[169:190]
+  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[169:190]
 }
 
 Freal<-rowMedians(Freal,na.rm=T)
@@ -78,19 +79,19 @@ Fratioreal<-Freal/Fproxy
 SSBreal<-rowMedians(SSBreal,na.rm=T)
 SSBproxy<-rowMedians(SSBproxy,na.rm=T)
 SSBratioreal<-SSBreal/SSBproxy
-Year<-2020:2040
+Year<-2019:2040
 Dftrue2<-as.data.frame(cbind(SSBratioreal,Fratioreal,Year))
 Dftrue2$Scenario<-Scenarios[2]
 df<-full_join(Dftrue,Dftrue2)
 
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[3],"/sim",sep=""))
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
 
 sims <- list.files()
 
-Freal<-matrix(NA,ncol=length(sims),nrow=21)
-Fproxy<-matrix(NA,ncol=length(sims),nrow=21)
-SSBreal<-matrix(NA,ncol=length(sims),nrow=21)
-SSBproxy<-matrix(NA,ncol=length(sims),nrow=21)
+Freal<-matrix(NA,ncol=length(sims),nrow=22)
+Fproxy<-matrix(NA,ncol=length(sims),nrow=22)
+SSBreal<-matrix(NA,ncol=length(sims),nrow=22)
+SSBproxy<-matrix(NA,ncol=length(sims),nrow=22)
 
 for (k in 1:length(sims)){
   if (file.size(sims[k])==0){
@@ -99,16 +100,16 @@ for (k in 1:length(sims)){
 sims<-na.omit(sims)
 
 ####True Values (From Operating Model)####
-setwd("C:/Users/mmazur/Desktop/Council_Sims")
+setwd("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims")
 tempwd <- getwd()
 setwd(paste(tempwd,"/Sim_",Scenarios[3],"/sim",sep=""))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Freal[,k]<-omvalGlobal[[1]]$F_full[170:190]
-  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[170:190]
-  SSBreal[,k]<-omvalGlobal[[1]]$SSB[170:190]
-  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[170:190]
+  Freal[,k]<-omvalGlobal[[1]]$F_full[169:190]
+  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[169:190]
+  SSBreal[,k]<-omvalGlobal[[1]]$SSB[169:190]
+  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[169:190]
 }
 
 Freal<-rowMedians(Freal,na.rm=T)
@@ -118,19 +119,19 @@ Fratioreal<-Freal/Fproxy
 SSBreal<-rowMedians(SSBreal,na.rm=T)
 SSBproxy<-rowMedians(SSBproxy,na.rm=T)
 SSBratioreal<-SSBreal/SSBproxy
-Year<-2020:2040
+Year<-2019:2040
 Dftrue2<-as.data.frame(cbind(SSBratioreal,Fratioreal,Year))
 Dftrue2$Scenario<-Scenarios[3]
 df<-full_join(df,Dftrue2)
 
-setwd(paste("C:/Users/mmazur/Desktop/Council_Sims/Sim_",Scenarios[4],"/sim",sep=""))
+setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
 
 sims <- list.files()
 
-Freal<-matrix(NA,ncol=length(sims),nrow=21)
-Fproxy<-matrix(NA,ncol=length(sims),nrow=21)
-SSBreal<-matrix(NA,ncol=length(sims),nrow=21)
-SSBproxy<-matrix(NA,ncol=length(sims),nrow=21)
+Freal<-matrix(NA,ncol=length(sims),nrow=22)
+Fproxy<-matrix(NA,ncol=length(sims),nrow=22)
+SSBreal<-matrix(NA,ncol=length(sims),nrow=22)
+SSBproxy<-matrix(NA,ncol=length(sims),nrow=22)
 
 for (k in 1:length(sims)){
   if (file.size(sims[k])==0){
@@ -139,16 +140,16 @@ for (k in 1:length(sims)){
 sims<-na.omit(sims)
 
 ####True Values (From Operating Model)####
-setwd("C:/Users/mmazur/Desktop/Council_Sims")
+setwd("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims")
 tempwd <- getwd()
 setwd(paste(tempwd,"/Sim_",Scenarios[4],"/sim",sep=""))
 
 for (k in 1:length(sims)){
   load(sims[k])
-  Freal[,k]<-omvalGlobal[[1]]$F_full[170:190]
-  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[170:190]
-  SSBreal[,k]<-omvalGlobal[[1]]$SSB[170:190]
-  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[170:190]
+  Freal[,k]<-omvalGlobal[[1]]$F_full[169:190]
+  Fproxy[,k]<-omvalGlobal[[1]]$FPROXYT[169:190]
+  SSBreal[,k]<-omvalGlobal[[1]]$SSB[169:190]
+  SSBproxy[,k]<-omvalGlobal[[1]]$SSBPROXYT[169:190]
 }
 
 Freal<-rowMedians(Freal,na.rm=T)
@@ -158,37 +159,37 @@ Fratioreal<-Freal/Fproxy
 SSBreal<-rowMedians(SSBreal,na.rm=T)
 SSBproxy<-rowMedians(SSBproxy,na.rm=T)
 SSBratioreal<-SSBreal/SSBproxy
-Year<-2020:2040
+Year<-2019:2040
 Dftrue2<-as.data.frame(cbind(SSBratioreal,Fratioreal,Year))
 Dftrue2$Scenario<-Scenarios[4]
 df<-full_join(df,Dftrue2)
 
 ####Kobe Plot####
 library(ggplot2)
-df$HCR<-df$Scenario
 maxSSB<-max(1.1,max(SSBratioreal))
 maxF<-max(1.1,max(Fratioreal))
-df$HCR[df$HCR==Scenarios[1]]<-'Ramped'
+df$HCR<-df$Scenario
+df$HCR[df$HCR==Scenarios[1]]<-'Ramp'
 df$HCR[df$HCR==Scenarios[2]]<-'P*'
-df$HCR[df$HCR==Scenarios[3]]<-'Step in F'
-df$HCR[df$HCR==Scenarios[4]]<-'Ramped with variation constraint'
+df$HCR[df$HCR==Scenarios[3]]<-'F-step'
+df$HCR[df$HCR==Scenarios[4]]<-'Constrained ramp'
 df$HCR<-as.factor(df$HCR)
-df$HCR<-ordered(df$HCR,levels=c('Ramped','P*','Step in F','Ramped with variation constraint'))
+df$HCR<-ordered(df$HCR,levels=c('Ramp','P*','F-step','Constrained ramp'))
 kobe <- ggplot(df, aes(x = SSBratioreal, y = Fratioreal)) +
-    theme_bw() 
-  
-  kobe <- kobe + annotate(geom = "rect", xmin = 1, xmax = maxSSB, ymin = 0, ymax = 1, fill = "green", colour = "green", alpha = 0.5) +
-    annotate(geom = "rect", xmin = 0, xmax = 1, ymin = 1, ymax = maxF, fill = "red", colour = "red", alpha = 0.5) +
-    annotate(geom = "rect", xmin = 0, xmax = 1, ymin = 0, ymax = 1, fill = "yellow", colour = "yellow", alpha = 0.5) +
-    annotate(geom = "rect", xmin = 1, xmax = maxSSB, ymin = 1, ymax = maxF, fill = "yellow", colour = "yellow", alpha = 0.5) +
-    geom_path(aes(linetype = HCR,colour=HCR), size = 0.3) +
-    geom_point(aes(colour=HCR)) + # colour = yr
-    labs(x = 'SSB/SSB MSY Proxy',
-         y = 'F/F MSY Proxy') +
-    xlim(0,maxSSB)+
-    ylim(0,maxF)+
-    geom_vline(xintercept=0.5, linetype='dotted')+
-    theme(text=element_text(size=16),legend.position='bottom')+
-    geom_text_repel(data=subset(df, Year > 2039 | Year < 2021),aes(x = SSBratioreal, y = Fratioreal, label = Year)) +
-    scale_color_manual(values=c("#EA4F12","#EACA00","#407331","#00608A"))
- kobe
+  theme_bw() 
+
+kobe <- kobe + annotate(geom = "rect", xmin = 1, xmax = maxSSB, ymin = 0, ymax = 1, fill = "green", colour = "green", alpha = 0.5) +
+  annotate(geom = "rect", xmin = 0, xmax = 1, ymin = 1, ymax = maxF, fill = "red", colour = "red", alpha = 0.5) +
+  annotate(geom = "rect", xmin = 0, xmax = 1, ymin = 0, ymax = 1, fill = "yellow", colour = "yellow", alpha = 0.5) +
+  annotate(geom = "rect", xmin = 1, xmax = maxSSB, ymin = 1, ymax = maxF, fill = "yellow", colour = "yellow", alpha = 0.5) +
+  geom_path(aes(linetype = HCR,colour=HCR), size = 0.3) +
+  geom_point(aes(colour=HCR)) + # colour = yr
+  labs(x = 'SSB/SSB MSY',
+       y = 'F/F MSY') +
+  xlim(0,maxSSB)+
+  ylim(0,maxF)+
+  scale_color_colorblind()+
+  geom_vline(xintercept=0.5, linetype='dotted')+
+  theme(text=element_text(size=16),legend.position='bottom')+
+  geom_text_repel(data=subset(df, Year > 2039 | Year < 2020),aes(x = SSBratioreal, y = Fratioreal, label = Year))
+kobe
