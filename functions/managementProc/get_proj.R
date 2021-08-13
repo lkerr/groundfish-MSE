c3fa1e5d4bc08cc485c8856f1460e5fd557673cb
Mackenzie Mazur <mmazur@neptune.org>
2021-08-13 15:18:27 -0400

    Fixed projections for no lag

--- functions/managementProc/get_proj.R
+++ functions/managementProc/get_proj.R
@@ -47,7 +47,7 @@ get_proj <- function(type, parmgt, parpop, parenv, Rfun,
                  'range. Check FREF_PAR0 or BREF_PAR0 in mproc.'))
     }
     
-    Tanom <- parenv$Tanom[startFCST:endFCST]
+    Tanom <- c(parenv$Tanom[startFCST:endFCST])
        
     ny <- length(Tanom)
     
@@ -102,47 +102,50 @@ get_proj <- function(type, parmgt, parpop, parenv, Rfun,
   # set up initial conditions
   N[1,] <- init 
   #Get beginning of year population in year t+1
-  # if (type=='current'){
-  #   Fhat<-parpop$Fhat
-  #   for(a in 2:(nage-1)){
+ if (type=='current'){
+     if (!exists('catchproj',stockEnv)){
+       Fhat<-parpop$Fhat
+     }
+     if (exists('catchproj',stockEnv)){
+       Fhat<-get_F(x = stockEnv$catchproj[2],
+                     Nv = init, 
+                     slxCv = parpop$sel, 
+                     M = parpop$M, 
+                     waav = parpop$waa)
+     }
+ for(a in 2:(nage-1)){
   #     #init= population at the beginning of the year in t-1  
   #     #exponential survival to the next year/age (t)
-  #     N[1,a] <- init[a-1] * exp(-parpop$sel[a-1]*Fhat - 
-  #                                   parpop$M[a-1])
-  #   }
+ N[1,a] <- init[a-1] * exp(-parpop$sel[a-1]*Fhat - 
+                                 parpop$M[a-1])
+  }
   #   # Deal with the plus group
-  #   N[1,nage] <- init[nage-1] * exp(-parpop$sel[nage-1] * Fhat - 
-  #                                      parpop$M[nage-1]) + 
-  #     init[nage] * exp(-parpop$sel[nage] * Fhat - 
-  #                         parpop$M[nage])
-  #   
-  #   Recruits<-parpop$R
-  #   
-  #   N[1,1] <- prod(tail(Recruits,5))^(1/5)
-  #   if (mproc$rhoadjust==TRUE & y>fmyearIdx & stockEnv$Mohns_Rho_SSB[y]>0.15){
-  #     N[1,]<-N[1,]/(1+stockEnv$Mohns_Rho_SSB[y])
-  #   }
-  # }
+   N[1,nage] <- init[nage-1] * exp(-parpop$sel[nage-1] * Fhat - 
+                                       parpop$M[nage-1]) + 
+     init[nage] * exp(-parpop$sel[nage] * Fhat - 
+                         parpop$M[nage])
+   parpop$switch<-'FALSE'
+   N[1,1] <- Rfun(type = stockEnv$R_typ,
+                  parpop = parpop, 
+                  parenv = parenv, 
+                  parmgt = parmgt,
+                  SSB = c(init) %*% c(parpop$waa* parpop$mat),
+                  sdR = stockEnv$pe_R,
+                  TAnom = Tanom[1],
+                  Rest = Rest)
+   if (mproc$rhoadjust==TRUE & y>fmyearIdx & stockEnv$Mohns_Rho_SSB[y]>0.15){
+     N[1,]<-N[1,]/(1+stockEnv$Mohns_Rho_SSB[y])
+    }
+ }
   for(y in 2:length(Tanom)){
   Fvalue<-F_val
-  if (type=='current'){
-    if (y==2 & !exists('catchproj',stockEnv)){
-      Fvalue<-parpop$Fhat
-    }
-    if (y==2 & exists('catchproj',stockEnv)){
-      Fvalue<-get_F(x = stockEnv$catchproj[2],
-                    Nv = init, 
-                    slxCv = parpop$sel, 
-                    M = parpop$M, 
-                    waav = parpop$waa)
-    }
-  }
     for(a in 2:(nage-1)){
       #N[y-1] is the population at the beginning of the previous year 
       #exponential survival to the next year/age
       N[y,a] <- N[y-1, a-1] * exp(-parpop$sel[a-1]*Fvalue - 
                                     parpop$M[a-1])
     }
+
     # Deal with the plus group
       N[y,nage] <- N[y-1,nage-1] * exp(-parpop$sel[nage-1] * Fvalue - 
                                        parpop$M[nage-1]) + 
@@ -168,15 +171,22 @@ get_proj <- function(type, parmgt, parpop, parenv, Rfun,
   SSBaa <- sweep(Waa, MARGIN=2, STATS=parpop$mat, FUN='*')
 
   # Calculate the catch in weight
+  
   sumCW <- sapply(2:nrow(N), function(i){
     CN <- (parpop$sel * F_val) / (parpop$sel * F_val + parpop$M) * 
       N[i,] * (1 - exp(-F_val * parpop$sel - parpop$M))
     tempSumCW <- CN %*% c(parpop$waa)
     return(tempSumCW)
   })
-  
+  if (type=='current'){
+    sumCW <- sapply(1:2, function(i){
+      CN <- (parpop$sel * F_val) / (parpop$sel * F_val + parpop$M) * 
+        N[i,] * (1 - exp(-F_val * parpop$sel - parpop$M))
+      tempSumCW <- CN %*% c(parpop$waa)
+      return(tempSumCW)
+    })
+  }
   J1Npj <- N
-
   SSBaa <- SSBaa
   SSBpj <- apply(SSBaa, 1, sum)
   sumCWpj <- sumCW
