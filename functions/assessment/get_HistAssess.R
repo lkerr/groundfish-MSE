2a42bc4850320c4079da90e981c057331be5efa7
mdmazur <64043006+mdmazur@users.noreply.github.com>
2021-10-06 12:54:29 -0400

    Updated get_HistAssess.R

--- functions/assessment/get_HistAssess.R
+++ functions/assessment/get_HistAssess.R
@@ -1,4 +1,4 @@
-### function to read in and use historic assessment values
+### function to read in and use historic assessment values to ultimately inform historical stock trajectories
 
 get_HistAssess <- function(stock) {
   # Stock assessment history file name
@@ -15,8 +15,8 @@ get_HistAssess <- function(stock) {
   assessdat <- read.csv(paste('./data/data_raw/AssessmentHistory/',stockNames[i], '.csv', sep = ''))
  if(stock$M_typ== 'ramp' && stock$stockName=='codGOM'){
    assessdat<- read.csv('./data/data_raw/AssessmentHistory/codGOM_highM.csv')
-if (stock$M == 0.3){
-  assessdat<- read.csv('./data/data_raw/AssessmentHistory/codGOM_mediumM.csv')}
+     if (stock$M == 0.3){
+     assessdat<- read.csv('./data/data_raw/AssessmentHistory/codGOM_mediumM.csv')}
  }
  if(ncol(assessdat) != 4){
     stop('Check that Assessment History file contains appropriate data.')
