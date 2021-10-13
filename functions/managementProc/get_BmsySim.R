d1eeea6f45c789d9d4288740a285ca2ea6f5655b
Mackenzie Mazur <mmazur@neptune.org>
2021-10-13 15:59:07 -0400

    Cleaned up code a bit

--- functions/managementProc/get_BmsySim.R
+++ functions/managementProc/get_BmsySim.R
@@ -1,12 +1,10 @@
-
-
-# Function to get Bmsy assuming an Fmsy, recruitment history and level of
+# Function to estimate Bmsy assuming an Fmsy, recruitment history and level of
 # natural mortality.
 # 
 # parmgt: a 1-row data frame of management parameters. The operational
 #         component of parmgt for this function are the (1-row) columns
 #         "FREF_TYP" and "FREF_PAR0". FREF_TYP/FREF_PAR0 determine
-#         how the Fmsy proxy is set (i.e., what the level of fishing
+#         how the F reference point is set (i.e., what the level of fishing
 #         mortality will be during the projections).
 # 
 # parpop: named ist of population parameters (vectors) needed for the 
@@ -23,16 +21,14 @@
 #       the trim=p argument in the ... if you wanted to apply the mean
 #       recruitment or a trimmed mean recruitment to every year of the
 #       projection/simulation. Another option would be to do something 
-#       like use rlnorm() and then specify an sd. Or you could define a
+#       like use rlnorm() and then specify an sd. Another impcat is to define a
 #       trend in the recruitment history and use that.
 #       
-# distillBmsy: function to aggregate biomasses under the particular F policy.
-#              Since temperature can play a role there is no equilibrium
-#              over the period, so need to make a decision about what function
-#              to use like mean, median or a quantile.
+# distillBmsy: function to aggregate biomass under the particular F policy.
+#              Since temperature can play a role there may be no equilibrium
+#              over the period, so a decision about what function
+#              to use for biomass needs to be made (mean, median or a quantile).
 #       
-#         
-#         
 # # Important note: If you want to use only the last n years of the
 # recruitment history, you can use something like
 #     function(x) sample(tail(x, n), 1)
@@ -42,15 +38,11 @@
 # number 5. Probably you wouldn't really want to use the last year
 # anyhow though (so I guess this note isn't so critically important!)
 
-
-### ** note: initial numbers-at-age is assuming age-1 is the minimum right now. model ages should be
-###          passed under parpop and the minimum should be used instead of 1.0.
-
-
+### ** note: initial numbers-at-age is assuming age-1 is the minimum right now. Model ages should be
+###    passed under parpop and the minimum should be used instead of 1.0.
 
 get_BmsySim <- function(parmgt, parpop, parenv, Rfun,
                         F_val, distillBmsy = mean, ...){
-
   
   get_proj(type = 'BREF',
            parmgt = parmgt, 
@@ -61,13 +53,12 @@ get_BmsySim <- function(parmgt, parpop, parenv, Rfun,
            stReportYr,
            stockEnv = stock)
   
-
   # Find the total SSB / year ([-1,] to get rid of the initial year, where
   # recruitment was coming out of an estimate and had nothing to do with
   # any projections.
+  
   SSBref <- distillBmsy(apply(SSBaa, 1, sum))
  
-
   out <- list(RPlevel = parmgt$FREF_PAR0,
               RPvalue = F_val,
               SSBvalue = SSBref,
