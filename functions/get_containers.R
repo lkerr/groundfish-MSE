7a3a003a622eeba223160ac70b58c16fa329161e
ahart1 <arhart555@gmail.com>
2021-10-19 17:35:45 -0400

    Update documentation, may want to describe what data stored in containers rather than focusing only on structure.

--- functions/get_containers.R
+++ functions/get_containers.R
@@ -1,11 +1,177 @@
-# Script to create containers (necessary because the main
-# script uses loops that it fills)
+#' @title Get storage containers
+#' @description Create containers (necessary because the main script uses loops that it fills) ??? Does this mean the main loop fills these containers?
+#' 
+#' @template global_stockPar
+#' @template global_nyear
+#' @template global_fyear
+#' @template global_nburn
+#' @template global_mproc
+#' @template global_nrep
+#' 
+#' @return A list containing storage containers for the following: ??? May want to describe what is stored in each matrix
+#' \itemize{
+#'   \item{Simulation storage containers}
+#'   \itemize{
+#'     \item{J1N - A matrix with nyear rows and a column for each age}
+#'     \item{CN - A matrix with nyear rows and a column for each age}
+#'     \item{CN_temp - A matrix with nyear rows and a column for each age}
+#'     \item{codCW - A matrix with nyear rows and a column for each age ??? project specific?}
+#'     \item{codCW2 - A vector of length nyear ??? project specific}
+#'     \item{CW - A matrix with nyear rows and a column for each age}
+#'     \item{IN - A matrix with nyear rows and a column for each age}
+#'     \item{IJ1 - A matrix with nyear rows and a column for each age}
+#'     \item{laa - A matrix with nyear rows and a column for each age}
+#'     \item{waa - A matrix with nyear rows and a column for each age}
+#'     \item{Z - A matrix with nyear rows and a column for each age},
+#'     \item{slxC - A matrix with nyear rows and a column for each age}
+#'     \item{slxI - A matrix with nyear rows and a column for each age}
+#'     \item{mat - A matrix with nyear rows and a column for each age}
+#'     \item{F_full - A vector of length nyear}
+#'     \item{F_fullAdvice - A vector of length nyear}
+#'     \item{ACL - A vector of length nyear}
+#'     \item{R - A vector of length nyear}
+#'     \item{Rhat - A vector of length nyear}
+#'     \item{N - A vector of length nyear}
+#'     \item{SSB - A vector of length nyear}
+#'     \item{RPmat - A matrix with nyear rows and 6 columns}
+#'     \item{OFdStatus - A vector of length nyear}
+#'     \item{mxGradCAA - A vector of length nyear}
+#'     \item{OFgStatus - A vector of length nyear}
+#'     \item{natM - A vector of length nyear}
+#'   }
+#'   \item{Storage containers that have corresponding outputs for the assessment model}
+#'   \itemize{
+#'     \item{sumCW - Catch weight, a vector of length nyear}
+#'     \item{obs_sumCW - Observed catch weight, a vector of length nyear}
+#'     \item{sumIN - Survey index in numbers, a vector of length nyear}
+#'     \item{obs_sumIN - Observed survey index in numbers, a vector of length nyear}
+#'     \item{SumIW - Survey index in weight, a vector of length nyear}
+#'     \item{obs_sumIW - Observed survey index in weight, a vector of length nyear}
+#'     \item{paaCN - Catch proportions-at-age, a matrix with nyear rows and a column for each age}
+#'     \item{obs_paaCN - Observed catch proportions-at-age, a matrix with nyear rows and a column for each age}
+#'     \item{paaIN - Survey proportions-at-age, a matrix with nyear rows and a column for each age}
+#'     \item{obs_paaIN - Observedurvey proportions-at-age, a matrix with nyear rows and a column for each age}
+#'     \item{effort - Fishing effort, a vector of length nyear}
+#'     \item{obs_effort - Observed fishing effort, a vector of length nyear}
+#'   }
+#'   \item{Stock assessment model results storage containers}
+#'   \itemize{
+#'     \item{relE_qI - A vector of length nyear}
+#'     \item{relE_qC - A vector of length nyear}
+#'     \item{relE_selCs0 - A vector of length nyear}
+#'     \item{relE_selCs1 - A vector of length nyear}
+#'     \item{relE_ipop_mean - A vector of length nyear}
+#'     \item{relE_ipop_dev - A vector of length nyear}
+#'     \item{relE_R_dev - A vector of length nyear}
+#'     \item{relE_SSB - A vector of length nyear}
+#'     \item{relE_N - A vector of length nyear}
+#'     \item{relE_CW - A vector of length nyear}
+#'     \item{relE_IN - A vector of length nyear}
+#'     \item{relE_R - A vector of length nyear}
+#'     \item{relE_F - A vector of length nyear}
+#'     \item{SSB_cur - A vector of length nyear}
+#'     \item{conv_rate - A vector of length nyear}
+#'     \item{Mohns_Rho_SSB - A vector of length nyear}
+#'     \item{Mohns_Rho_N - A vector of length nyear}
+#'     \item{Mohns_Rho_F - A vector of length nyear}
+#'     \item{Mohns_Rho_R - A vector of length nyear}
+#'     \item{mincatchcon - A vector of length nyear}
+#'     \item{relTermE_SSB - A number}
+#'     \item{relTermE_CW - A number}
+#'     \item{relTermE_IN - A number}
+#'     \item{relTermE_qI - A number}
+#'     \item{relTermE_R - A number}
+#'     \item{relTermE_F - A number}
+#'     \item{SSBest - A matrix with nyear rows and 54 columns}
+#'     \item{Fest - A matrix with nyear rows and 54 columns}
+#'     \item{Catchest - A matrix with nyear rows and 54 columns}
+#'     \item{Rest - A matrix with nyear rows and 54 columns}
+#'   }
+#'   \item{Economic model storage containers}
+#'   \itemize{
+#'     \item{econCW - A vector of length nyear}
+#'   }
+#'   \item{Operating and assessment model comparison storage containers}
+#'   \itemize{
+#'     \item{dn_rep - A nrep length vector of strings describing the rep number}
+#'     \item{dn_omyear - A nomyear length vector of strings describing the OM year}
+#'     \item{dn_mproc - A nmproc length vector of strings describing the mproc number}
+#'     \item{omval - A list of the following}
+#'     \itemize{
+#'       \item{N - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSB - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{R - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{F_full - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{F_fullAdvice - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{ACL - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{econCW - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{sumCW - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{annPercentChange - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{meanSizeCN - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{meanSizeIn - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FPROXY - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBPROXY - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FPROXYT - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBPROXYT - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FPROXYT2 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBPROXYT2 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBRATIO - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBRATIOT - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBRATIOT2 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FRATIO - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FRATIOT - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{FRATIOT2 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{OFdStatus - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{mxGradCAA - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_qI - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_qC - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_selCs0 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_selCs1 - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_ipop_mean - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_ipop_dev - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_R_dev - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_SSB - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_N - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_CW - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_IN - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_R - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relE_F - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{OFgStatus - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSB_cur - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{natM - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{conv_rate - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Mohns_Rho_SSB - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Mohns_Rho_N - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Mohns_Rho_F - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Mohns_Rho_R - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{mincatchcon - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_SSB - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_CW - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_IN - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_qI - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_R - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{relTermE_F - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{SSBest - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Fest - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Catchest - A nyear length array of matrices with nrep rows and nmproc columns}
+#'       \item{Rest - A nyear length array of matrices with nrep rows and nmproc columns}
+#'     }
+#'   }
+#' }
+#' 
+#' @family setup
+#' 
 
-get_containers <- function(stockPar){
+get_containers <- function(stockPar,
+                           nyear,
+                           fyear,
+                           nburn,
+                           mproc,
+                           nrep = 2){
   
   yxage = matrix(NA, nrow=nyear, ncol=stockPar$nage)
   yx0 = rep(NA, nyear)
-  est = matrix(NA,nyear,54)
+  est = matrix(NA,nyear,54) #??? Why 54? should this be an argument passed to the function?
   
   nomyear = nyear - (stockPar$ncaayear + fyear + nburn)
   nmproc = nrow(mproc)
@@ -15,7 +181,7 @@ get_containers <- function(stockPar){
                           dimnames = list(paste0('rep', 1:nrep), 
                                           paste0('mproc', 1:nmproc),
                                           paste0('nyear', 1:nyear)))
-  
+  # List of storage containers
   out <- list(
 
     # Containers that save the simulation data
@@ -48,31 +214,29 @@ get_containers <- function(stockPar){
     OFgStatus = yx0,
     natM = yx0,
     
-    # Containers that have corresponding outputs for the
-    # assessment model
+    # Containers that have corresponding outputs for the assessment model
     
-    # catch weight
+    # Catch weight
     sumCW = yx0,
     obs_sumCW = yx0,
     
-    # total survey index in numbers
+    # Total survey index in numbers
     sumIN = yx0,
     obs_sumIN = yx0,
     
-    # total survey index in weight (if obs_sumIW is needed see note in
-    # get_indexData comments)
+    # Total survey index in weight (if obs_sumIW is needed see note in get_indexData comments)
     sumIW = yx0,
     obs_sumIW = yx0,
     
-    # catch proportions-at-age
+    # Catch proportions-at-age
     paaCN = yxage,
     obs_paaCN = yxage,
     
-    # survey proportions-at-age
+    # Survey proportions-at-age
     paaIN = yxage,
     obs_paaIN = yxage,
     
-    # fishing effort
+    # Fishing effort
     effort = yx0,
     obs_effort = yx0,
     
@@ -112,7 +276,7 @@ get_containers <- function(stockPar){
     # Total Weight of catch
     econCW= yx0, 
     
-    # container to hold operating/assessment model results
+    # Container to hold operating/assessment model results
     # (operating model-assessment model comparison)
     
     dn_rep = paste0('rep', 1:nrep),
@@ -177,11 +341,9 @@ get_containers <- function(stockPar){
       Fest = est,
       Catchest = est,
       Rest = est
-    )
+    ) # End OM list
     
-  )
+  ) # End container list
   
   return(out)
-
 }
-
