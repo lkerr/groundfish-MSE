0e781cea7daaabe81af27fe744ec23eb1a91d463
ahart1 <arhart555@gmail.com>
2021-10-19 17:37:09 -0400

    Update documentation, need to update return.

--- functions/get_dwindow.R
+++ functions/get_dwindow.R
@@ -1,16 +1,22 @@
-# Function for reducing data to the window of interest
-# for feeding into the assessment model
+#' @title Cut data to window of interest
+#' @description  Reduce data to the window of interest (subsequently passed into the assessment model)
+#'
+#' @param data A list of data to be windowed ??? structure???
+#' @param starty A number specifying the year to start passing data (i.e. first year in window)
+#' @param endy A number specifying the year to stop passing data (i.e. last year in window)
+#' 
+#' @return 
+#' 
+#' @family ???
+#' 
 
-# x: list of data to be windowed
-# starty: year to start passing data
-# endy: year to stop passing data
-
-get_dwindow <- function(data, starty, endy){
+get_dwindow <- function(data, 
+                        starty, 
+                        endy){
   w <- window(data, start=starty, end=endy)
   attr(w, 'tsp') <- NULL
   
   return(w)
-  
 }
 
 
