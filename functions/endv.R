860bdb14e8a52e101076c6c4352bd0fc1c8c7557
ahart1 <arhart555@gmail.com>
2021-10-19 18:02:43 -0400

    Add documentation, need to update family.

--- functions/endv.R
+++ functions/endv.R
@@ -1,12 +1,12 @@
-
-
-# Function to return the last value of a vector (just a space-saver). In some
-# cases x is a matrix of 1 column, in which case it needs to first be
-# converted to a vector before using the tail function.
-# 
-# x: the vector for which you want the last value
-
-
+#' @title Return last value 
+#' @description Return the last value of a vector (just a space-saver). 
+#' 
+#' @param x A vector or a single-column matrix for which you want the last value.
+#' 
+#' @return The last item in the vector or matrix
+#' 
+#' @family ???
+#' 
 
 endv <- function(x){
   
@@ -17,10 +17,4 @@ endv <- function(x){
   }
   
   return(tail(x, 1))
-  
 }
-
-
-
-
-
