4790a3f3a94c6c55bbff452de9e80e01facb6b22
ahart1 <arhart555@gmail.com>
2021-10-19 17:45:38 -0400

    Add documentation, check reference and family.

--- functions/get_stability.R
+++ functions/get_stability.R
@@ -1,19 +1,22 @@
-
-# Function created to assess stability -- Dichmont et al. Fisheries Res
-# 81:235-245 used (nearly) this equation to assess catch stability. They
-# accounted for a discount rate however. Represents the percent change in
-# landed catches from year-to-year.
-
-
+#' @title Assess catch stability
+#' @description Assess catch stability per the methods of Dichmont et al. ??? year??? Fisheries Res 81:235-245, excluding accounting for discount rate. Stability represented as the percent change in landed catches from year-to-year.
+#'
+#' @param x A vector of catch numbers.
+#' 
+#' @return A proportion between 0 and 1??? check how this is used and that this is bounded by 0/1
+#' 
+#' @family ???
+#' 
+#' @examples 
+#' get_stability(rnorm(10000, 100, 2))
+#' get_stability(rnorm(10000, 100, 12))
+#' 
 
 get_stability <- function(x){
+  
   ny <- length(x)
   num <- sum(abs(x[2:ny] - x[1:(ny-1)]), na.rm=TRUE)
   den <- sum(x[2:ny], na.rm=TRUE)
+  
   return(num / den)
 }
-
-
-# get_stability(rnorm(10000, 100, 2))
-# get_stability(rnorm(10000, 100, 12))
-
