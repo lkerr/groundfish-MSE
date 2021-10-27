e32bdac6bb8dcc03c78c0dd2d6f428f0c242b371
Mackenzie Mazur <mmazur@neptune.org>
2021-10-27 11:05:46 -0400

    Moved calc_pstar function to its own script (not in get_nextF script anymore).

--- functions/managementProc/get_nextF.R
+++ functions/managementProc/get_nextF.R
@@ -202,24 +202,6 @@ get_nextF <- function(parmgt, parpop, parenv, RPlast, evalRP, stockEnv){
         
       catchproj<-c(median(catchproj[,1]),median(catchproj[,2]))
       if(tolower(parmgt$HCR) == 'pstar'){
-        calc_pstar = function(maxp, relB)#function to calculate P* based on SSB/SSBmsy
-        {
-          if(relB>=1) #at asymptote
-          {
-            P = maxp
-          }
-          else if(relB<=0.1)
-          {
-            P = 0.0
-          }
-          else
-          {
-            slope <- (maxp)/(1-0.1)
-            inter <- maxp-slope
-            P = inter+slope*relB
-          }
-          return(P)
-        }
         P<-calc_pstar(0.4,tail(parpop$SSBhat,1)/BThresh)
         CV<-1
         calc_ABC <- function(OFL, P, CV)
