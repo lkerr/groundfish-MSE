b4bbc7ee161e414511da5f0ca77e352cd97b06f0
MackenzieMazur <mmazur@gmri.org>
2021-11-23 15:16:19 -0500

    Fixed Plot code for Fmsy error plot

--- postprocessing/Plots/FMSYErrorPlot.R
+++ postprocessing/Plots/FMSYErrorPlot.R
@@ -5,6 +5,7 @@ Scenarios<-c(7,8,9)
 wd<-"C:/Users/mmazur/Desktop/COCA_Sims"
 #List what is being compared
 comparison<-c('Ramp','F-step','Constrained ramp')
+
 ####Set up files####
 library(matrixStats)
 library(dplyr)
@@ -47,7 +48,8 @@ Df2$Time<-'Long-term'
 Df<-full_join(Df,Df2)
 Df$HCR<-Scenarios[1]
 
-setwd(paste(wd,"/Sim_",Scenarios[2],"/sim",sep=""))
+for (m in 2:length(comparison)){
+setwd(paste(wd,"/Sim_",Scenarios[m],"/sim",sep=""))
 sims <- list.files()
 
 Fratiot<-matrix(NA,nrow=length(sims),ncol=22)
@@ -82,90 +84,14 @@ Df3$Fratiot<-Fratiotl
 Df3$Fratiotl<-NULL
 Df3$Time<-'Long-term'
 Df2<-full_join(Df2,Df3)
-Df2$HCR<-Scenarios[2]
+Df2$HCR<-Scenarios[m]
 Df<-full_join(Df,Df2)
-
-setwd(paste(wd,"/Sim_",Scenarios[3],"/sim",sep=""))
-sims <- list.files()
-
-Fratiot<-matrix(NA,nrow=length(sims),ncol=22)
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
-}
-sims<-na.omit(sims)
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  Fratiot[k,]<-omvalGlobal[[1]]$FPROXY[169:190]/omvalGlobal[[1]]$FPROXYT2[169:190]
 }
 
-Fratiots<-rowMedians(Fratiot[,1:5])
-Df2<-as.data.frame(Fratiots)
-Df2$Fratiot<-Fratiots
-Df2$Fratiots<-NULL
-Df2$Time<-'Short-term'
-
-Fratiotm<-rowMedians(Fratiot[,6:10])
-Df3<-as.data.frame(Fratiotm)
-Df3$Fratiot<-Fratiotm
-Df3$Fratiotm<-NULL
-Df3$Time<-'Medium-term'
-Df2<-full_join(Df2,Df3)
-
-Fratiotl<-rowMedians(Fratiot[,11:21])
-Df3<-as.data.frame(Fratiotl)
-Df3$Fratiot<-Fratiotl
-Df3$Fratiotl<-NULL
-Df3$Time<-'Long-term'
-Df2<-full_join(Df2,Df3)
-Df2$HCR<-Scenarios[3]
-Df<-full_join(Df,Df2)
-
-setwd(paste(wd,"/Sim_",Scenarios[4],"/sim",sep=""))
-sims <- list.files()
-
-Fratiot<-matrix(NA,nrow=length(sims),ncol=22)
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
+for (i in 1:length(comparison)){
+  Df$HCR[Df$HCR==Scenarios[i]]<-comparison[i]
 }
-sims<-na.omit(sims)
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  Fratiot[k,]<-omvalGlobal[[1]]$FPROXY[169:190]/omvalGlobal[[1]]$FPROXYT2[169:190]
-}
-
-Fratiots<-rowMedians(Fratiot[,1:5])
-Df2<-as.data.frame(Fratiots)
-Df2$Fratiot<-Fratiots
-Df2$Fratiots<-NULL
-Df2$Time<-'Short-term'
-
-Fratiotm<-rowMedians(Fratiot[,6:10])
-Df3<-as.data.frame(Fratiotm)
-Df3$Fratiot<-Fratiotm
-Df3$Fratiotm<-NULL
-Df3$Time<-'Medium-term'
-Df2<-full_join(Df2,Df3)
-
-Fratiotl<-rowMedians(Fratiot[,11:21])
-Df3<-as.data.frame(Fratiotl)
-Df3$Fratiot<-Fratiotl
-Df3$Fratiotl<-NULL
-Df3$Time<-'Long-term'
-Df2<-full_join(Df2,Df3)
-Df2$HCR<-Scenarios[4]
-Df<-full_join(Df,Df2)
-
-Df$HCR[Df$HCR==Scenarios[1]]<-comparison[1]
-Df$HCR[Df$HCR==Scenarios[2]]<-comparison[2]
-Df$HCR[Df$HCR==Scenarios[3]]<-comparison[3]
-Df$HCR[Df$HCR==Scenarios[4]]<-comparison[4]
-df$HCR<-as.factor(df$HCR)
+Df$HCR<-as.factor(Df$HCR)
 Df$HCR<-ordered(Df$HCR,levels=comparison)
 
 ggplot(Df)+
