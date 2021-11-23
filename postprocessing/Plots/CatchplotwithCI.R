521628d587348f2c88652d16d8cc648fdd7855ca
MackenzieMazur <mmazur@gmri.org>
2021-11-23 15:00:38 -0500

    Fixed Catch CI Plot

--- postprocessing/Plots/CatchplotwithCI.R
+++ postprocessing/Plots/CatchplotwithCI.R
@@ -28,18 +28,19 @@ sims<-na.omit(sims)
 
 for (k in 1:length(sims)){
   load(sims[k])
-  sumCWratiots[,k]<-omvalGlobal$codGOM$sumCW[169:190]
+  sumCWratiots[,k]<-omvalGlobal[[1]]$sumCW[169:length(omvalGlobal[[1]]$sumCW)]
 }
 
 D<-as.data.frame(sumCWratiots)
-D$Year<-2019:2040
+D$Year<-2019:((length(omvalGlobal[[1]]$sumCW)-169)+2019)
 Df<- D %>% gather(Year, sumCW, 1:(length(D)-1))
-Df$Year<-rep(2019:2040,(length(D)-1))
+Df$Year<-rep(2019:((length(omvalGlobal[[1]]$sumCW)-169)+2019),(length(D)-1))
 Df<-na.omit(Df)
 Df <- ddply(Df, "Year", summarise, median = MedianCI(sumCW)[1], CI_lower=MedianCI(sumCW)[2], CI_upper=MedianCI(sumCW)[3])
 Df$HCR<-Scenarios[1]
 
-setwd(paste(wd,"/Sim_",Scenarios[2],"/sim",sep=""))
+for (m in 2:length(comparison)){
+setwd(paste(wd,"/Sim_",Scenarios[m],"/sim",sep=""))
 sims <- list.files()
 
 sumCWratiots<-matrix(NA,ncol=length(sims),nrow=22)
@@ -52,73 +53,23 @@ sims<-na.omit(sims)
 
 for (k in 1:length(sims)){
   load(sims[k])
-  sumCWratiots[,k]<-omvalGlobal$codGOM$sumCW[169:190]
+  sumCWratiots[,k]<-omvalGlobal[[1]]$sumCW[169:length(omvalGlobal[[1]]$sumCW)]
 }
 
 D2<-as.data.frame(sumCWratiots)
-D2$Year<-2019:2040
+D2$Year<-2019:((length(omvalGlobal[[1]]$sumCW)-169)+2019)
 Df2<- D2 %>% gather(Year, sumCW, 1:(length(D2)-1))
-Df2$Year<-rep(2019:2040,(length(D2)-1))
+Df2$Year<-rep(2019:((length(omvalGlobal[[1]]$sumCW)-169)+2019),(length(D2)-1))
 Df2<-na.omit(Df2)
 Df2 <- ddply(Df2, "Year", summarise, median = MedianCI(sumCW)[1], CI_lower=MedianCI(sumCW)[2], CI_upper=MedianCI(sumCW)[3])
-Df2$HCR<-Scenarios[2]
+Df2$HCR<-Scenarios[m]
 Df<-full_join(Df,Df2)
-
-setwd(paste(wd,"/Sim_",Scenarios[3],"/sim",sep=""))
-sims <- list.files()
-
-sumCWratiots<-matrix(NA,ncol=length(sims),nrow=22)
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
-}
-sims<-na.omit(sims)
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  sumCWratiots[,k]<-omvalGlobal$codGOM$sumCW[169:190]
 }
 
-D2<-as.data.frame(sumCWratiots)
-D2$Year<-2019:2040
-Df2<- D2 %>% gather(Year, sumCW, 1:(length(D2)-1))
-Df2$Year<-rep(2019:2040,(length(D2)-1))
-Df2<-na.omit(Df2)
-Df2 <- ddply(Df2, "Year", summarise, median = MedianCI(sumCW)[1], CI_lower=MedianCI(sumCW)[2], CI_upper=MedianCI(sumCW)[3])
-Df2$HCR<-Scenarios[3]
-Df<-full_join(Df,Df2)
-
-setwd(paste(wd,"/Sim_",Scenarios[4],"/sim",sep=""))
-sims <- list.files()
-
-sumCWratiots<-matrix(NA,ncol=length(sims),nrow=22)
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
+for (i in 1:length(comparison)){
+  Df$HCR[Df$HCR==Scenarios[i]]<-comparison[i]
 }
-sims<-na.omit(sims)
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  sumCWratiots[,k]<-omvalGlobal$codGOM$sumCW[169:190]
-}
-
-D2<-as.data.frame(sumCWratiots)
-D2$Year<-2019:2040
-Df2<- D2 %>% gather(Year, sumCW, 1:(length(D2)-1))
-Df2$Year<-rep(2019:2040,(length(D2)-1))
-Df2<-na.omit(Df2)
-Df2 <- ddply(Df2, "Year", summarise, median = MedianCI(sumCW)[1], CI_lower=MedianCI(sumCW)[2], CI_upper=MedianCI(sumCW)[3])
-Df2$HCR<-Scenarios[4]
-Df<-full_join(Df,Df2)
-
-Df$HCR[Df$HCR==Scenarios[1]]<-comparison[1]
-Df$HCR[Df$HCR==Scenarios[2]]<-comparison[2]
-Df$HCR[Df$HCR==Scenarios[3]]<-comparison[3]
-Df$HCR[Df$HCR==Scenarios[4]]<-comparison[4]
-df$HCR<-as.factor(df$HCR)
+Df$HCR<-as.factor(Df$HCR)
 Df$HCR<-ordered(Df$HCR,levels=comparison)
 
 #colorblind friendly plot
