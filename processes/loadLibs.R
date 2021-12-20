42304c90ed6384be0f2d6fa038a99e3d9703453b
ahart1 <arhart555@gmail.com>
2021-12-20 17:22:39 -0500

    Comment out lines no longer used, once changes tested this file can be removed.

--- processes/loadLibs.R
+++ processes/loadLibs.R
@@ -1,38 +1,38 @@
 
-# load the necessary libraries. Same libraries are loaded
-# whether using Windows or Linux but a little less complicated
-# to not keep a local copy of corresponding Windows R
-# packages on the machine. An Rlib directory will be
-# necessary for running on the ghpcc. Note that the Rlib
-# directory is located one level outside the home directory
-# (i.e., it is not include in any of the github file
-# transfers).
-
-
-if(runClass == 'HPCC'){
-
-  # seems you need to load gmm prior to loading tmvtnorm
-  require(gmm, lib.loc='../Rlib/')
-  require(mvtnorm, lib.loc='../Rlib/')
-  require(tmvtnorm, lib.loc='../Rlib/')
-
-  require(expm, lib.loc='../Rlib/')
-  require(msm, lib.loc='../Rlib/')
-  require(Matrix, lib.loc='../Rlib/')
-  require(TMB, lib.loc='../Rlib/')
-  require(abind, lib.loc='../Rlib/')
-  require(forcats, lib.loc='../Rlib/')
-  require(readr, lib.loc='../Rlib/')
-  require(tidyverse, lib.loc='../Rlib/')
-  require(dplyr, lib.loc='../Rlib/')
-  require(data.table, lib.loc='../Rlib/')
-  require(ASAPplots, lib.loc = '../Rlib')
-  require(fishmethods, lib.loc = '../Rlib')
-  require(timeSeries, lib.loc = '../Rlib')
-  require(fBasics, lib.loc = '../Rlib')
-  require(fGarch, lib.loc = '../Rlib')
-
-} # else{ # Now loaded with R package
+# # load the necessary libraries. Same libraries are loaded
+# # whether using Windows or Linux but a little less complicated
+# # to not keep a local copy of corresponding Windows R
+# # packages on the machine. An Rlib directory will be
+# # necessary for running on the ghpcc. Note that the Rlib
+# # directory is located one level outside the home directory
+# # (i.e., it is not include in any of the github file
+# # transfers).
+# 
+# 
+# if(runClass == 'HPCC'){
+# 
+#   # seems you need to load gmm prior to loading tmvtnorm
+#   require(gmm, lib.loc='../Rlib/')
+#   require(mvtnorm, lib.loc='../Rlib/')
+#   require(tmvtnorm, lib.loc='../Rlib/')
+# 
+#   require(expm, lib.loc='../Rlib/')
+#   require(msm, lib.loc='../Rlib/')
+#   require(Matrix, lib.loc='../Rlib/')
+#   require(TMB, lib.loc='../Rlib/')
+#   require(abind, lib.loc='../Rlib/')
+#   require(forcats, lib.loc='../Rlib/')
+#   require(readr, lib.loc='../Rlib/')
+#   require(tidyverse, lib.loc='../Rlib/')
+#   require(dplyr, lib.loc='../Rlib/')
+#   require(data.table, lib.loc='../Rlib/')
+#   require(ASAPplots, lib.loc = '../Rlib')
+#   require(fishmethods, lib.loc = '../Rlib')
+#   require(timeSeries, lib.loc = '../Rlib')
+#   require(fBasics, lib.loc = '../Rlib')
+#   require(fGarch, lib.loc = '../Rlib')
+# 
+# } # else{ # Now loaded with R package
 # 
 #   # check.packages function: install and load multiple R packages.
 #   # Check to see if packages are installed. Install them if they are not,
