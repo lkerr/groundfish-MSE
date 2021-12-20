8e316b1a086ccff1eab6a861e33bb9dab2908cc7
ahart1 <arhart555@gmail.com>
2021-12-20 17:17:19 -0500

    Turn this into a function, it is unclear where the output of this function is used.

--- processes/setupEconType.R
+++ processes/setupEconType.R
@@ -1,24 +1,74 @@
-# this small file controls some economic options that may vary with the mproc lines.  For example:
-  # in the "validation" model runs, we use the "post" production regression, the  "pre" choice equations, post multipliers, prices.  
-  # in the counterfactual model runs, we use the "pre" production regression, the  "pre" choice equations, post multipliers, prices.   
-  # LandZero is unused
+#' @title Setup Economic Options
+#' @description Pull economic options from mproc file for specified management procedure (may vary with the mproc lines).  
+#' For example:
+#' in the "validation" model runs, we use the "post" production regression, the  "pre" choice equations, post multipliers, prices.  
+#' in the counterfactual model runs, we use the "pre" production regression, the  "pre" choice equations, post multipliers, prices.   
+#' LandZero is unused
+#' 
+#' @template global_mproc
+#' @template global_m
+#' 
+#' #' itemize{
+#'   \item{econ_data_stub - ???????}
+#'   \item{production_vars - A vector of strings indicating names of production variables to use}
+#'   \item{spstock_equation - A vector of strings indicating names used in species stock equation}
+#'   \item{choice_equation - A vector of strings indicating names used in choice equation}
+#'   \item{multipliers - A table??? of multipliers}
+#'   \item{outputprices - A table??? of output prices}
+#'   \item{inputprices - A table??? of input prices}
+#' }
+#' 
+#' @family 
+#' 
+#' @export
 
-myvars<-c("LandZero", "CatchZero","EconType","EconName","EconData","MultiplierFile","OutputPriceFile","InputPriceFile","ProdEqn","ChoiceEqn")
-econtype<-mproc[m,]
-
-econtype<-econtype[myvars]
-econ_data_stub<-econtype$EconData
-
-multiplier_loc<-econtype$MultiplierFile
-output_price_loc<-econtype$OutputPriceFile
-input_price_loc<-econtype$InputPriceFile
-
-
-# you need to fix this so it flexibly reads in the vectors
-production_vars<-get(paste0("production_vars_",econtype$ProdEqn))
-spstock_equation<-get(paste0("spstock_equation_",econtype$ChoiceEqn))
-choice_equation<-get(paste0("choice_equation_",econtype$ChoiceEqn))
-
-multipliers<-readRDS(file.path(econdatapath,multiplier_loc))
-outputprices<-readRDS(file.path(econdatapath,output_price_loc))
-inputprices<-readRDS(file.path(econdatapath,input_price_loc))
+setupEconType <- function(mproc, m){
+  
+  ##### Define independent variables in the targeting equation 
+  ### If there are different targeting equations, you can set there up here, then use their suffix in the mproc file to use these new targeting equations
+  ### example, using ChoicEqn=small in the mproc file and uncommenting the next two lines will be appropriate for a logit with just 3 RHS variables.
+  
+  ##spstock_equation_small=c("exp_rev_total", "fuelprice_distance")
+  ##choice_equation_small=c("fuelprice_len")
+  spstock_equation_pre <- c("exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
+  choice_equation_pre <- c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len")
+  
+  spstock_equation_post <- spstock_equation_pre
+  choice_equation_post <- choice_equation_pre
+  
+  ##### Define independent variables in the Production equation 
+  ### If there are different the equations, you can set there up here, then use their suffix in the mproc file to use these new targeting equations
+  ### example, using ProdEqn=tiny in the mproc file and uncommenting the next  line will be regression with 2 RHS variables and no constant.
+  # production_vars_tiny=c("log_crew","log_trip_days")
+  
+  production_vars_pre <- c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","constant")
+  production_vars_post <- c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","log_sector_acl", "constant")
+  
+  
+  ##### Subset mproc by economic variables of interest
+  myvars<-c("LandZero", "CatchZero","EconType","EconName","EconData","MultiplierFile","OutputPriceFile","InputPriceFile","ProdEqn","ChoiceEqn")
+  econtype <- mproc[m,]
+  econtype <- econtype[myvars]
+  
+  econ_data_stub <- econtype$EconData
+  
+  ##### Setup econ variables based on specification in mproc & independent variable definitions above
+  production_vars <- get(paste0("production_vars_",econtype$ProdEqn))
+  spstock_equation <- get(paste0("spstock_equation_",econtype$ChoiceEqn))
+  choice_equation <- get(paste0("choice_equation_",econtype$ChoiceEqn))
+  
+  # Read in relevant data files using provided filepaths 
+  multipliers <- readRDS(file.path(econtype$MultiplierFile))
+  outputprices <- readRDS(file.path(econtype$OutputPriceFile))
+  inputprices <- readRDS(file.path(econtype$InputPriceFile))
+  
+  # Return
+  econSetup$econ_data_stub <- econ_data_stub
+  econSetup$production_vars <- production_vars
+  econSetup$spstock_equation <- spstock_equation
+  econSetup$choice_equation <- choice_equation
+  econSetup$multipliers <- multipliers
+  econSetup$outputprices <- outputprices
+  econSetup$inputprices <- inputprices
+  return(econSetup)
+}
