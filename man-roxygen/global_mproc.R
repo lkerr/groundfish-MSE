b89093286c275b1ff218599ece885775c3f0c2fa
ahart1 <arhart555@gmail.com>
2021-12-21 13:22:55 -0500

    Update descriptions for economic inputs, in particular requires full path for files/folders rather than providing file path separately.

--- man-roxygen/global_mproc.R
+++ man-roxygen/global_mproc.R
@@ -34,10 +34,19 @@
 #'   \item{CatchZero - }
 #'   \item{EconType - }
 #'   \item{EconName - }
-#'   \item{EconData - }
-#'   \item{MultiplierFile - }
-#'   \item{OutputPriceFile - }
-#'   \item{InputPriceFile - }
-#'   \item{ProdEqn - }
-#'   \item{ChoiceEqn - }
+#'   \item{EconData - Full file path to folder containing annual targeting data files (labeled "####.Rds" for each year ####). !!! This is a change from only providing the file name to specifying the full path to folder}
+#'   \item{MultiplierFile - Full file path for multiplier data containing columns for???}
+#'   \item{OutputPriceFile - Full file path for output price data containing columns for???}
+#'   \item{InputPriceFile - Full file path for imput price data containing columns for ???}
+#'   \item{ProdEqn - A string specifying the production equation to use (see also setupEconType), options include:}
+#'   \itemize{
+#'     \item{"pre" - Use the following production variables: "log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","constant"}
+#'     \item{"post" - Use the following production variables: "log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","log_sector_acl", "constant"}
+#'   }
+#'   \item{ChoiceEqn - A string specifying the species stock equation and choice equation to use (see also setupEconType), options include:}
+#'   \itemize{
+#'     \item{"pre" - Use the following stock equation: "exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season"}
+#'     \item{"pre" - Use the following choice equation: "wkly_crew_wage", "len", "fuelprice", "fuelprice_len"}
+#'     \item{"post" - Use same stock and choice equations as for "pre"}
+#'   }
 #' }
