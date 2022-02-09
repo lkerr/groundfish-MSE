de88c7442459107d034de85164d0aff7a1414a04
mdmazur <64043006+mdmazur@users.noreply.github.com>
2022-02-09 18:58:23 -0500

    Update MohnsRhoFplot.R

--- postprocessing/Plots/MohnsRhoFplot.R
+++ postprocessing/Plots/MohnsRhoFplot.R
@@ -1,13 +1,17 @@
 #####Mohn's Rho for F Plot####
-#Scenarios<-c(6,32,58,116)
+#Lists numbers of scenarios that you want to compare here
 Scenarios<-c(7,8,9)
+#Set working directory--where the results you want to compare are stored
+wd<-"C:/Users/mmazur/Desktop/COCA_Sims"
+#List what is being compared
+comparison<-c('Ramp','F-step','Constrained ramp')
+
 ####First Sims####
 library(matrixStats)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
-setwd(paste("C:/Users/mmazur/Desktop/COCA_Sims/Sim_",Scenarios[1],"/sim",sep=""))
-#setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[1],"/sim",sep=""))
+setwd(paste(wd,"/Sim_",Scenarios[1],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -28,9 +32,8 @@ Year<-1984:2039
 df<-as.data.frame(cbind(Catchsim,Year))
 df$HCR<-Scenarios[1]
 
-####Second Sims####
- setwd(paste("C:/Users/mmazur/Desktop/COCA_Sims/Sim_",Scenarios[2],"/sim",sep=""))
-# #setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[2],"/sim",sep=""))
+for (i in 2:length(Scenarios)){
+setwd(paste(wd,"/Sim_",Scenarios[i],"/sim",sep=""))
 sims <- list.files()
 
 for (k in 1:length(sims)){
@@ -49,58 +52,10 @@ for (k in 1:length(sims)){
 Catchsim<-rowMedians(Catchsim,na.rm=T)
 Year<-1984:2039
 df2<-as.data.frame(cbind(Catchsim,Year))
-df2$HCR<-Scenarios[2]
+df2$HCR<-Scenarios[i]
 
 df<-full_join(df,df2)
-
-# ####Third Sims####
- setwd(paste("C:/Users/mmazur/Desktop/COCA_Sims/Sim_",Scenarios[3],"/sim",sep=""))
-# #setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[3],"/sim",sep=""))
-sims <- list.files()
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
 }
-sims<-na.omit(sims)
-
-Catchsim<-matrix(NA,nrow=56,ncol=length(sims))
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$Mohns_Rho_F[134:189]
-}
-
-Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-1984:2039
-df2<-as.data.frame(cbind(Catchsim,Year))
-df2$HCR<-Scenarios[3]
-df<-full_join(df,df2)
-
-# ####Fourth Sims####
- setwd(paste("C:/Users/mmazur/Box/Mackenzie_Mazur/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
-# #setwd(paste("C:/Users/jjesse/Box/HCR_Sims/Sim_",Scenarios[4],"/sim",sep=""))
-sims <- list.files()
-
-for (k in 1:length(sims)){
-  if (file.size(sims[k])==0){
-    sims[k]<-NA}
-}
-sims<-na.omit(sims)
-
-Catchsim<-matrix(NA,nrow=56,ncol=length(sims))
-
-for (k in 1:length(sims)){
-  load(sims[k])
-  Catchsim[,k]<-omvalGlobal[[1]]$Mohns_Rho_F[134:189]
-}
-
-Catchsim<-rowMedians(Catchsim,na.rm=T)
-Year<-1984:2039
-df2<-as.data.frame(cbind(Catchsim,Year))
-df2$HCR<-Scenarios[4]
-
-df<-full_join(df,df2)
 
 df$HCR[df$HCR==Scenarios[1]]<-'Ramp'
 df$HCR[df$HCR==Scenarios[2]]<-'F-step'
