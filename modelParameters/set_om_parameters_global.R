f4e135262d7f96cc14de93d020e3d4e48da9e1c8
mle2718 <Min-Yang.Lee@noaa.gov>
2022-04-13 11:34:08 -0400

    testing changes, but left with original mproc.csv

--- modelParameters/set_om_parameters_global.R
+++ modelParameters/set_om_parameters_global.R
@@ -7,11 +7,9 @@ simpleTemperature <- FALSE
 
 
 # Which management procedures csv do you want to read:
-#mprocfile<-"mproc.csv"
+mprocfile<-"mproc.csv"
 #mprocfile<-"mprocTest.csv"
-#mprocfile<-"mprocEcon_counterfactual_single.csv"
-
-mprocfile<-"mprocEcon_validate.csv"
+#mprocfile<-"mprocEcon.csv"
 
 #### Stock parameters ####
 
