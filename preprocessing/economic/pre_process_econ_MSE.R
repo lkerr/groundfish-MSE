f301b797b355257d8b0b5e2c82cf3cf77387fcbf
mle2718 <Min-Yang.Lee@noaa.gov>
2022-05-13 11:12:33 -0400

    here()ify. Change require to library. Add spstock equation lines.

--- preprocessing/economic/pre_process_econ_MSE.R
+++ preprocessing/economic/pre_process_econ_MSE.R
@@ -4,23 +4,18 @@
 #       Post-choice coefficients, 
 #       slightly adjusted data. 
 # 
-rm(list=ls())
-if(!require(readstata13)) {  
-    install.packages("readstata13")
-    require(readstata13)}
-if(!require(tidyr)) {  
-    install.packages("tidyr")
-    require(tidyr)}
-if(!require(dplyr)) {  
-    install.packages("dplyr")
-    require(dplyr)}
-if(!require(data.table)) {  
-    install.packages("data.table")
-    require(data.table)}
+here::i_am("preprocessing/economic/pre_process_econ_MSE.R")
+
+
+library(readstata13)
+library(tidyr)
+library(dplyr)
+library(data.table)
+library(here)
 
 # Setting up the data.
 # Before you do anything, you put all your "data" files into the 
-# /data/data_raw/econ folder
+# /data/data_raw/econ 
  
     # main data file: data_for_simulations_mse.dta, data_for_simulations_POSTasPOST.dta, data_for_simulations_POSTasPRE.dta
     # multipliers.dta (multipliers)
@@ -30,23 +25,56 @@ if(!require(data.table)) {
 
 # file paths for the raw and final directories
 # windows is kind of stupid, so you'll have to deal with it in some way.
-rawpath <- './data/data_raw/econ'
-savepath <- './data/data_processed/econ'
+rawpath <- here("data","data_raw","econ")
+savepath <- here("data","data_processed","econ")
 # Just a guess on your paths.  
-#rawpath <- 'C:/Users/abirken/Documents/GitHub/groundfish-MSE/data/data_raw/econ'
-#savepath <- 'C:/Users/abirken/Documents/GitHub/groundfish-MSE/data/data_processed/econ'
 
 
-            
 ###########################Make sure you have the correct set of RHS variables.
-spstock_equation=c("exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
-choice_equation=c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len")
-targeting_vars=c(spstock_equation, choice_equation)
+# Model 2 has a slightly different exp_rev_total variable.
 
-production_vars=c("log_crew","log_trip_days","log_trawl_survey_weight","log_sector_acl","primary", "secondary","constant")
+spstock_equation_prenc1=c("exp_rev_total", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
+choice_equation_prenc1=c("das_price_mean", "das_price_mean_len","wkly_crew_wage", "len", "fuelprice", "fuelprice_len")
 
-#models = c("coefsnc2", "coefs")
-models = c("coefsnc2")
+spstock_equation_pre1<-c(spstock_equation_prenc1,"constant")
+choice_equation_pre1<-choice_equation_prenc1
+
+
+spstock_equation_prenc2<-c("exp_rev_total_das", "fuelprice_distance", "distance", "mean_wind", "mean_wind_noreast", "permitted", "lapermit", "choice_prev_fish", "partial_closure", "start_of_season")
+choice_equation_prenc2<-c("wkly_crew_wage", "len", "fuelprice", "fuelprice_len")
+
+spstock_equation_pre2<-c(spstock_equation_prenc2,"constant")
+choice_equation_pre2<-choice_equation_prenc2
+
+
+#These are mostly placeholders
+spstock_equation_postnc1<-spstock_equation_prenc1
+choice_equation_postnc1<-choice_equation_prenc1
+
+spstock_equation_post1<-spstock_equation_pre1
+choice_equation_post1<-choice_equation_pre1
+
+spstock_equation_postnc2<-spstock_equation_prenc2
+choice_equation_postnc2<-choice_equation_prenc2
+
+spstock_equation_post2<-spstock_equation_pre2
+choice_equation_post2<-choice_equation_pre2
+############## End Independent variables in the targeting equation ##########################
+
+# 
+# ##############Independent variables in the Production equation ##########################
+# ### If there are different the equations, you can set there up here, then use their suffix in the mproc file to use these new targeting equations
+# ### example, using ProdEqn=tiny in the mproc file and uncommenting the next  line will be regression with 2 RHS variables and no constant.
+# # production_vars_tiny=c("log_crew","log_trip_days")
+# 
+production_vars_pre=c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","constant")
+production_vars_post=c("log_crew","log_trip_days","primary","secondary", "log_trawl_survey_weight","log_sector_acl", "constant")
+
+
+#the MSE  dataset always uses post production.
+production_vars<- production_vars_post
+# ############## End Independent variables in the Production equation ##########################
+models <-c("post_coefsnc2")
 
 ####################Locations of files. 
 
@@ -92,22 +120,24 @@ yrstub<-"econ_data"
 yearly_savename<-c("full_targeting_coefsnc2")
 
 
-source('preprocessing/economic/targeting_coeff_import.R')
-source('preprocessing/economic/production_coefs.R')
+source(here("preprocessing","economic","targeting_coeff_import.R"))
+source(here("preprocessing","economic","production_coefs.R"))
 #output prices
-source('preprocessing/economic/output_price_import.R')
-source('preprocessing/economic/multiplier_import.R')
+ 
+source(here("preprocessing","economic","output_price_import.R"))
+source(here("preprocessing","economic","multiplier_import.R"))
+
 #input prices
-source('preprocessing/economic/input_price_import.R')
+source(here("preprocessing","economic","input_price_import.R"))
 
 
 # This takes quite a while 
-
-# This takes quite a while 
-
 production_coefs<-production_outfile
 production_coefs<-readRDS(file.path(savepath, production_coefs))
 production_coefs[, post:=NULL]
-source('preprocessing/economic/import_day_limits_validation.R')
-source('preprocessing/economic/targeting_data_import.R')
+
+source(here("preprocessing","economic","import_day_limits_validation.R"))
+source(here("preprocessing","economic","targeting_data_import.R"))
+
+
 
