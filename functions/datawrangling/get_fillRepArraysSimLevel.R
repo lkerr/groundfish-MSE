bc173881e94a48f465f4fe97c81035660ffb56d6
mle2718 <Min-Yang.Lee@noaa.gov>
2022-09-13 13:43:35 -0400

    bugfix naming of variables.

--- functions/datawrangling/get_fillRepArraysSimLevel.R
+++ functions/datawrangling/get_fillRepArraysSimLevel.R
@@ -2,8 +2,8 @@
 # Borrowed from get_fillRepArrays, but simpler because my objects are in the global environment
 
 get_fillRepArraysSimLevel <- function(simulation_container){
-  simulation_container$HHI[r,m,y]<-HHI
-  simulation_container$shannon[r,m,y]<-shannon
+  simulation_container$HHI_fleet[r,m,y]<-HHI
+  simulation_container$Shannon_fleet[r,m,y]<-shannon
   return(simulation_container)
   
 }
