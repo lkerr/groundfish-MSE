8d0c158ceef4e38b0a79b56d470df22e23e889ac
mle2718 <Min-Yang.Lee@noaa.gov>
2022-09-20 15:24:39 -0400

    use kg_per_mt instead of 1000

--- functions/economic/get_bio_for_econ.R
+++ functions/economic/get_bio_for_econ.R
@@ -39,8 +39,8 @@ get_bio_for_econ=function(stock,basecase){
   biocontain$stockName<- as.character(biocontain$stockName)
   
   #convert trawlsurvery and obs_trawlsurvey from mt to kg and take logs
-  biocontain$trawlsurvey<-log(biocontain$trawlsurvey/1000)
-  biocontain$obs_trawlsurvey<-log(biocontain$obs_trawlsurvey/1000)
+  biocontain$trawlsurvey<-log(biocontain$trawlsurvey/kg_per_mt)
+  biocontain$obs_trawlsurvey<-log(biocontain$obs_trawlsurvey/kg_per_mt)
   #rename
   colnames(biocontain)[colnames(biocontain) %in% c("trawlsurvey", "obs_trawlsurvey")] <- c("ln_trawlsurvey", "ln_obs_trawlsurvey")
   
