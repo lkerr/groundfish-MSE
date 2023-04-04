9e0ca17ad4a1e2cd7e49768fd04858468baa00e6
jerellejesse <jjesse@neptune.gmri.org>
2023-04-04 17:52:52 -0400

    + notes on hydra input for RunSim

--- processes/runSim.R
+++ processes/runSim.R
@@ -85,23 +85,24 @@ for(r in 1:nrep){
 
       for(i in 1:nstock){
      # stock[[i]] <- get_J1Updates(stock = stock[[i]]) SKIP THIS WITH HYDRA
-        
-     # CONVERT TO AGES AND WRANGLE INTO CORRECT FORMAT
-        stock[[i]] <- get_lengthConvert(hydraData) # still working on a few things here
+        # CONVERT TO AGES AND WRANGLE INTO CORRECT FORMAT
+        stock[[i]] <- get_lengthConvert(stock=stock[[i]], hydraData) 
       }
       
       source('processes/withinYearAdmin.R')
-      begin_rng_holder[[yearitercounter]]<-c(r,m,y,yrs[y],.Random.seed)
+      #begin_rng_holder[[yearitercounter]]<-c(r,m,y,yrs[y],.Random.seed) # what exactly is this doing? 
 
       # if burn-in period is over... DON'T NEED THIS WITH HYDRA?
      # if(y >= fmyearIdx){
-
+      
+      # MOVE THIS LATER??
         manage_counter<-manage_counter+1 #this only gets incremented when y>=fmyearIdx
 
         for(i in 1:nstock){
           stock[[i]] <- get_advice(stock = stock[[i]]) # RUNS MP
           #stock[[i]] <- get_relError(stock = stock[[i]]) # this happens somewhere else now I think...
         }
+    } # stop year loop here to check MP loop... delete later
         
           #Construct the year-replicate index and use those to look up their values from random_sim_draw. This is currently unused.
 # NOT RUNNING ECONOMIC MODEL WITH HYDRA
