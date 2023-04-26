c8bc061cc49a619a9d06a24a2ae2a3a20ee1fed4
mle2718 <Min-Yang.Lee@noaa.gov>
2023-04-26 14:11:26 -0400

    update to new data.  make more summaries.

--- postprocessing/economic/summary_econ.R
+++ postprocessing/economic/summary_econ.R
@@ -3,15 +3,15 @@
 library(data.table)
 
 #selects most recent file path in groundfish_MSE folder, which should be resuls_2020-... if the runsim_Econonly.R has just been run 
+source('processes/identifyResultDirectory.R')
 
-file_path = file.info(list.files(full.names = T))
-file_path= rownames(file_path)[which.max(file_path$mtime)]
-
+dir.create(file.path(ResultDirectory,"summary"))
 #if that does not work then the file path needs to be copied in 
-file_path = "./results_2020-07-31-19-06-26/"
+#file_path = "./results_2020-07-31-19-06-26/"
 
-#selects just the econ_2020 datasets
-file_names = list.files(path=file.path(file_path, "econ","raw"), pattern = "econ_2020", full.names = TRUE)
+#selects just the econ_2023 datasets
+file_names = list.files(path=file.path(ResultDirectory,"econ", "raw"), 
+                        pattern = "econ_2023", full.names = TRUE)
 
 #binding into a data.table
 simulations = (lapply(file_names, fread, stringsAsFactors = FALSE))
@@ -26,7 +26,7 @@ daily_summary = sim[sim$nreps==1]
 daily_summary [ ,c("id","hullnum", "y") := NULL]
 daily_summary = daily_summary [, lapply(.SD, sum, na.rm=TRUE), by=list(year, doffy, spstock2, gearcat, nreps, model)]
 
-write.csv(daily_summary, paste0(file_path, "daily_summary.csv"))
+write.csv(daily_summary, file.path(ResultDirectory,"summary","daily_summary.csv"))
 
 #monthly resolution data 
 
@@ -38,7 +38,7 @@ sim[ ,c("id","hullnum", "doffy", "y") := NULL]
 
 monthly_summary = sim[, lapply(.SD, sum, na.rm=TRUE), by=list(year, month, spstock2, gearcat, nreps, model)]
 
-write.csv(monthly_summary, paste0(file_path, "monthly_summary.csv"))
+write.csv(daily_summary, file.path(ResultDirectory,"summary","monthly_summary.csv"))
 
 
 
