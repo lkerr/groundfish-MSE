6fd1b5718f4197895170761901705641d78d236f
gavinfay <gfay42@gmail.com>
2023-05-19 19:26:01 -0400

    fixes rec_dev assignment

--- functions/hydra/get_hydra.R
+++ functions/hydra/get_hydra.R
@@ -72,7 +72,7 @@ get_hydra <- function(newseed=404,newdata=list(bs_temp=c(),F_full=c(),rec_devs=c
   if(!is.null(newdata$rec_devs))
   {
     rec_devs <- matrix(newdata$rec_devs,nrow=MSEyr,ncol=hydra_data$Nspecies)
-    hydra_data$recruitment_devs <- as.vector(t(cbind(matrix(hydra_data$recruitment_devs,nrow=(hydra_data$Nspecies),byrow = TRUE),rec_devs)))
+    hydra_data$recruitment_devs <- as.vector(t(cbind(matrix(hydra_data$recruitment_devs,nrow=(hydra_data$Nspecies),byrow = TRUE),t(rec_devs))))
   }
   #############################################################################
   #Start of Emily's code, it runs hydra and pulls data files:
