707f4a339a171c04de47813ad2563150654fc68c
ahart1 <arhart555@gmail.com>
2023-05-26 16:49:20 -0400

    Update .Dat file read-in, add draft code for checking convergence and updated res as in get_ASAP)

--- functions/assessment/get_WHAM.R
+++ functions/assessment/get_WHAM.R
@@ -1,16 +1,49 @@
-get_WHAM <- function(stock...){
+#' @title get_WHAM
+#' @description Run Woods Hole Assessment Model (WHAM) executable and produce results object
+#' 
+#' @param stock
+#' @param ... Additional stock parameters used in WHAM setup that are not otherwise specified in the .Dat file
+#' 
+#' @return
+
+get_WHAM <- function(stock, ...){
   
 
     # Run the wham model
     #devtools::install_github("timjmiller/wham", dependencies=TRUE, ref="devel")
     library(wham)
-    #read in saved ASAP file with wham function 
-    wham_dat_file <- read_asap3_dat()
-    input <- prepare_wham_input(wham_dat_file) #more settings here, testing needed
-    whamEst <- fit_wham(input, do.osa=T)
+  
+  # Read in saved ASAP .Dat file with wham function based on operating system
+  if (Sys.info()['sysname'] == "Windows") {
+    wham_dat_file <- read_asap3_dat('assessment/ASAP/ASAP3.dat')
+  } else if (Sys.info()['sysname'] == "Linux") {
+    wham_dat_file <- read_asap3_dat(paste(rundir, '/ASAP3.dat', sep = ''))
+  } 
+  
+  # Prepare wham input
+  input <- prepare_wham_input(wham_dat_file, ...) #more settings here, testing needed
+  
+  # Fit wham model
+  whamEst <- fit_wham(input, do.osa=T)
     
-    #save results from wham
-    saveRDS(whamEst, file = paste("Assessment/WHAM/", stock,'_', t, '_', y, '.rdat', sep = ''))
+  #save results from wham
+  saveRDS(whamEst, file = paste("Assessment/WHAM/", stock,'_', t, '_', y, '.rdat', sep = ''))
+  
+  # Update asapEst and res objects for WHAM? Need something to fill this role and either store the same type of information here and have get_WHAM return the same type of iformation as get_ASAP
+    # or update stock to have wham-specific storage (ASAP code copied here but needs updating)
+  out <- within(stock, {
+    # Run the ASAP assessment model
+    asapEst <- try(system('assessment/ASAP/ASAP3.exe', show.output.on.console = FALSE)) # I think this is convergence criteria
+    
+    # Example of convergence check for wham
+    check <- check_convergence(whamEst, ret=TRUE) # May want to suppress printing to screen using sink()
+    whamConverge <- ifelse((check$na_sdrep == FALSE & check$is_sdrep == TRUE & check$convergence == 0), TRUE, FALSE) # If no NAs in sdrep, hessian invertible and model thinks it is converged (small gradient) then model converged
+    
+    # Read in results
+    res <- dget('assessment/ASAP/ASAP3.rdat')
+    
+  }) # Close stock object
+  
     
     #pull data for wham (modified from wham github R/wham_plots_tables.R)
     std <-summary(whamEst$sdrep)
@@ -65,5 +98,6 @@ get_WHAM <- function(stock...){
   
   #catch projections
   
+  # Return !!!! still needed
   
 }
