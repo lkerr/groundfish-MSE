22aceb2ed49a247a1d8633605e99fa16d5cf4999
gavinfay <gfay42@gmail.com>
2023-06-12 00:12:05 -0400

    re-adds metric plots

---
+++ postprocessing/Plots/create-output-for-viewer.R
@@ -0,0 +1,313 @@
+library(tidyverse)
+library(ggdist)
+library(fmsb)
+
+source("R/radar_chart.R")
+source("R/get_perfmetrics.R")
+
+settings <- NULL
+settings$assessType = "stock complex"
+settings2 <- settings
+settings2$assessType = "single species"
+#results <- readRDS("~/research/groundfish-MSE/results_2023-06-11-13-20-09/sim/mpres_stock_complex.rds")
+complexes <- tibble(isp = 1:10,
+                    complex = c(1, 3, 3, 1, 2, 1, 1, 2, 1, 2))
+#my_metrics <- get_perfmetrics(results, complexes, settings)
+
+### create a tibble that is the main table with the labels for the scenarios, mps, etc., 
+# and the corresponding complexes & settings, & files with the results from the MSE
+#basically this is a main table that can be used to create the results tables for plots/tables/shiny viewer.
+res_setup <- tibble(
+settings = list(settings2, settings),
+complexes = list(complexes, complexes),
+mp = c("single species", "stock complex"),
+scenario = c("lowB","lowB"),
+files = c("~/research/groundfish-MSE/results_2023-06-11-18-45-34/sim/mpres_single_species.rds",
+                  #"~/research/groundfish-MSE/results_2023-06-11-13-20-09/sim/mpres_stock_complex.rds",  # 2 replicates, stock complex, lowB
+          "~/research/groundfish-MSE/results_2023-06-11-18-23-49/sim/mpres_stock_complex.rds"), # 10 replicates, stock complex, lowB
+)
+
+
+res_metrics <- res_setup %>% 
+  mutate(results = map(files, readRDS))
+res_metrics <- res_metrics %>% 
+  mutate(metric_bundle = pmap(list(results = res_metrics$results,
+                                   complexes = res_setup$complexes,
+                                   settings = res_setup$settings),
+                              #list(results, complexes, settings),
+                              get_perfmetrics)) %>% 
+  select(mp, scenario, metric_bundle) %>% 
+  # mutate(metrics = map(metric_bundle, "metrics"),
+  #        median_metrics = map(metric_bundle, "median_metrics"),
+  #        sp_timeseries = map(metric_bundle, "sp_timeseries"),
+  #        fleet_catch = map(metric_bundle, "fleet_catch"))
+  I()
+saveRDS(res_metrics, file = "output/res_metrics.rds")
+#res_metrics
+
+
+########## plotting #############
+
+res_metrics <- readRDS("output/res_metrics.rds") %>% 
+  mutate(metrics = map(metric_bundle, "metrics"),
+         median_metrics = map(metric_bundle, "median_metrics"),
+         sp_timeseries = map(metric_bundle, "sp_timeseries"),
+         fleet_catch = map(metric_bundle, "fleet_catch"))
+
+# metrics (by replicate)
+metrics <- res_metrics %>% 
+  select(mp, scenario, metrics) %>% 
+  unnest(metrics)
+#metrics
+
+# median_metrics (summarized over replicates)
+median_metrics <- res_metrics %>% 
+  select(mp, scenario, median_metrics) %>% 
+  unnest(median_metrics)
+#median_metrics
+
+# metrics (by replicate)
+sp_timeseries <- res_metrics %>% 
+  select(mp, scenario, sp_timeseries) %>% 
+  unnest(sp_timeseries) %>% 
+  select(-species) %>% 
+  I()
+
+
+sp_names <- tibble(species = 1:14, species_name=factor(c(
+"Atlantic_cod",
+"Atlantic_herring",
+"Atlantic_mackerel",
+"Goosefish",
+"Haddock",
+"Silver_hake",
+"Spiny_dogfish",
+"Winter_flounder",
+"Winter_skate",
+"Yellowtail_flounder",
+"Piscivores",
+"Benthivores",
+"Planktivores",
+"Ecosystem"),
+levels = c(
+  "Atlantic_cod",
+  "Atlantic_herring",
+  "Atlantic_mackerel",
+  "Goosefish",
+  "Haddock",
+  "Silver_hake",
+  "Spiny_dogfish",
+  "Winter_flounder",
+  "Winter_skate",
+  "Yellowtail_flounder",
+  "Piscivores",
+  "Benthivores",
+  "Planktivores",
+  "Ecosystem"), ordered = TRUE))
+
+key_metrics <- c("cat_yield_Ecosystem",
+                 "fleet_yield_1",
+                 "fleet_yield_2",
+                 "iav_catch_Ecosystem",
+                 "prop_below_lim",
+                 "eco_above_bmsy",
+                 "near_btarg_Piscivores",
+                 "near_btarg_Benthivores",
+                 "near_btarg_Planktivores",
+                 "f_reduced")
+
+
+# box plots for key metrics
+bxp1 <- metrics %>% 
+  filter(metric %in% key_metrics,
+         time == "long") %>% 
+  ggplot() +
+  aes(x = mp, y = value, fill = scenario) +
+  geom_boxplot(outlier.shape=NA) +
+  scale_fill_brewer(type = "qual", palette = 2) +
+  ylim(0,NA) +
+  facet_wrap(~metric, scales = "free_y", drop=F) +
+  theme_bw() +
+  labs(x = "management alternative", y = "value",
+       title = "key metrics over the long-term") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+bxp1
+ggsave("output/2023-06-11/metric_long_boxplots.png", bxp1)
+
+# box plots for key metrics, short-term
+bxp2 <- metrics %>% 
+  filter(metric %in% key_metrics,
+         time == "short") %>% 
+  ggplot() +
+  aes(x = mp, y = value, fill = scenario) +
+  geom_boxplot(outlier.shape=NA) +
+  scale_fill_brewer(type = "qual", palette = 2) +
+  ylim(0,NA) +
+  facet_wrap(~metric, scales = "free_y", drop=F) +
+  theme_bw() +
+  labs(x = "management alternative", y = "value",
+       title = "key metrics over the short-term") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+bxp2
+ggsave("output/2023-06-11/metric_short_boxplots.png", bxp2)
+
+# box plots for key metrics, single OM, both time horizons
+bxp3 <- metrics %>% 
+  filter(metric %in% key_metrics,
+         scenario == "lowB") %>% 
+  mutate(time = factor(time, levels=c("short","long"))) %>% 
+  ggplot() +
+  aes(x = mp, y = value, fill = time) +
+  geom_boxplot(outlier.shape=NA) +
+  scale_fill_brewer(type = "qual", palette = 2) +
+  ylim(0,NA) +
+  facet_wrap(~metric, scales = "free_y", drop=F) +
+  theme_bw() +
+  labs(x = "management alternative", y = "value",
+       title = "key metrics for the lowB operating model scenario") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+bxp3
+ggsave("output/2023-06-11/metric_long_boxplots.png", bxp3)
+
+
+# dot plots for medians of key metrics - long-term
+dotp1 <- median_metrics %>% 
+  filter(metric %in% key_metrics,
+         time == "long") %>% 
+  ggplot() +
+  aes(x = mp, y = value, col = mp, shape = scenario) +
+  geom_point() +
+  geom_line(aes(x = mp, y = value, group = scenario), col = "gray") +
+  geom_point() + 
+  scale_color_brewer(type = "qual", palette = 2) +
+  facet_wrap(~metric, scale = "free_y", drop = F) +
+  ylim(0,NA) +
+  ylab("value") +
+  xlab("management alternative") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        axis.text.x = element_blank()) +
+  labs(fill = "",
+       col = "",
+       title = "key metrics in the long-term over all operating model scenarios") +
+  guides(col = guide_legend(nrow = 2),
+         shape = guide_legend(nrow = 2))
+dotp1  
+ggsave("output/2023-06-11/metric_long_dotplots.png", dotp1)
+
+# dot plots for medians of key metrics - long-term
+dotp2 <- median_metrics %>% 
+  filter(metric %in% key_metrics,
+         time == "short") %>% 
+  ggplot() +
+  aes(x = mp, y = value, col = mp, shape = scenario) +
+  geom_point() +
+  geom_line(aes(x = mp, y = value, group = scenario), col = "gray") +
+  geom_point() + 
+  scale_color_brewer(type = "qual", palette = 2) +
+  facet_wrap(~metric, scale = "free_y", drop = F) +
+  ylim(0,NA) +
+  ylab("value") +
+  xlab("management alternative") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        axis.text.x = element_blank()) +
+  labs(fill = "",
+       col = "",
+       title = "key metrics in the long-term over all operating model scenarios") +
+  guides(col = guide_legend(nrow = 2),
+         shape = guide_legend(nrow = 2))
+dotp1  
+ggsave("output/2023-06-11/metric_short_dotplots.png", dotp2)
+
+
+# time series of OM biomass
+om_plot1 <- sp_timeseries %>% 
+  dplyr::filter(year > 40,
+                scenario == "lowB") %>% 
+  ggplot() +
+  aes(x = year, y = biomass, fill = mp, col = mp) +
+  stat_lineribbon(
+    #show.legend = FALSE,
+    alpha = 0.35) +
+  ylim(0,NA) +
+  labs(y = "biomass",
+       title = "true biomass for the lowB operating model scenario") +
+  facet_wrap(~species_name, scales = "free_y") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+ggsave("output/2023-06-11/biomass_timeseries.png", om_plot1)
+
+# time series of catch
+om_plot2 <- sp_timeseries %>% 
+  dplyr::filter(year > 40,
+                scenario == "lowB") %>% 
+  ggplot() +
+  aes(x = year, y = catch, fill = mp, col = mp) +
+  stat_lineribbon(
+    #show.legend = FALSE,
+    alpha = 0.35) +
+  ylim(0,NA) +
+  labs(y = "catch",
+       title = "realized catch for the lowB operating model scenario") +
+  facet_wrap(~species_name, scales = "free_y") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+ggsave("output/2023-06-11/catch_timeseries.png", om_plot2)
+
+# time series of b/bmsy
+om_plot3 <- sp_timeseries %>% 
+  dplyr::filter(year > 40,
+                scenario == "lowB") %>% 
+  ggplot() +
+  aes(x = year, y = bbmsy, fill = mp, col = mp) +
+  stat_lineribbon(
+    #show.legend = FALSE,
+    alpha = 0.35) +
+  ylim(0,NA) +
+  labs(y = "B / HCR_BMSY",
+       title = "B/BMSY for the lowB operating model scenario") +
+  geom_hline(yintercept = 1, lty=2) +
+  facet_wrap(~species_name, scales = "free_y") +
+  theme(axis.text.y = element_blank(),
+        legend.position = "bottom")
+ggsave("output/2023-06-11/bbmsy_timeseries.png", om_plot3)
+
+# radar chart of tradeoffs among key metrics for the Base operating model scenario
+# radar chart - not sure how to save this to file automagically
+key_metrics <- c("cat_yield_Ecosystem",
+                 "fleet_yield_1",
+                 "fleet_yield_2",
+                 "iav_catch_Ecosystem",
+                 "prop_below_lim",
+                 "eco_above_bmsy",
+                 # "near_btarg_Piscivores",
+                 # "near_btarg_Benthivores",
+                 # "near_btarg_Planktivores",
+                 "f_reduced")
+png(filename ="output/2023-06-11/radar_plot.png")
+radar_p1 <- median_metrics %>% 
+  filter(metric %in% key_metrics,
+         time == "long") %>% 
+  mutate(value = ifelse(metric=="prop_below_lim",1-value,value),
+         metric = ifelse(metric=="prop_below_lim","prop_above_lim",metric),
+         scenario = factor(scenario),
+         #om = fct_recode(om, "Base" = "1","MRIP Bias" = "2","Shift" = "3"),
+         ) %>% 
+  #filter(scenario == "lowB") %>% 
+  # mutate(mp = fct_recode(mp,
+  #                        "status quo" = "MP 1",
+  #                        "minsize-1" = "MP 2", 
+  #                        "season" = "MP 3", 
+  #                        "region" = "MP 4",
+  #                        #"c1@14" = "MP 5", 
+  #                        "3@17" = "MP 6",
+  #                        "1@16-19" = "MP 7",
+  #                        "slot" = "MP 8")) %>% 
+  #filter(metric %in% c("change_cs", "kept per trip", "kept:released", "not overfished", "not overfishing", "keep_one")) %>% 
+  do_small_radar_plot()
+  dev.off()
+  
--- postprocessing/Plots/get_perfmetrics.R
+++ postprocessing/Plots/get_perfmetrics.R
@@ -4,11 +4,11 @@ get_perfmetrics <- function(results, complexes = NULL, settings = list(assessTyp
   library(janitor)
   #library(ggdist)
   
-#  settings <- NULL
-#  settings$assessType = "stock complex"
-#  results <- readRDS("~/research/groundfish-MSE/results_2023-06-11-13-20-09/sim/mpres_stock_complex.rds")
-  complexes <- tibble(isp = 1:10,
-                      complex = c(1, 3, 3, 1, 2, 1, 1, 2, 1, 2))
+   # settings <- NULL
+   # settings$assessType = "stock complex"
+  #  results <- readRDS("~/research/groundfish-MSE/results_2023-06-11-13-20-09/sim/mpres_stock_complex.rds")
+  # complexes <- tibble(isp = 1:10,
+  #                     complex = c(1, 3, 3, 1, 2, 1, 1, 2, 1, 2))
   
   # fleet_sp <- tibble(species = 1:10,
   #                    fleet = c(1,2,2,rep(1,7)))
@@ -19,13 +19,13 @@ get_perfmetrics <- function(results, complexes = NULL, settings = list(assessTyp
   #mse_results$fleet_catch
   
   #calculate metrics in both short and long-term
-  yr_range <- list(short = 43:45,
-                   long = 43:48)
+  yr_range <- list(short = 43:47,
+                   long = 53:72)
   metric_calcs <- map(yr_range, ~calc_metrics(mse_results, yrs = .))
   metrics <- map_dfr(metric_calcs, function(x) x$metrics, .id = "time")
   
   median_metrics <- metrics %>% 
-    group_by(metric) %>% 
+    group_by(metric, time) %>% 
     summarize(value = median(value, na.rm = TRUE),
               .groups = "drop") %>% 
     I()
@@ -69,9 +69,9 @@ get_perfmetrics <- function(results, complexes = NULL, settings = list(assessTyp
     left_join(sp_names) %>% 
     I()
   perfmetrics$fleet_catch <- mse_results$fleet_catch
-    
+  
   return(perfmetrics)
-
+  
 } #end get_perfmetrics function
 
 
@@ -86,14 +86,14 @@ calc_metrics <- function(results, yrs = 1:10) {
   #results = mse_results
   #yrs = 43:48
   #yrs = 46:48
- 
-  cat_yield <- mse_results$sp_results %>% 
+  
+  cat_yield <- results$sp_results %>% 
     dplyr::filter(year %in% yrs) %>% 
     group_by(rep, species, year) %>% 
     summarize(
       cat_yield = sum(catch, na.rm =TRUE),
       near_btarg = sum(bbmsy > 0.75 & bbmsy < 1.25),
-              .groups = "drop") %>% 
+      .groups = "drop") %>% 
     group_by(rep, species) %>% 
     arrange(year) %>% 
     summarize(iav_catch = sqrt(sum(diff(cat_yield)^2)/(n()-1))/(sum(cat_yield)/n()),
@@ -101,9 +101,9 @@ calc_metrics <- function(results, yrs = 1:10) {
               near_btarg = sum(near_btarg)/n(),
               .groups = "drop") %>% 
     I()
-    cat_yield$near_btarg[cat_yield$species %in% 1:10] <- NA 
+  cat_yield$near_btarg[cat_yield$species %in% 1:10] <- NA 
   #cat_yield 
-  fleet_yield <- mse_results$fleet_catch %>% 
+  fleet_yield <- results$fleet_catch %>% 
     dplyr::filter(year %in% yrs) %>% 
     group_by(rep, fleet, year) %>% 
     summarize(fleet_yield = sum(catch, na.rm =TRUE),.groups = "drop") %>% 
@@ -113,7 +113,7 @@ calc_metrics <- function(results, yrs = 1:10) {
               fleet_yield = median(fleet_yield,na.rm = TRUE), .groups = "drop") %>% 
     I()
   #fleet_yield
-  prop_below <- mse_results$sp_results %>% 
+  prop_below <- results$sp_results %>% 
     dplyr::filter(year %in% yrs,
                   species %in% 1:10) %>% 
     group_by(rep, year) %>% 
@@ -125,7 +125,7 @@ calc_metrics <- function(results, yrs = 1:10) {
     summarize(prop_below_lim = sum(prop_below_lim)/n(), .groups = "drop") %>% 
     I()
   #prop_below
-  prop_overfishing <- mse_results$sp_results %>% 
+  prop_overfishing <- results$sp_results %>% 
     dplyr::filter(year %in% yrs) %>% 
     group_by(rep, species) %>% 
     summarize(prop_fabove_lim = mean(ffmsy>1, na.rm=TRUE),
@@ -133,7 +133,7 @@ calc_metrics <- function(results, yrs = 1:10) {
     drop_na() %>%  
     I()
   #prop_overfishing
-  eco_above_bmsy <- mse_results$sp_results %>% 
+  eco_above_bmsy <- results$sp_results %>% 
     dplyr::filter(year %in% yrs,
                   species == 14) %>% 
     group_by(rep) %>% 
@@ -143,7 +143,7 @@ calc_metrics <- function(results, yrs = 1:10) {
   #eco_above_bmsy
   
   #foregone yield
-  foregone <- mse_results$sp_results %>% 
+  foregone <- results$sp_results %>% 
     dplyr::filter(year %in% yrs,
                   !is.na(advice)) %>% 
     mutate(diff_cat = advice - catch) %>% 
@@ -155,8 +155,8 @@ calc_metrics <- function(results, yrs = 1:10) {
               .groups = "drop") %>% 
     I()
   #foregone
-    
-  f_reduced <- mse_results$sp_results %>% 
+  
+  f_reduced <- results$sp_results %>% 
     dplyr::filter(year %in% yrs,
                   species %in% 1:10) %>% 
     select(rep, species, year, Fyr, bfloor) %>% 
@@ -170,7 +170,7 @@ calc_metrics <- function(results, yrs = 1:10) {
     summarize(f_reduced = mean(correct_Fadjust,na.rm=TRUE)) %>% 
     #arrange(rep, species) %>% 
     I()
-
+  
   sp_names <- tibble(species = 1:14, species_name=factor(c(
     "Atlantic_cod",
     "Atlantic_herring",
@@ -202,7 +202,7 @@ calc_metrics <- function(results, yrs = 1:10) {
       "Planktivores",
       "Ecosystem")))
   
-    
+  
   metrics1 <- f_reduced %>% 
     left_join(foregone) %>% 
     left_join(eco_above_bmsy) %>% 
@@ -236,8 +236,8 @@ calc_metrics <- function(results, yrs = 1:10) {
   return(results)
   
 } #end function calc_metrics
-  
-  
+
+
 get_mse_results <- function(results, complexes, settings = list(assessType = "stock complex")) {
   #get tables of time series by simulation
   om_bio <- map_dfr(results$biomass,I,.id = "rep") %>% 
@@ -318,7 +318,7 @@ get_mse_results <- function(results, complexes, settings = list(assessType = "st
   
   sp_results <- sp_results %>% 
     left_join(Fyr)
-
+  
   
   ###### finishes getting tables of time series output
   mse_results <- NULL
@@ -327,230 +327,3 @@ get_mse_results <- function(results, complexes, settings = list(assessType = "st
   
   return(mse_results)  
 } #ends get_mse_results() function  
-  
-  
-library(tidyverse)
-library(ggdist)
-library(reshape2)
-
-#mp_results <- readRDS("~/research/groundfish-MSE/results_2023-05-19-16-39-54/sim/mpres2023-05-19_170353_6465.rds")
-# mp_results <- readRDS("~/research/groundfish-MSE/functions/hydra/plots/sc-res-20230519.rds")
-mp_results <- `mpresstock complex`
-feeding_complexes <- tibble(isp = 1:10,
-                            complex = c(1, 3, 3, 1, 2, 1, 1, 2, 1, 2))
-gear_complexes <- tibble(isp = 1:10,
-                         complex = c(1, 3, 3, 1, 1, 3, 1, 2, 1, 2))
-
-Fyrspecies <- map_dfr(results$Fyrspecies,I,.id="rep")
-names(Fyrspecies)[4:ncol(Fyrspecies)] <-seq(1,ncol(Fyrspecies)-3)
-Fyrspecies <- reshape2::melt(Fyrspecies,id=1:3)
-names(Fyrspecies)[4:ncol(Fyrspecies)] <- c("year","Fyr")
-Fyrspecies$year <- as.double(Fyrspecies$year)
-
-biomass <- map_dfr(mp_results$biomass,I,.id = "rep") %>% 
-  janitor::clean_names()
-
-catch <- map_dfr(mp_results$catch,I,.id = "rep") %>% 
-  #select(-cv) %>% 
-  group_by(year, species, rep) %>% 
-  summarize(catch = sum(predcatch), .groups = "drop")
-
-results1 <- biomass %>% 
-  left_join(catch) %>% 
-  left_join(Fyrspecies) %>%
-  left_join(feeding_complexes, by = c("species" = "isp")) %>% 
-  mutate(mp = "stock complex MA") 
-
-#ss_results <- readRDS("~/research/groundfish-MSE/results_2023-05-19-16-08-04/sim/mpres2023-05-19_163236_5129.rds")
-#ss_results <- readRDS("~/research/groundfish-MSE/functions/hydra/plots/ss-2023-05-19.rds")
-# ss_results <- readRDS("~/research/groundfish-MSE/functions/hydra/plots/ss-2023-05-20.rds")
-# ss_results <- readRDS("/results_2023-06-08-11-50-09/sim/mpressinglespecies.rds")
-ss_results <- `mpressinglespecies`
-
-Fyrspecies <- map_dfr(results$Fyrspecies,I,.id="rep")
-names(Fyrspecies)[4:ncol(Fyrspecies)] <-seq(1,ncol(Fyrspecies)-3)
-Fyrspecies <- reshape2::melt(Fyrspecies,id=1:3)
-names(Fyrspecies)[4:ncol(Fyrspecies)] <- c("year","Fyr")
-Fyrspecies$year <- as.double(Fyrspecies$year)
-
-biomass <- map_dfr(ss_results$biomass,I,.id = "rep") %>% 
-  janitor::clean_names()
-
-catch <- map_dfr(ss_results$catch,I,.id = "rep") %>% 
-  #select(-cv) %>% 
-  group_by(year, species, rep) %>% 
-  summarize(catch = sum(predcatch), .groups = "drop")
-
-results2 <- biomass %>% 
-  left_join(catch) %>% 
-  left_join(Fyrspecies) %>%
-  left_join(feeding_complexes, by = c("species" = "isp")) %>% 
-  mutate(mp = "single species MA")
-
-results <- results1 %>% 
-  bind_rows(results2)
-
-complex_res <- results %>% 
-  select(-species) %>% 
-  group_by(rep, year, complex, mp) %>% 
-  summarize_all(sum) %>% 
-  mutate(species = complex + 10) %>% 
-  select(-complex)
-
-eco_res <- results %>% 
-  select(-species, -complex) %>% 
-  group_by(rep, year, mp) %>% 
-  summarize_all(sum) %>% 
-  mutate(species = 14)
-
-results <- results %>% 
-  bind_rows(complex_res) %>%
-  bind_rows(eco_res) %>% 
-  pivot_longer(cols = c("biomass","catch","Fyr"), names_to = "type")
-
-sp_names <- tibble(species = 1:14, species_name=factor(c(
-  "Atlantic_cod",
-  "Atlantic_herring",
-  "Atlantic_mackerel",
-  "Goosefish",
-  "Haddock",
-  "Silver_hake",
-  "Spiny_dogfish",
-  "Winter_flounder",
-  "Winter_skate",
-  "Yellowtail_flounder",
-  "Piscivores",
-  "Benthivores",
-  "Planktivores",
-  "Ecosystem"),
-  levels = c(
-    "Atlantic_cod",
-    "Atlantic_herring",
-    "Atlantic_mackerel",
-    "Goosefish",
-    "Haddock",
-    "Silver_hake",
-    "Spiny_dogfish",
-    "Winter_flounder",
-    "Winter_skate",
-    "Yellowtail_flounder",
-    "Piscivores",
-    "Benthivores",
-    "Planktivores",
-    "Ecosystem")))
-
-perf_metrics <- results %>% 
-  left_join(sp_names)
-
-bmsy <- perf_metrics %>% 
-  dplyr::filter(year <= 40, type == "biomass") %>% 
-  group_by(species, rep, mp) %>% 
-  summarize(bmsy = mean(value, na.rm=TRUE)) %>% 
-  ungroup()
-
-perf_metrics <- perf_metrics %>% 
-  left_join(bmsy) %>% 
-  mutate(bbmsy = ifelse(type == "biomass", value / bmsy, NA))
-
-p1 <- perf_metrics %>% 
-  dplyr::filter(type == "biomass",
-                year > 40) %>% 
-  ggplot() +
-  aes(x = year, y = value, fill = mp, col = mp) +
-  stat_lineribbon(
-    #show.legend = FALSE,
-    alpha = 0.35) +
-  ylim(0,NA) +
-  labs(y = "biomass") +
-  facet_wrap(~species_name, scales = "free_y") +
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom")
-ggsave("output/biomass.png", p1)
-
-p2 <- perf_metrics %>% 
-  dplyr::filter(type == "biomass",
-                year > 40) %>% 
-  ggplot() +
-  aes(x = year, y = bbmsy, fill = mp, col = mp) +
-  stat_lineribbon(
-    #show.legend = FALSE,
-    alpha = 0.35) +
-  ylim(0,NA) +
-  labs(y = "B / HCR_BMSY") +
-  geom_hline(yintercept = 1, lty=2) +
-  facet_wrap(~species_name, scales = "free_y") +
-  theme(#axis.text.y = element_blank(),
-    legend.position = "bottom")
-ggsave("output/bbmsy.png", p2)
-
-p3 <- perf_metrics %>% 
-  dplyr::filter(type == "catch",
-                year > 40) %>% 
-  ggplot() +
-  aes(x = year, y = value, fill = mp, col = mp) +
-  stat_lineribbon(
-    #show.legend = FALSE,
-    alpha = 0.35) +
-  ylim(0,NA) +
-  labs(y = "catch") +
-  facet_wrap(~species_name, scales = "free_y") +
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom")
-ggsave("output/catch.png", p3)
-
-summary_metrics <- perf_metrics %>% 
-  dplyr::filter(year >= 40) %>% 
-  group_by(type, rep, mp, species_name) %>% 
-  summarize(short = mean(value[year<52], na.rm = TRUE),
-            long = mean(value[year>62], na.rm = TRUE,)) %>% 
-  pivot_longer(cols = c("short","long"),names_to = "time")
-
-p4 <- summary_metrics %>% 
-  dplyr::filter(type == "biomass") %>% 
-  ggplot() +
-  aes(x = time, y = value, fill = mp) +
-  geom_boxplot() +
-  ylim(0,NA) +
-  facet_wrap(~species_name, scales = "free_y") +
-  labs(x = "time horizon", y = "biomass") +
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom")
-ggsave("output/bio_boxplots.png", p4)
-# summary_metrics %>% 
-#   dplyr::filter(type == "biomass") %>% 
-#   ggplot() +
-#   aes(x = time, y = bbmsy, fill = mp) +
-#   geom_boxplot() +
-#   ylim(0,NA) +
-#   geom_hline(yintercept = 1, lty=2) +
-#   facet_wrap(~species_name, scales = "free_y") +
-#   labs(x = "time horizon", y = "B / HCR BMSY") +
-#   theme(#axis.text.y = element_blank(),
-#         legend.position = "bottom")
-
-p5 <- summary_metrics %>% 
-  dplyr::filter(type == "catch") %>% 
-  ggplot() +
-  aes(x = time, y = value, fill = mp) +
-  geom_boxplot() +
-  ylim(0,NA) +
-  facet_wrap(~species_name, scales = "free_y") +
-  labs(x = "time horizon", y = "catch") +
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom")
-ggsave("output/cat_boxplots.png", p5)
-
-p6 <- perf_metrics %>% 
-  dplyr::filter(type == "Fyr",
-                year > 40) %>% 
-  ggplot() +
-  aes(x = year, y = value, fill = mp, col = mp) +
-  stat_lineribbon(
-    #show.legend = FALSE,
-    alpha = 0.35) +
-  ylim(0,NA) +
-  labs(y = "Fyr") +
-  facet_wrap(~species_name, scales = "free_y") +
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom")
-ggsave("output/Fyr.png", p6)
