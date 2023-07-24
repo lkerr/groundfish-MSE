8fdaa990fd12435f5ff240b9f9e06d4b594ead08
emilylil <eliljestrand@gmail.com>
2023-07-24 09:43:25 -0500

    Same as previous commit

--- functions/managementProc/get_proj.R
+++ functions/managementProc/get_proj.R
@@ -23,12 +23,12 @@ get_proj <- function(type, parmgt, parpop, parenv, Rfun,
     nR <- length(parpop$R)
 
     # historical R estimates over the time window specified in the file mprocfile (defined in set_om_parameters_global.R)
-    if(settings$dynamicRP==TRUE)
-    {
-    Rest <- get_dwindow(parpop$R,
-                        start = unlist(nR- (-startHCM) + 1),
-                        end = unlist(nR - (-endHCM) + 1))
-    }
+    # if(settings$dynamicRP==TRUE)
+    # {
+    # Rest <- get_dwindow(parpop$R,
+    #                     start = unlist(nR- (-startHCM) + 1),
+    #                     end = unlist(nR - (-endHCM) + 1))
+    # }
     if(settings$dynamicRP==TRUE)
     {
       Rest <- get_dwindow(parpop$R,
