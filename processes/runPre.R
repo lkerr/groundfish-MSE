53f241c2504a048010f1315569f98c7230594c9a
jerellejesse <jjesse@neptune.gmri.org>
2023-07-31 11:03:09 -0400

    + new getUSerDLL

--- processes/runPre.R
+++ processes/runPre.R
@@ -45,8 +45,9 @@ dir.create(file.path(ResultDirectory,"sim"), showWarnings = FALSE, recursive=TRU
 dir.create(file.path(ResultDirectory,"fig"), showWarnings = FALSE, recursive=TRUE)
 
 # load the required libraries
-source('processes/loadLibs.R')
-
+#source('processes/loadLibs.R')
+#source('getUserDLL.R') # loading modified version to resolve DLL conflict when using WHAM- AH & JJ
+                      # fishmethods package has TMB code that messes with WHAM
 # compile the c++ file and make available to R
 TMB::compile("assessment/caa.cpp")
 
