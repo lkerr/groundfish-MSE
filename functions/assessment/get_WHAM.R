fc64eed453175d98c5639e004cee7de3ff010235
ahart1 <arhart555@gmail.com>
2023-08-11 11:33:07 -0400

    Save Mohn's rho values, change F.report syntax so consistentent with ASAP EM.

--- functions/assessment/get_WHAM.R
+++ functions/assessment/get_WHAM.R
@@ -42,32 +42,37 @@ get_WHAM <- function(stock,...){
     input <- do.call(prepare_wham_input, c(list(asap3 = wham_dat_file), stock_wham_settings)) # need to set up a wham-settings object to pull these from - make it a list so that only list objects that matter get added - only source the wham-settings object once at the start of the file & only if using WHAM
     
     # Fit wham model
-    whamEst <- fit_wham(input, do.osa=F, MakeADFun.silent = T, do.retro = F) # Setting do.osa = TRUE results in "Error in getUserDLL() Multiple TMB models loaded" which is likely an issue with what model TMB is used by make_osa_residuals() - make_osa_resiudals() probably calls TMB::MakeADFun without specifying DLL = "wham"
+    whamEst <- fit_wham(input, do.osa=F, MakeADFun.silent = TRUE, do.retro = TRUE) # Setting do.osa = TRUE results in "Error in getUserDLL() Multiple TMB models loaded" which is likely an issue with what model TMB is used by make_osa_residuals() - make_osa_resiudals() probably calls TMB::MakeADFun without specifying DLL = "wham"
     #save results from wham
     # saveRDS(whamEst, file = paste("Assessment/WHAM/", stockName,'_', r, '_', y, '.rdat', sep = '')) #??? probably don't want to save this, save a subset of results 
     
-    
-    # Example of convergence check for wham
+    # Convergence check for wham
     check <- check_convergence(whamEst, ret=TRUE) # May want to suppress printing to screen using sink()
     whamConverge <- ifelse((check$na_sdrep == FALSE & check$is_sdrep == TRUE & check$convergence == 0), TRUE, FALSE) # If no NAs in sdrep, hessian invertible and model thinks it is converged (small gradient) then model converged
     
-    print("convergence")
-    print(whamConverge)
-    print(wham_storage$checkConvergence[[r]][[y]])
+    # Calculate Mohn's rho values
+    MohnsRho <- mohns_rho(whamEst)
     
   # Store WHAM results in final MSE output (indexed by stock i, rep r, and year y)
-  wham_storage$SSB[[r]][[y]] <- whamEst$rep$SSB
+  wham_storage$SSB[[r]][[y]] <- whamEst$rep$SSB 
   wham_storage$F[[r]][[y]] <- whamEst$rep$F
   wham_storage$FAA[[r]][[y]] <- whamEst$rep$FAA_tot
   wham_storage$R[[r]][[y]] <- whamEst$rep$NAA[,1]
   wham_storage$NAA[[r]][[y]] <- whamEst$rep$NAA
-  wham_storage$Catch[[r]][[y]] <- whamEst$rep$pred_catch
-  wham_storage$CAA[[r]][[y]] <- whamEst$rep$pred_CAA[,1,]
-  wham_storage$FMSY[[r]][[y]] <- whamEst$rep$log_FXSPR_static
-  wham_storage$SSBMSY[[r]][[y]] <- whamEst$rep$log_SSB_FXSPR_static
-  wham_storage$MSY[[r]][[y]] <- whamEst$rep$log_Y_FXSPR_static
+  wham_storage$Catch[[r]][[y]] <- c(whamEst$rep$pred_catch) # Not successfully saved in wham_storage for each assessment year
+  print("get_WHAM")
+  print(y)
+  print(wham_storage$Catch[[r]][[y]])
+  wham_storage$CAA[[r]][[y]] <- whamEst$rep$pred_CAA[,1,] 
+  wham_storage$FMSY[[r]][[y]] <- exp(whamEst$rep$log_FXSPR_static)
+  wham_storage$SSBMSY[[r]][[y]] <- exp(whamEst$rep$log_SSB_FXSPR_static)
+  wham_storage$MSY[[r]][[y]] <- exp(whamEst$rep$log_Y_FXSPR_static)
   wham_storage$SelAA[[r]][[y]] <- whamEst$rep$selAA
   wham_storage$checkConvergence[[r]][[y]] <- whamConverge
+  wham_storage$MohnsRho_SSB[[r]][[y]] <- MohnsRho["SSB"]
+  wham_storage$MohnsRho_F[[r]][[y]] <- MohnsRho["Fbar"]
+  wham_storage$MohnsRho_R[[r]][[y]] <- MohnsRho["R"]
+  wham_storage$MohnsRho_N[[r]][[y]] <- MohnsRho[grep("N", names(MohnsRho))]
   
   # Read in results
   res <- list(
@@ -78,13 +83,13 @@ get_WHAM <- function(stock,...){
     R=whamEst$rep$NAA[,1],
     SSB=whamEst$rep$SSB,
     J1N=tail(whamEst$rep$NAA,1),
-    Fhat= whamEst$rep$F,
+    F.report= whamEst$rep$F,
     catch = whamEst$rep$pred_catch
   )
   
     # !!! Maybe look at this code to pull together performance metrics/diagnostics to save
   # !!! Look at postprocessing/Plots, and functions/plotResults (auto generated) - get_plots function runs everything, prioritize this
-   
+  
   }) # Close stock object
 
   return (out)
