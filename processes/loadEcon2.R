d41f0fb350bf859c159b01b8d209cb70234a5d01
mle2718 <Min-Yang.Lee@noaa.gov>
2023-08-25 09:10:40 -0400

    construct quarterly prices from the targeting_dataset instead of from the quotaprice paper dataset.

--- processes/loadEcon2.R
+++ processes/loadEcon2.R
@@ -20,8 +20,52 @@ quarterly_prices<-quarterly_output_prices[[econ_quarterlyprice_idx_draw]]
 if ('gffishingyear' %in% colnames(quarterly_prices)){
   quarterly_prices[, gffishingyear:=NULL]
 }
-  
-  
+
+
+prices<-do.call(rbind, targeting_dataset)
+
+keep_cols<-c("MONTH","doffy", "spstock2","gearcat",grep("^p_",colnames(prices), value=TRUE)) 
+
+prices<-prices[, ..keep_cols]
+# just keep the daily prices corresponding to one target.
+prices<-prices[spstock2 %in% "codGOM",]
+
+# pick 1 row per gear-day
+prices <-prices %>%
+  group_by(gearcat,doffy) %>%
+  slice(1)%>%
+  ungroup()
+
+# cast the zeros to n/a and classify quarter  
+prices<-prices %>%
+  mutate(across(grep("^p_",colnames(prices), value=TRUE), ~ na_if(.x,0))) %>%
+  mutate(q_fy = case_when(MONTH >= 5  & MONTH <= 7 ~ 1,
+                          MONTH >= 8  & MONTH <= 10 ~ 2,
+                          MONTH >= 11  | MONTH <= 1 ~ 3,
+                          MONTH >= 2 & MONTH <= 4 ~ 4))   
+prices<-prices %>%
+  group_by(q_fy)%>%
+  summarise(across(grep("^p_",colnames(prices), value=TRUE), \(x) mean(x, na.rm = TRUE)))
+
+#Reshape to long format
+prices<-prices %>%
+  gather(spstock2, price_SFD2016,grep("^p_",colnames(prices), value=TRUE)) %>%
+  mutate(spstock2=str_replace(spstock2,"p_",""))
+
+# Join to the quarterly prices dataset
+quarterly_prices <-quarterly_prices %>%
+  dplyr::left_join(prices, by=join_by(q_fy,spstock2))%>%
+  rename(live_priceGDP_bak=live_priceGDP)
+
+# update the live prices with the (normalized) ones from the targeting dataset. 
+quarterly_prices <-quarterly_prices %>%
+  mutate(live_priceGDP=price_SFD2016/fGDPtoSFD)
+
+quarterly_prices$live_priceGDP <- ifelse(is.na(quarterly_prices$live_priceGDP), quarterly_prices$live_priceGDP_bak, quarterly_prices$live_priceGDP)
+quarterly_prices<-quarterly_prices %>%
+  select(-c(price_SFD2016, live_priceGDP_bak))
+
+
 #temp for debugging
 #targeting_dataset$prhat<-targeting_dataset$pr
 
