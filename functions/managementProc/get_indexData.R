5f79da1a87b18d07908dbc43f79ca4e20e0d0065
Mazur <Mackenzie.Mazur@dfo-mpo.gc.ca>
2026-02-02 09:49:18 -0800

    added y argument

--- functions/managementProc/get_indexData.R
+++ functions/managementProc/get_indexData.R
@@ -1,16 +1,16 @@
-get_indexData <- function(stock){
-
+get_indexData <- function(stock,y){
+  
   within(stock, {
-
+    
     sumCW[y] <- CN[y,] %*% waa[y,]    # (dot product)
-
+    
     paaCN[y,] <- (CN[y,]) / sum(CN[y,])
-   
+    
     # calculate the predicted survey index in year y and the predicted
     # survey proportions-at-age
-
+    
     IN[y,] <- get_survey(F_full=F_full[y], M=natM[y], N=J1N[y,], slxC[y,], 
-                           slxI=selI, timeI=timeI, qI=qI, 
+                         slxI=selI, timeI=timeI, qI=qI, 
                          DecCatch=DecCatch, IncCatch= IncCatch, Tanom=Tanom[y],y=y)
     sumIN[y] <- sum(IN[y,])
     sumIW[y] <- IN[y,] %*% waa[y,]
@@ -20,40 +20,40 @@ get_indexData <- function(stock){
     # calculate effort based on catchability and the implemented fishing
     # mortality. Effort not typically derived ... could go the other way
     # around and implement E as a policy and calculate F.
-
+    
     effort[y] <- F_full[y] / qC
     obs_effort[y] <- get_error_idx(type=oe_effort_typ, idx=effort[y], 
-                                     par=oe_effort)
+                                   par=oe_effort)
     # Get observation error data for the assessment model
     # change point where bias in catch is applied in 2015 before the start of
     # the projection period
-
-     if (y < c(fmyearIdx)){
-     obs_sumCW[y] <- sumCW[y]
-     }
-     if (y >= (fmyearIdx)){
-    obs_sumCW[y] <- get_error_idx(type=oe_sumCW_typ, 
+    
+    if (y < c(fmyearIdx)){
+      obs_sumCW[y] <- sumCW[y]
+    }
+    if (y >= (fmyearIdx)){
+      obs_sumCW[y] <- get_error_idx(type=oe_sumCW_typ, 
                                     idx=sumCW[y] * ob_sumCW, 
                                     par=oe_sumCW)
-     }
-
+    }
+    
     obs_paaCN[y,] <- get_error_paa(paa=paaCN[y,], 
-                                     par=oe_paaCN)
-  
+                                   par=oe_paaCN)
+    
     obs_sumIN[y] <- get_error_idx(type=oe_sumIN_typ, 
-                                          idx=sumIN[y] * ob_sumIN, 
-                                          par=oe_sumIN)
+                                  idx=sumIN[y] * ob_sumIN, 
+                                  par=oe_sumIN)
     
     # Observed index by weight is a function of the observed index and the
     # true paa. This preserves the fact that the multinomial paa and the
     # lognormal numbers-at-age are separate processes (which is consistent with
     # most assessment models)
     obs_sumIW[y] <- (obs_sumIN[y] * paaIN[y,]) %*% waa[y,]
-
+    
     obs_paaIN[y,] <- get_error_paa(paa=paaIN[y,], 
-                                     par=oe_paaIN)
-    })
-
+                                   par=oe_paaIN)
+  })
+  
   
 }
-      
+
