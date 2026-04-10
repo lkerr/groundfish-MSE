daea2726a865a58d9b192c9bdc6d3fb035fd2d54
Mazur <Mackenzie.Mazur@dfo-mpo.gc.ca>
2026-04-10 09:12:02 -0700

    Updated Rfun_BmsySim to not have the option for estimated uses, since all estimated uses will be through WHAM

--- processes/Rfun_BmsySim.R
+++ processes/Rfun_BmsySim.R
@@ -1,25 +1,15 @@
-# Recruitment function list for use in the recruitment index column
-# of the object mproc (created by the file generateMP.R)
-
 Rfun_BmsySim <- list(
   
-  forecast = function(type, parpop, parenv, SSB, sdR, stockEnv,y,block,...){
+  forecast = function(type, parpop, parenv, SSB, sdR, stockEnv, y, block,...){
     Rpar<-parpop$Rpar
-    if (stock[[i]]$R_mis=='TRUE'){
-      type<-'BH' 
-      Rpar<-parpop$Rpar_mis}
-    if (parpop$switch==TRUE){
-      type2<-'True'}
-    else{type2<-'Est'}
     Rpar['rho'] <- 0
     gr <- get_recruits(type = type, 
-                       type2=type2,
                        par = Rpar, 
                        SSB = SSB,
                        pe_R = sdR,
                        R_ym1 = 1, block = block,
                        Rhat_ym1 = 1,
-                       R_est=parpop$R, y=y)
+                       y=y)
     return(gr[['Rhat']])
   },
   
@@ -27,8 +17,8 @@ Rfun_BmsySim <- list(
     mean(tail(parpop$R,parmgt$BREF_PAR0))
   },
   
-  hindcastSample = function(Rest,...){
-    sample(Rest, 1)
+  hindcastSample = function(parpop,...){
+    sample(parpop$R, 1)
   }
   
 )
