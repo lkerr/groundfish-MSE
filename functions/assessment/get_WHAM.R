4b7cd0328bd17832db6eb404521bc2307de08614
Alexandra Schneider <alexandra.schneider@maine.edu>
2026-04-20 10:54:57 -0400

    keeps BRP R timeseries the same length as the OM

--- functions/assessment/get_WHAM.R
+++ functions/assessment/get_WHAM.R
@@ -147,9 +147,23 @@ get_WHAM <- function(stock,...){
       wham_dat_file[[1]]$dat$R_avg_end <- endyear - 10
 
     
+      
     # calls wham paramaterization set in wham_settings.R  
     input <- do.call(prepare_wham_input, c(list(asap3 = wham_dat_file), stock_wham_settings)) 
-
+    
+    
+       
+    # for the BRPs in the OM, the length of the R time series is set by BREF_PAR0
+    # if BREF_PAR0 == 200 the whole time series is used, if not an abbreviated timeseries is used
+    # to have equal BRP estimation techniques:
+    
+    if(mproc[m,'BREF_PAR0'] < 200){
+      
+      input$basic_info <- c(stock_wham_settings$basic_info, 
+                            list(XSPR_R_avg_yrs = tail(1:wham_dat_file[[1]]$dat$n_years, mproc[m,'BREF_PAR0'])))
+      
+    }
+    
     #### retaining fixed NAA to start, come back to this!!!!! 
     
     if (stock$stockName=='codWGOM'){
@@ -160,6 +174,8 @@ get_WHAM <- function(stock,...){
     }
     }
     
+ 
+    
     
     # Fit wham model
     
@@ -167,9 +183,10 @@ get_WHAM <- function(stock,...){
                         n.peels = 3, do.check=TRUE)
 
     
+    
     # Setting do.osa = TRUE results in "Error in getUserDLL() Multiple TMB models loaded" which is likely an issue with what model TMB is used by make_osa_residuals() - make_osa_resiudals() probably calls TMB::MakeADFun without specifying DLL = "wham"
     #save results from wham
-    # browser()
+    
     saveRDS(whamEst, file = paste(ResultDirectory, "/WHAM_", stockName,'_', r, '_', y, '.rdat', sep = '')) #??? probably don't want to save this, save a subset of results 
 
     
