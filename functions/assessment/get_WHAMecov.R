b3ccc9b93a1dd0eeffcc71ebb5d07aeb863dbbe2
Alexandra Schneider <alexandra.schneider@maine.edu>
2026-07-06 10:08:21 -0400

    update wham ecov

--- functions/assessment/get_WHAMecov.R
+++ functions/assessment/get_WHAMecov.R
@@ -1,21 +1,29 @@
 
 get_WHAMecov <- function(stock_wham_settings,...){
   
-  if(stock_wham_settings$model_name == "codWGOM"){
+  if(stock_wham_settings$model_name == "codWGOM"){ 
     
         ecov <-list(
       label = "Recruitment",
       logsigma = 'est_1', # estimate obs sigma, 1 value shared across years
       process_model = 'ar1', # "rw" or "ar1"
       recruitment_how = matrix("controlling-lag-1-linear")) # limiting when BH g > 0 I - think
-  }else{
+  
+        # update the rest of the wham settings for when you use ecovs
+        NAA_re = NULL
+        
+        recruit_model = 3
+        
+        updates <- list(ecov, NAA_re, recruit_model)
+        
+      }else{
 stop("no ecov setting for stock")
   }
     
 
     
 
-    return(ecov)
+    return(updates)
     
 }
 
