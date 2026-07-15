6ef3ebe551c6f95692fd241eac5d8892ea22a85b
Alexandra Schneider <alexandra.schneider@maine.edu>
2026-07-15 11:41:34 -0400

    continue NAA devs in OM

--- functions/popdy/get_J1Updates.R
+++ functions/popdy/get_J1Updates.R
@@ -73,17 +73,27 @@ get_J1Updates <- function(stock){
 
     # Add NAA deviations if in historical period and they exist. Otherwise, assume 0.
     devs <- if (histAssess == F |
-                is.null(assess_vals$NAA_deviations) |
-                !y %in% assess_vals$assessdat$MSEyr){rep(0, nage)} else {
+                is.null(assess_vals$NAA_deviations)) {rep(0, nage)} else {
                   
-                  assess_vals$NAA_deviations %>%
+                  if(y %in% assess_vals$assessdat$MSEyr){
+                    assess_vals$NAA_deviations %>%
                     dplyr::filter(MSEyr == y) %>% pull(re)
+                  
+                    }else{
+                    # add in mean + sd to carry forward
+                      assess_vals$NAA_m_sd %>%
+                        group_by(age) %>%
+                        mutate(re = rnorm(1, mean = mean, sd = sd)) %>% pull(re)
+                      
+                    
+                  }
+    
                 }
 
     # calculate what the Jan 1 population numbers are for year y, which
     # depend on the numbers and mortality rate in the previous year and
     # on the recruitment and naa deviations in this year
-    J1N[y,] <- get_J1Ny(J1Ny0=J1N[y-1,], Zy0=Z[y-1,], R[y], devs = devs)
+    J1N[y,] <- get_J1Ny(J1Ny0=J1N[y-1,], Zy0=Z[y-1,], Ry1 = R[y], devs = devs)
 
     # calculate the predicted catch in year y, the catch weight and the
     # proportions of catch numbers-at-age. Add small number in case F=0
